429d2ff3c46d8fdd0870806c973d0d41b0cbcfdc
diegofdg <diegofdg@gmail.com>
2023-01-28 19:52:26 -0300

    01_citas_react-creando el componente de formulario

--- 01_citas_react/src/App.jsx
+++ 01_citas_react/src/App.jsx
@@ -7,8 +7,11 @@ function App() {
   return (
     <div className="container mx-auto mt-20">
       <Header />
-      <Formulario />
-      <ListadoPacientes />
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+      
     </div>    
   )
 }
--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -1,6 +1,15 @@
 const Formulario = () => {
   return (
-    <h2>Formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center">
+        Añade Pacientes y {''}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+      <form>
+        
+      </form>
+    </div>
   )
 }
 
