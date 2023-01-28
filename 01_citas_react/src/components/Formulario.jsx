c0fcb6c8ea6439c2426515367edae32e0dc9f3cc
diegofdg <diegofdg@gmail.com>
2023-01-28 20:01:02 -0300

    01_citas_react-creando un input en el formulario

--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -2,12 +2,22 @@ const Formulario = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-      <p className="text-lg mt-5 text-center">
+      <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-      <form>
-        
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div>
+          <label 
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
       </form>
     </div>
   )
