91fa284d563f9258db1e12ae71edb04f45072cb4
diegofdg <diegofdg@gmail.com>
2023-01-29 18:31:51 -0300

    01_citas_react-finalizando la funcionalidad de localstorage

--- 01_citas_react/src/App.jsx
+++ 01_citas_react/src/App.jsx
@@ -5,8 +5,17 @@ import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
 
-  const [ pacientes, setPacientes ] = useState([]);
+  const [ pacientes, setPacientes ] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
   const [ paciente, setPaciente ] = useState({});
+
+  //  *** Deprecated *** //
+  //  useEffect( () => {
+  //    const obtenerLS = () => {
+  //      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+  //      setPacientes(pacientesLS);
+  //    }
+  //    obtenerLS();
+  //  }, []);
 
   useEffect( () => {
     localStorage.setItem('pacientes', JSON.stringify(pacientes));
