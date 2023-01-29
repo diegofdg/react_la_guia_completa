91b4ee30c777819b3b3150e710f7834169febe0e
diegofdg <diegofdg@gmail.com>
2023-01-29 06:35:27 -0300

    01_citas_react-eventos y sintaxis en react

--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 
 const Formulario = () => {
   const [ nombre, setNombre ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Enviando Formulario');
+  }
   
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -10,7 +15,9 @@ const Formulario = () => {
         Añade Pacientes y {''}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form 
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
           <label 
             htmlFor="mascota"
