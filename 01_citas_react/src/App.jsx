2ec4c572433342cecad68210b2a56c372bcbdb8b
diegofdg <diegofdg@gmail.com>
2023-01-29 07:44:45 -0300

    01_citas_react-iterando sobre un array en react

--- 01_citas_react/src/App.jsx
+++ 01_citas_react/src/App.jsx
@@ -15,7 +15,9 @@ function App() {
           pacientes={pacientes}
           setPacientes={setPacientes}
         />
-        <ListadoPacientes />
+        <ListadoPacientes
+          pacientes={pacientes}
+        />
       </div>
       
     </div>    
--- 01_citas_react/src/components/ListadoPacientes.jsx
+++ 01_citas_react/src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -8,10 +8,14 @@ const ListadoPacientes = () => {
         Administra tus {''}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
+
+      { pacientes.map( paciente => {
+        <Paciente
+          paciente={paciente}
+        />
+      })}
+
+      
     </div>
   )
 }
