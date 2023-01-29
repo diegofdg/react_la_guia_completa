955e138d87f2973664e37913d0bbd20a783cede8
diegofdg <diegofdg@gmail.com>
2023-01-29 07:36:44 -0300

    01_citas_react-el prop children

--- 01_citas_react/src/components/Error.jsx
+++ 01_citas_react/src/components/Error.jsx
@@ -1,7 +1,7 @@
-const Error = ({ mensaje }) => {
+const Error = ({ children }) => {
   return (
     <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-      <p>{mensaje}</p>
+      {children}
     </div>
   )
 }
--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -54,9 +54,9 @@ const Formulario = ({ pacientes, setPacientes }) => {
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
         { error && 
-          <Error
-            mensaje={'Todos los campos son obligatorios'}
-          />
+          <Error>
+            <p>Todos los campos son obligatorios</p>
+          </Error>
         }
         <div className="mb-5">
           <label 
