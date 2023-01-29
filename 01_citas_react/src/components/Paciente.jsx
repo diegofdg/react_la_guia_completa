222df68fda00d0d0ffd84eca66dad667ec4928a1
diegofdg <diegofdg@gmail.com>
2023-01-29 11:07:09 -0300

    01_citas_react-creando botones de editar y eliminar

--- 01_citas_react/src/components/Paciente.jsx
+++ 01_citas_react/src/components/Paciente.jsx
@@ -23,7 +23,22 @@ const Paciente = ({ paciente }) => {
           Síntomas: {''}
           <span className="font-normal normal-case">{sintomas}</span>
       </p>
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
       </div>
+    </div>
   )
 }
 
