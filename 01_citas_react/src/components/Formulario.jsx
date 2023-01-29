284c984bed918c9e05f9a05427ab18b319867f50
diegofdg <diegofdg@gmail.com>
2023-01-29 07:32:54 -0300

    01_citas_react-creando un componente de errores

--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = ({ pacientes, setPacientes }) => {
   const [ nombre, setNombre ] = useState('');
@@ -52,10 +53,10 @@ const Formulario = ({ pacientes, setPacientes }) => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error && 
-          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-            <p>Todos los campos son obligatorios</p>
-          </div>
+        { error && 
+          <Error
+            mensaje={'Todos los campos son obligatorios'}
+          />
         }
         <div className="mb-5">
           <label 
