6a37d956274454215e15a9b4bb6f17e4898b3887
diegofdg <diegofdg@gmail.com>
2023-01-29 11:43:38 -0300

    01_citas_react-mostrando de forma condicional textos en el formulario

--- 01_citas_react/src/components/Formulario.jsx
+++ 01_citas_react/src/components/Formulario.jsx
@@ -143,7 +143,7 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-          value="Agregar Paciente"
+          value={paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
         />
       </form>
     </div>
