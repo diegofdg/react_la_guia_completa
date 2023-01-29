492744403bc0969387ea4427148755d63898be49
diegofdg <diegofdg@gmail.com>
2023-01-29 07:13:27 -0300

    01_citas_react-pasando información entre componentes con props

--- 01_citas_react/src/App.jsx
+++ 01_citas_react/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+
+  const [ pacientes, setPacientes ] = useState([]);
   
   return (
     <div className="container mx-auto mt-20">
-      <Header />
+      <Header
+        
+      />
       <div className="mt-12 md:flex">
         <Formulario />
         <ListadoPacientes />
