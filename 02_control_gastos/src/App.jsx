6b8a4e2b90d042df4b117191d1bf845645c4134f
diegofdg <diegofdg@gmail.com>
2023-01-30 07:26:21 -0300

    02_control_gastos-creando el formulario de presupuesto

--- 02_control_gastos/src/App.jsx
+++ 02_control_gastos/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+
+  const [ presupuesto, setPresupuesto ] = useState(0);
+
   return (
     <div>
-      <Header />
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </div>
   )
 }
--- 02_control_gastos/src/components/NuevoPresupuesto.jsx
+++ 02_control_gastos/src/components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra">
       <form className="formulario">
@@ -10,6 +10,8 @@ const NuevoPresupuesto = () => {
             type="text"
             className="nuevo-presupuesto"
             placeholder="Añade tu Presupuesto"
+            value={presupuesto}
+            onChange={ e => setPresupuesto(e.target.value) }
           />
         </div>
         <input
