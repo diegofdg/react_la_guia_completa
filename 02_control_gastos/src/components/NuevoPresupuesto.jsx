b92bb178a1834543a6d7d86c98faaad77fecc497
diegofdg <diegofdg@gmail.com>
2023-01-30 08:13:46 -0300

    02_control_gastos-mostrando el componente de gastos si el presupuesto es válido

--- 02_control_gastos/src/components/NuevoPresupuesto.jsx
+++ 02_control_gastos/src/components/NuevoPresupuesto.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({
+  presupuesto,
+  setPresupuesto,
+  setIsValidPresupuesto
+}) => {
 
   const [ mensaje, setMensaje ] = useState('');
 
   const handlePresupuesto = (e) => {
     e.preventDefault();
 
-    if(!Number(presupuesto) || Number(presupuesto) < 0) {
+    if(!presupuesto || presupuesto < 0) {
       setMensaje('No es un presupuesto válido');
+      return;
     }
+    setMensaje('');
+    setIsValidPresupuesto(true);
 
   }
   return (
@@ -21,11 +28,11 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
         <div className="campo">
           <label>Definir Presupuesto</label>
           <input
-            type="text"
+            type="number"
             className="nuevo-presupuesto"
             placeholder="Añade tu Presupuesto"
             value={presupuesto}
-            onChange={ e => setPresupuesto(e.target.value) }
+            onChange={ e => setPresupuesto(Number(e.target.value)) }
           />
         </div>
         <input
