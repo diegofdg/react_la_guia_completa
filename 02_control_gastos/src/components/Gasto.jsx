29ef65491e71123fe73c208e16ef59465dfa3c98
diegofdg <diegofdg@gmail.com>
2023-01-31 19:58:58 -0300

    02_control_gastos-finalizando el efecto de swipe

--- 02_control_gastos/src/components/Gasto.jsx
+++ 02_control_gastos/src/components/Gasto.jsx
@@ -28,38 +28,46 @@ const diccionarioIconos = {
 const Gasto = ({ gasto }) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto;
 
-  const leadingActions = () => {
-    console.log("Editar");
-  }
+  const leadingActions = () => (
+    <LeadingActions>
+      <SwipeAction onClick={()=> console.log('Editar')}>
+        Editar
+      </SwipeAction>  
+    </LeadingActions>
+  )
 
-  const trailingActions = () => {
-    console.log("Eliminar");
-  }
+  const trailingActions = () => (
+    <TrailingActions>
+      <SwipeAction onClick={()=> console.log('Eliminar')}>
+        Eliminar
+      </SwipeAction>  
+    </TrailingActions>
+  )
 
   return (
     <SwipeableList>
       <SwipeableListItem
-        leadingActions={leadingActions}
-        trailingActions={trailingActions}
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
       >
         <div className="gasto sombra">
         <div className="contenido-gasto">
-            <img
-                src={diccionarioIconos[categoria]}
-                alt="Icono Gasto"
-            />
-            <div className="descripcion-gasto">
-            <p className="categoria">
-                {categoria}
-            </p>
-            <p className="nombre-gasto">
-                {nombre}
-            </p>
-            <p className="fecha-gasto">
-                Agregado el: {''}
-                <span>{formatearFecha(fecha)}</span>
-            </p>
-            </div>
+          <img
+            src={diccionarioIconos[categoria]}
+            alt="Icono Gasto"
+          />
+          <div className="descripcion-gasto">
+          <p className="categoria">
+            {categoria}
+          </p>
+          <p className="nombre-gasto">
+            {nombre}
+          </p>
+          <p className="fecha-gasto">
+            Agregado el: {''}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
+          </div>
         </div>
         <p className="cantidad-gasto">${cantidad}</p>
         </div>
