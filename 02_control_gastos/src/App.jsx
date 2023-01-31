e2e0f6757e96ab07e60ecfde09a0e3ce4f015d06
diegofdg <diegofdg@gmail.com>
2023-01-31 05:51:17 -0300

    02_control_gastos-añadir un botón para registrar un nuevo gasto

--- 02_control_gastos/src/App.jsx
+++ 02_control_gastos/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
 
@@ -14,6 +15,12 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+        </div>
+      )}
     </div>
   )
 }
