6c6d1a70056d9927dfe07370e7b657b584dd919e
diegofdg <diegofdg@gmail.com>
2023-01-31 17:42:42 -0300

    02_control_gastos-creando una función para generar un id único y añadiendo gastos al state

--- 02_control_gastos/src/App.jsx
+++ 02_control_gastos/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Modal from './components/Modal';
+import { generarId } from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
@@ -20,8 +21,14 @@ function App() {
   }
 
   const guardarGasto = gasto => {
-    console.log(gasto);
+    gasto.id = generarId();
+    setGastos([ ...gastos, gasto ]);
 
+    setAnimarModal(false);
+
+    setTimeout(()=>{
+        setModal(false);
+      }, 500);
   }
 
   return (
