36b1626b5ea7c7259357cf00187a4d3f99697076
diegofdg <diegofdg@gmail.com>
2023-01-31 05:55:05 -0300

    02_control_gastos-primeros pasos para generar un modal

--- 02_control_gastos/src/App.jsx
+++ 02_control_gastos/src/App.jsx
@@ -6,6 +6,11 @@ function App() {
 
   const [ presupuesto, setPresupuesto ] = useState(0);
   const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(false);
+  const [ modal, setModal ] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  }
 
   return (
     <div>
@@ -18,9 +23,15 @@ function App() {
 
       {isValidPresupuesto && (
         <div className='nuevo-gasto'>
-          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+            />
         </div>
       )}
+
+      {modal && <p>Desde modal</p>}
     </div>
   )
 }
