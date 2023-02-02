d4b97957bdd53d37687397151dcd5679e729f045
diegofdg <diegofdg@gmail.com>
2023-02-02 20:31:46 -0300

    02_control_gastos-colocar el presupuesto en localstorage

--- 02_control_gastos/src/App.jsx
+++ 02_control_gastos/src/App.jsx
@@ -8,7 +8,9 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg';
 function App() {
 
   const [ gastos, setGastos ] = useState([]);
-  const [ presupuesto, setPresupuesto ] = useState(0);
+  const [ presupuesto, setPresupuesto ] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [ isValidPresupuesto, setIsValidPresupuesto ] = useState(false);
   const [ modal, setModal ] = useState(false);
   const [ animarModal, setAnimarModal ] = useState(false);
@@ -24,6 +26,18 @@ function App() {
     }
     
   },[gastoEditar]);
+
+  useEffect(()=>{
+    localStorage.setItem('presupuesto', presupuesto ?? 0);
+  },[presupuesto]);
+
+  useEffect(()=>{
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+    if(presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+
+  },[]);
 
   const handleNuevoGasto = () => {
     setModal(true);
