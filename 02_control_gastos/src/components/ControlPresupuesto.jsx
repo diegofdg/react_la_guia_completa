e6669496647c16036360f7adad93c209de36cbc0
diegofdg <diegofdg@gmail.com>
2023-02-02 19:02:18 -0300

    02_control_gastos-calcular el porcentaje gastado para la gráfica circular

--- 02_control_gastos/src/components/ControlPresupuesto.jsx
+++ 02_control_gastos/src/components/ControlPresupuesto.jsx
@@ -5,14 +5,24 @@ import 'react-circular-progressbar/dist/styles.css';
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
   const [ disponible, setDisponible ] = useState(0);
-  const [ gastado, setGastado ] = useState(0);  
+  const [ gastado, setGastado ] = useState(0);
+  const [ porcentaje, setPorcentaje ] = useState(0);
+
 
   useEffect(()=>{
     const totalGastado = gastos.reduce( ( total, gasto ) => gasto.cantidad + total, 0);
     const totalDisponible = presupuesto - totalGastado;
 
+    // Calcular el porcentaje gastado
+    const nuevoPorcentaje = ( ( (presupuesto - totalDisponible) / presupuesto ) * 100 ).toFixed(2);
+
     setGastado(totalGastado);
     setDisponible(totalDisponible);
+
+    setTimeout(()=>{
+      setPorcentaje(nuevoPorcentaje);
+    },1000);
+
   }, [gastos]);
 
   const formatearCantidad = cantidad => {
@@ -26,7 +36,7 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
         <div>
             <CircularProgressbar
-              value={50}
+              value={porcentaje}
             />
         </div>
         <div className="contenido-presupuesto">
