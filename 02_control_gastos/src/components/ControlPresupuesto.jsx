742e7da5cae1f4f67c2f75f7c8850f831f6fe68d
diegofdg <diegofdg@gmail.com>
2023-02-02 18:37:16 -0300

    02_control_gastos-agregando una gráfica circular para el control del presupuesto

--- 02_control_gastos/src/components/ControlPresupuesto.jsx
+++ 02_control_gastos/src/components/ControlPresupuesto.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
 
@@ -23,7 +25,9 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
         <div>
-            <p>Gráfica aquí</p>
+            <CircularProgressbar
+              value={50}
+            />
         </div>
         <div className="contenido-presupuesto">
             <p>
