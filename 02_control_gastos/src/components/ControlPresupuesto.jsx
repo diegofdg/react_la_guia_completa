2f4adedd81ec101546f26594415606f72a480b70
diegofdg <diegofdg@gmail.com>
2023-02-02 19:07:13 -0300

    02_control_gastos-personalizando la gráfica circular con estilos

--- 02_control_gastos/src/components/ControlPresupuesto.jsx
+++ 02_control_gastos/src/components/ControlPresupuesto.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { CircularProgressbar } from 'react-circular-progressbar';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => {
@@ -36,6 +36,10 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
         <div>
             <CircularProgressbar
+              styles={buildStyles({
+                pathColor: '#3B82F6',
+                trailColor: '#F5F5F5'
+              })}
               value={porcentaje}
             />
         </div>
