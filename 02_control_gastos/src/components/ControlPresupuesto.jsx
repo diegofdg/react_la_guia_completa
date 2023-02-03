2928b58967fc6d9ce4b7baa33144e2cd92338e34
diegofdg <diegofdg@gmail.com>
2023-02-02 21:31:01 -0300

    02_control_gastos-cambiando la apariencia de presupuesto disponible si ya fue agregado

--- 02_control_gastos/src/components/ControlPresupuesto.jsx
+++ 02_control_gastos/src/components/ControlPresupuesto.jsx
@@ -37,8 +37,9 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
         <div>
             <CircularProgressbar
               styles={buildStyles({
-                pathColor: '#3B82F6',
-                trailColor: '#F5F5F5'
+                pathColor: porcentaje > 100 ? '#DC2626' : '#3B82F6',
+                trailColor: '#F5F5F5',
+                textColor: porcentaje > 100 ? '#DC2626' : '#3B82F6'
               })}
               value={porcentaje}
               text={`${porcentaje}% Gastado`}
@@ -48,7 +49,7 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
             <p>
                 <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
             </p>
-            <p>
+            <p className={`${disponible < 0 ? 'negativo' : ''}`}>
                 <span>Disponible: </span>{formatearCantidad(disponible)}
             </p>
             <p>
