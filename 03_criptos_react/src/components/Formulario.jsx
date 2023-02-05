8ee62536dcd16cdbc7a6a8c5b5d2097037bc2804
diegofdg <diegofdg@gmail.com>
2023-02-05 07:02:32 -0300

    03_criptos_react-formateando un array para pasarlo como opciones

--- 03_criptos_react/src/components/Formulario.jsx
+++ 03_criptos_react/src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from "@emotion/styled";
 import useSelectMoneda from "../hooks/useSelectMoneda";
 import { monedas } from "../data/Monedas";
@@ -23,15 +23,27 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+  const [ criptos, setCriptos ] = useState([]);
   const [ moneda, SelectMonedas ] = useSelectMoneda('Elige tu Moneda', monedas);
 
   useEffect(()=>{
     const consultarAPI = async () => {
-      const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`;
+      const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
-      console.log(resultado.Data);
+
+      const arrayCriptos = resultado.Data.map( cripto => {
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName
+        }
+
+        return objeto;
+      });
+
+      setCriptos(arrayCriptos);
     }
+    
     consultarAPI();
   },[]);
   
