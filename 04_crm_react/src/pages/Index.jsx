fca6a19d618691047af6f1b2271fe0f9a5d4d92f
diegofdg <diegofdg@gmail.com>
2023-02-07 23:51:45 -0300

    04_crm_react-creando un loader

--- 04_crm_react/src/pages/Index.jsx
+++ 04_crm_react/src/pages/Index.jsx
@@ -1,6 +1,13 @@
+export function loader() {
+  return "Desde Loader";
+}
+
 function Index() {
   return (
-    <div>Clientes</div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+    </>
   )
 }
   
