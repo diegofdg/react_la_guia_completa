63293609d366b2ca98b4ee375f75e6130f325e0c
diegofdg <diegofdg@gmail.com>
2023-02-08 00:43:27 -0300

    04_crm_react-creando un action para el formulario

--- 04_crm_react/src/pages/NuevoCliente.jsx
+++ 04_crm_react/src/pages/NuevoCliente.jsx
@@ -1,5 +1,10 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Form } from 'react-router-dom';
 import Formulario from '../components/Formulario';
+
+export function action() {
+  console.log('submit');
+  return null;
+}
 
 function NuevoCliente() {
   const navigate = useNavigate();
@@ -18,14 +23,16 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <form>
+        <Form
+          method="POST"
+        >
           <Formulario />
           <input
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
             value="Registrar Cliente"
           />
-        </form>
+        </Form>
       </div>
     </>
   )
