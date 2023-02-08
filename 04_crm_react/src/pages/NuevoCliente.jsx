f3389d7afe4b0a28f884d6e109f82260c03452b2
diegofdg <diegofdg@gmail.com>
2023-02-08 00:35:14 -0300

    04_crm_react-agregando el formulario

--- 04_crm_react/src/pages/NuevoCliente.jsx
+++ 04_crm_react/src/pages/NuevoCliente.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import Formulario from '../components/Formulario';
 
 function NuevoCliente() {
   const navigate = useNavigate();
@@ -6,6 +7,7 @@ function NuevoCliente() {
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
       <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
       <div className="flex justify-end">
         <button
           className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
@@ -14,8 +16,16 @@ function NuevoCliente() {
           Volver
         </button>
       </div>
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <p>Formulario aquí</p>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registrar Cliente"
+          />
+        </form>
       </div>
     </>
   )
