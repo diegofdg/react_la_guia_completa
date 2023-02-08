4e6ee23234164a37909ea8c88878c1e455cc326e
diegofdg <diegofdg@gmail.com>
2023-02-08 01:09:17 -0300

    04_crm_react-diferencias de useloaderdata y useactiondata y más validaciones

--- 04_crm_react/src/pages/NuevoCliente.jsx
+++ 04_crm_react/src/pages/NuevoCliente.jsx
@@ -5,11 +5,17 @@ import Error from '../components/Error';
 export async function action({ request }) {
   const formData = await request.formData();
   const datos = Object.fromEntries(formData);
+  const email = formData.get('email');
   
   // Validación
   const errores = [];
   if(Object.values(datos).includes('')) {
-    errores.push('Todos los campos son obligatorios')
+    errores.push('Todos los campos son obligatorios');
+  }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)) {
+    errores.push('El email no es válido');
   }
 
   // Retornar datos si hay errores
