8171342d146a7e06c7950ecfb9b637da897ba7ad
diegofdg <diegofdg@gmail.com>
2023-02-08 00:18:26 -0300

    04_crm_react-iterando sobre clientes y mostrando la información

---
+++ 04_crm_react/src/components/Cliente.jsx
@@ -0,0 +1,13 @@
+function Cliente ({ cliente }) {
+  const { nombre, empresa, email, telefono, id } = cliente;
+
+  return (
+    <tr>
+      <td className="p-6">
+        {nombre}
+      </td>
+    </tr>
+  )
+}
+
+export default Cliente;
--- 04_crm_react/src/pages/Index.jsx
+++ 04_crm_react/src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
+import Cliente from '../components/Cliente';
 
 export function loader() {
   const clientes = [
@@ -45,11 +46,34 @@ export function loader() {
 function Index() {
   const clientes = useLoaderData();
   console.log(clientes);
-  
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tus clientes</p>
+      {clientes.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Cliente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map(cliente => (
+              <Cliente
+                cliente={cliente}
+                key={cliente.id}
+              />
+            ))}
+          </tbody>
+
+        </table>
+
+      ) : (
+        <p className="text-center mt-10">No hay clientes aún</p>
+      )}
     </>
   )
 }
