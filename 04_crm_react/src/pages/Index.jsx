0f8ae180314ac7fbf09e5dc8520d56d7479894bc
diegofdg <diegofdg@gmail.com>
2023-02-07 23:58:07 -0300

    04_crm_react-obtener los datos del loader con useloaderdata

--- 04_crm_react/src/pages/Index.jsx
+++ 04_crm_react/src/pages/Index.jsx
@@ -1,8 +1,51 @@
+import { useLoaderData } from 'react-router-dom';
+
 export function loader() {
-  return "Desde Loader";
+  const clientes = [
+    {
+      id: 1,
+      nombre: 'Juan',
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: 'Codigo Con Juan'
+    },
+    {
+      id: 2,
+      nombre: 'Karen',
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: 'Codigo Con Juan'
+    },
+    {
+      id: 3,
+      nombre: 'Josue',
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: 'Codigo Con Juan'
+    },
+    {
+      id: 4,
+      nombre: 'Miguel',
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: 'Codigo Con Juan'
+    },
+    {
+      id: 5,
+      nombre: 'Pedro',
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: 'Codigo Con Juan'
+    },
+  ];
+
+  return clientes;
 }
 
 function Index() {
+  const clientes = useLoaderData();
+  console.log(clientes);
+  
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
