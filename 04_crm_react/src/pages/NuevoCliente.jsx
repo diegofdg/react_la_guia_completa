a77120e9c4500eb063ee4e6cf71eacee26ba09ec
diegofdg <diegofdg@gmail.com>
2023-02-08 01:03:27 -0300

    04_crm_react-añadir validación al formulario

--- 04_crm_react/src/pages/NuevoCliente.jsx
+++ 04_crm_react/src/pages/NuevoCliente.jsx
@@ -1,15 +1,28 @@
-import { useNavigate, Form } from 'react-router-dom';
+import { useNavigate, Form, useActionData } from 'react-router-dom';
 import Formulario from '../components/Formulario';
+import Error from '../components/Error';
 
 export async function action({ request }) {
   const formData = await request.formData();
   const datos = Object.fromEntries(formData);
-  console.log(datos);
+  
+  // Validación
+  const errores = [];
+  if(Object.values(datos).includes('')) {
+    errores.push('Todos los campos son obligatorios')
+  }
+
+  // Retornar datos si hay errores
+  if(Object.keys(errores).length) {
+    return errores;
+  }
   return null;
 }
 
 function NuevoCliente() {
   const navigate = useNavigate();
+  const errores = useActionData();
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -25,6 +38,7 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        {errores?.length && errores.map(( error, i ) => <Error key={i}>{error}</Error>)}
         <Form
           method="POST"
         >
