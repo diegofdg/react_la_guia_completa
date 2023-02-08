049cbbbe8218cffaeaa8c1ee83089b57f51fb22b
diegofdg <diegofdg@gmail.com>
2023-02-08 09:32:29 -0300

    04_crm_react-conectando json-server a nuestro proyecto

--- 04_crm_react/src/pages/Index.jsx
+++ 04_crm_react/src/pages/Index.jsx
@@ -1,51 +1,14 @@
 import { useLoaderData } from 'react-router-dom';
 import Cliente from '../components/Cliente';
+import { obtenerClientes } from '../data/Clientes';
 
 export function loader() {
-  const clientes = [
-    {
-      id: 1,
-      nombre: 'Juan',
-      telefono: 102013313,
-      email: "juan@juan.com",
-      empresa: 'Codigo Con Juan'
-    },
-    {
-      id: 2,
-      nombre: 'Karen',
-      telefono: 138198313,
-      email: "karen@juan.com",
-      empresa: 'Codigo Con Juan'
-    },
-    {
-      id: 3,
-      nombre: 'Josue',
-      telefono: 31983913,
-      email: "josue@juan.com",
-      empresa: 'Codigo Con Juan'
-    },
-    {
-      id: 4,
-      nombre: 'Miguel',
-      telefono: 319381983,
-      email: "miguel@juan.com",
-      empresa: 'Codigo Con Juan'
-    },
-    {
-      id: 5,
-      nombre: 'Pedro',
-      telefono: 1398198938,
-      email: "pedro@juan.com",
-      empresa: 'Codigo Con Juan'
-    },
-  ];
-
+  const clientes = obtenerClientes();
   return clientes;
 }
 
 function Index() {
   const clientes = useLoaderData();
-  console.log(clientes);
 
   return (
     <>
