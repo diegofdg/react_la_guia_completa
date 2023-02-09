684895a8f99a6f764a2ce7c6bdd433858fa82f22
diegofdg <diegofdg@gmail.com>
2023-02-09 16:57:01 -0300

    06_guitarla_remix-creando la página de nosotros

--- 06_guitarla_remix/app/routes/nosotros.jsx
+++ 06_guitarla_remix/app/routes/nosotros.jsx
@@ -1,8 +1,19 @@
+import imagen from '../../public/img/nosotros.jpg';
+
 function Nosotros() {
   return (
-    <div>
-      Desde nosotros.jsx
-    </div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">
+        Nosotros
+      </h2>
+      <div className="contenido">
+        <img src={imagen} alt="Imagen sobre nosotros" />
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, doloremque? Natus eius quasi necessitatibus ab qui omnis aspernatur quibusdam at dolorum id rerum, pariatur sapiente. Cumque minima ea quo culpa?</p>
+          <p>Alias aut temporibus suscipit eaque, officia non nobis voluptates, ab consequatur unde molestias mollitia iste eligendi cupiditate deleniti dolorem, dolores vel quis. Id laudantium distinctio iste aperiam a facere corrupti!</p>
+        </div>
+      </div>
+    </main>
   )
 }
 
