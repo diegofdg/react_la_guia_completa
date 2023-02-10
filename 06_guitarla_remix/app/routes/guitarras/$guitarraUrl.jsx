f39b769d3821068d7c51beb58938f61f76b8e1f6
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-10 16:06:07 -0300

    06_guitarla_remix-mostrando información de la guitarra

--- 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
+++ 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -1,5 +1,16 @@
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarra } from '~/models/guitarras.server';
+import styles from '~/styles/guitarras.css';
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+  ]
+
+}
 
 export async function loader({ params }) {
   const { guitarraUrl } = params;
@@ -9,12 +20,17 @@ export async function loader({ params }) {
 
 function Guitarra() {
   const guitarra = useLoaderData();
-  console.log(guitarra.data[0].attributes.nombre);
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
   
   return (
-    <div>
-      Desde Guitarra.jsx
-    </div>
+    <main className="contenedor guitarra">
+      <img className="imagen" src={imagen.data.attributes.url} alt={`Imagen de la guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">{precio}</p>
+      </div>
+    </main>
   )
 }
 
