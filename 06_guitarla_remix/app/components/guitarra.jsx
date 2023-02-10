7656f224eb9fa6c2282d70845249a7f62cacf13c
diegofdg <diegofdg@gmail.com>
2023-02-10 02:23:34 -0300

    06_guitarla_remix-mostrando la información de la guitarra

--- 06_guitarla_remix/app/components/guitarra.jsx
+++ 06_guitarla_remix/app/components/guitarra.jsx
@@ -1,8 +1,23 @@
+import { Link } from '@remix-run/react';
+
 function Guitarra({ guitarra }) {
-  console.log(guitarra);
-  
+  const { descripcion, imagen, precio, url, nombre } = guitarra;
+
   return (
-    <div>Guitarra</div>
+    <div className="guitarra">
+      <img src={imagen.data.attributes.formats.medium.url} alt={`Imagen guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="descripcion">{descripcion}</p>
+        <p className="precio">${precio}</p>
+        <Link
+          className="enlace"
+          to={`/guitarras/${url}`}
+        >
+          Ver Producto
+        </Link>
+      </div>
+    </div>
   )
 }
 
