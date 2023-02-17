a2320686f162bc0821d8900d44d045673144113b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-17 07:44:24 -0300

    06_guitarla_remix-aplicando css al blog

--- 06_guitarla_remix/app/routes/blog.jsx
+++ 06_guitarla_remix/app/routes/blog.jsx
@@ -1,6 +1,16 @@
 import { useLoaderData } from "@remix-run/react";
 import Post from "../components/post";
 import { getPosts } from "../models/posts.server";
+import styles from '~/styles/blog.css';
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader() {
   const posts = await getPosts();
