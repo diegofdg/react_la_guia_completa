b2b7bfb4b968cba0d0a04e50e03afd8e1194a4b3
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-18 08:53:08 -0300

    06_guitarla_remix-state en remix

--- 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
+++ 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarra } from '~/models/guitarras.server';
 
@@ -29,6 +30,8 @@ export function meta({ data }) {
 }
 
 function Guitarra() {
+  const [ cantidad, setCantidad ] = useState(0);
+
   const guitarra = useLoaderData();
   const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
   
@@ -41,7 +44,10 @@ function Guitarra() {
         <p className="precio">{precio}</p>
         <form className="formulario">
           <label htmlFor="cantidad">Cantidad</label>
-          <select id="cantidad">
+          <select
+            onChange={e => setCantidad(parseInt(e.target.value))}
+            id="cantidad"
+          >
             <option value="">--Seleccione--</option>
             <option value="1">1</option>
             <option value="2">2</option>
