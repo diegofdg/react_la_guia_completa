b247ed033f75ef6b417d4489dea4e5df6b95b43a
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-18 08:59:43 -0300

    06_guitarla_remix-validando la acción de añadir al carrito

--- 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
+++ 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -33,7 +33,24 @@ function Guitarra() {
   const [ cantidad, setCantidad ] = useState(0);
 
   const guitarra = useLoaderData();
-  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(cantidad < 1) {
+      alert('Debes seleccionar una guitarra');
+      return;
+    }
+
+    const guitarraSeleccionada = {
+      id: guitarra.data[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad 
+    }
+    console.log(guitarraSeleccionada);
+  }
   
   return (
     <div className="guitarra">
@@ -42,13 +59,16 @@ function Guitarra() {
         <h3>{nombre}</h3>
         <p className="texto">{descripcion}</p>
         <p className="precio">{precio}</p>
-        <form className="formulario">
+        <form
+          onSubmit={handleSubmit}
+          className="formulario"
+        >
           <label htmlFor="cantidad">Cantidad</label>
           <select
             onChange={e => setCantidad(parseInt(e.target.value))}
             id="cantidad"
           >
-            <option value="">--Seleccione--</option>
+            <option value="0">--Seleccione--</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
