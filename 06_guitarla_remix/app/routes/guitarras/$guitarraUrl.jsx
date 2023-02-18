19993a78f22c09985f0daf1f1faaeabd4be071e1
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-18 08:45:51 -0300

    06_guitarla_remix-añadiendo la opción de añadir al carrito en la url de guitarra

--- 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
+++ 06_guitarla_remix/app/routes/guitarras/$guitarraUrl.jsx
@@ -39,6 +39,21 @@ function Guitarra() {
         <h3>{nombre}</h3>
         <p className="texto">{descripcion}</p>
         <p className="precio">{precio}</p>
+        <form className="formulario">
+          <label htmlFor="cantidad">Cantidad</label>
+          <select id="cantidad">
+            <option value="">--Seleccione--</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <input
+            type="submit"
+            value="Agregar al carrito"
+          />
+        </form>
       </div>
     </div>
   )
