581c422fa79a56fbd334dfef8550b6dff6670e89
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-18 08:33:49 -0300

    06_guitarla_remix-añadiendo el contenido y css

--- 06_guitarla_remix/app/routes/carrito.jsx
+++ 06_guitarla_remix/app/routes/carrito.jsx
@@ -1,6 +1,35 @@
+import styles from '~/styles/carrito.css';
+
+export function meta() {
+  return {
+    title: 'GuitarLA - Carrito de Compras',
+    description: 'Venta de guitarras, música, blog, carrito de compras, tienda'
+  }
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
 function Carrito() {
   return (
-    <div>carrito</div>
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Contenido</h2>
+        </div>
+        <aside className="resumen">
+          <h3>Resumen del Pedido</h3>
+          <p>Total a Pagar: $</p>
+        </aside>
+      </div>
+    </main>
   )
 }
 
