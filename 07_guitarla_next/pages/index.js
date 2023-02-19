a15b537f2842e21cafbe4d895e4dabab447da422
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-19 07:56:25 -0300

    07_guitarla_next-el modelo basado en rutas y link en nextjs

--- 07_guitarla_next/pages/index.js
+++ 07_guitarla_next/pages/index.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
       <h1>Hola Mundo desde Next</h1>
+      <Link href="/nosotros">Nosotros</Link>
     </>
   )
 }
---
+++ 07_guitarla_next/pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Nosotros() {
+  return (
+    <>
+      <div>nosotros</div>
+      <Link href="/">Ir a Inicio</Link>
+    </>
+  )
+}
