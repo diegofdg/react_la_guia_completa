4dcb8018f4690d241004da33cd7ba65f4f461fb9
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-20 07:03:03 -0300

    07_guitarla_next-cómo dar estilos a un proyecto de next.js con css modules

--- 07_guitarla_next/components/header.js
+++ 07_guitarla_next/components/header.js
@@ -1,12 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
+import styles from "../styles/header.module.css"
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <div className="contenedor">
         <Image src="/img/logo.svg" width={300} height={40} alt='imagen logotipo' />
-        <nav>
+        <nav className={styles.navegacion}>
           <Link href="/">
             Inicio
           </Link>
