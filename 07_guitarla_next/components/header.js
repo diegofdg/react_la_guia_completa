4433180eefaf78e3a8d93873ba6fd5203539e33d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-20 07:24:47 -0300

    07_guitarla_next-resaltar la página actual

--- 07_guitarla_next/components/header.js
+++ 07_guitarla_next/components/header.js
@@ -1,26 +1,42 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from "../styles/header.module.css"
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
-        <Image src="/img/logo.svg" width={300} height={40} alt='imagen logotipo' />
+        <Link href={'/'} >
+          <Image src="/img/logo.svg" width={300} height={40} alt='imagen logotipo' />
+        </Link>
         <nav className={styles.navegacion}>
-          <Link href="/">
+          <Link
+            href="/"
+            className={router.pathname === '/' ? styles.active : ''}
+          >
             Inicio
           </Link>
-          <Link href="/nosotros">
-            Nosotros
+          <Link
+            href="/nosotros"
+            className={router.pathname === '/nosotros' ? styles.active : ''}
+          >
+            Nosotros          
           </Link>
-          <Link href="/blog">
+          <Link
+            href="/blog"
+            className={router.pathname === '/blog' ? styles.active : ''}
+          >
             Blog
           </Link>
-          <Link href="/tienda">
-            Tienda
+          <Link
+            href="/tienda"
+            className={router.pathname === '/tienda' ? styles.active : ''}
+          >
+            Tienda            
           </Link>
-
         </nav>
       </div>
     </header>    
