467681eaa7ca44327864fc1a47ff6237046ed621
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-21 09:47:36 -0300

    07_guitarla_next-routing dinámico con getStaticPaths y getStaticProps

--- 07_guitarla_next/pages/guitarras/[url].js
+++ 07_guitarla_next/pages/guitarras/[url].js
@@ -5,14 +5,31 @@ export default function Producto({ guitarra }) {
   )
 }
 
-export async function getServerSideProps({ query: { url } }) {
-  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+export async function getStaticPaths() {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`);
+  const { data } = await respuesta.json();  
+
+  const paths = data.map( guitarra => ({
+    params: {
+      url: guitarra.attributes.url
+    }
+  }))
+
+  console.log(paths)
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params: { url } }) {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);  
   const { data : guitarra } = await respuesta.json();
   
   return {
     props: {
       guitarra
     }
-
   }
 }
