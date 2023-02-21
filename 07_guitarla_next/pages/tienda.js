47aef95978008365db922314faba66762047f82c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-21 08:22:09 -0300

    07_guitarla_next-consultar la api con getServerSideProps

--- 07_guitarla_next/pages/tienda.js
+++ 07_guitarla_next/pages/tienda.js
@@ -2,8 +2,7 @@ import Layout from '@/components/layout'
 import ListadoGuitarras from '@/components/listado-guitarras'
 
 export default function Tienda({ guitarras }) {
-  console.log(guitarras);
-  
+
   return (
     <>
       <Layout
@@ -13,7 +12,7 @@ export default function Tienda({ guitarras }) {
         <main className="contenedor">
           <h1 className="heading">Nuestra colección</h1>
           <ListadoGuitarras
-
+            guitarras={guitarras}
           />
         </main>
       </Layout>
@@ -21,15 +20,13 @@ export default function Tienda({ guitarras }) {
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
   const { data : guitarras } = await respuesta.json();
 
   return {
     props: {
       guitarras
-
-
     }
   }
 }
