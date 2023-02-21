e03f1a75f7c1ca60a0f16a25916e5caf11335c40
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-21 09:15:23 -0300

    07_guitarla_next-routing dinámico con getServerSideProps

--- 07_guitarla_next/pages/guitarras/[url].js
+++ 07_guitarla_next/pages/guitarras/[url].js
@@ -1,5 +1,18 @@
-export default function Producto() {
+export default function Producto({ guitarra }) {
+  console.log(guitarra[0].attributes.nombre);
   return (
     <div>url</div>
   )
+}
+
+export async function getServerSideProps({ query: { url } }) {
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+  const { data : guitarra } = await respuesta.json();
+  
+  return {
+    props: {
+      guitarra
+    }
+
+  }
 }
