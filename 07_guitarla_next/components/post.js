a1d759bf8cebc3c3fb36e586fc79e71351b1a1df
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-22 21:00:09 -0300

    07_guitarla_next-css al blog y función helper de fecha

--- 07_guitarla_next/components/post.js
+++ 07_guitarla_next/components/post.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/blog.module.css";
+import { formatearFecha } from "@/utils/helpers";
 
 export default function Post({post}) {
   const { contenido, imagen, titulo, url, publishedAt } = post;
@@ -7,11 +9,14 @@ export default function Post({post}) {
   return (
     <article>
       <Image src={imagen.data.attributes.formats.medium.url} width="600" height="400" style={{ width: 'auto', height: 'auto' }} alt={`imagen blog ${titulo}`} />
-      <div>
+      <div className={styles.contenido}>
         <h3>{titulo}</h3>
-        <p>{publishedAt}</p>
-        <p>{contenido}</p>
-        <Link href={`/blog/${url}`}>Leer Post</Link>
+        <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
+        <p className={styles.resumen}>{contenido}</p>
+        <Link
+          className={styles.enlace}
+          href={`/blog/${url}`}
+        >Leer Post</Link>
       </div>
     </article>
   )
--- 07_guitarla_next/pages/blog.js
+++ 07_guitarla_next/pages/blog.js
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import Post from '@/components/post';
+import styles from '../styles/grid.module.css'
 
 export default function Blog({posts}) {
 
@@ -11,7 +12,7 @@ export default function Blog({posts}) {
       >
         <main className="contenedor">
           <h1 className="heading">Blog</h1>
-          <div>
+          <div className={styles.grid}>
             {posts?.map(post => (
               <Post 
                 key={post.id}
