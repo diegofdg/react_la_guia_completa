75c5a0cbedc322f3840c8b1778022edbc441b474
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-23 19:39:26 -0300

    07_guitarla_next-mostrando el contenido del carrito

--- 07_guitarla_next/pages/carrito.js
+++ 07_guitarla_next/pages/carrito.js
@@ -1,7 +1,8 @@
+import Image from "next/image"
 import Layout from "@/components/layout"
 import styles from "../styles/carrito.module.css"
 
-export default function Carrito() {
+export default function Carrito({ carrito }) {
   return (
     <Layout
       title="Carrito de Compras"
@@ -11,6 +12,25 @@ export default function Carrito() {
         <div className={styles.contenido}>
           <div className={styles.carrito}>
             <h2>Articulos</h2>
+            {carrito.length === 0 ? 'Carrito Vacío' : (
+              carrito.map(producto => (
+                <div
+                  key={producto.id}
+                  className={styles.producto}
+                >
+                  <div>
+                    <Image width={250} height={480} src={producto.imagen} alt={producto.nombre} />
+                  </div>
+                  <div>
+                    <p className={styles.nombre}>{producto.nombre}</p>
+                    <p className={styles.precio}>$<span>{producto.precio}</span></p>
+                    <p className={styles.subtotal}>Subtotal: $<span>{producto.cantidad * producto.precio}</span></p>
+                  </div>
+
+                </div>
+              ))
+            )}
+
           </div>
           <aside className={styles.resumen}>
             <h3>Resumen del Pedido</h3>
