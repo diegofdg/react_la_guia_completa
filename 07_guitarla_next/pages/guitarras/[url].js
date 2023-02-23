09c59420adb4f2426406c0783efab3e21dfa2e90
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-23 19:04:40 -0300

    07_guitarla_next-agregando un botón para añadir al carrito

--- 07_guitarla_next/pages/guitarras/[url].js
+++ 07_guitarla_next/pages/guitarras/[url].js
@@ -15,6 +15,25 @@ export default function Producto({ guitarra }) {
           <h3>{nombre}</h3>
           <p className={styles.descripcion}>{descripcion}</p>
           <p className={styles.precio}>$ {precio}</p>
+          <form
+            className={styles.formulario}
+          >
+            <label htmlFor="cantidad">Cantidad</label>
+            <select
+              id="cantidad"
+            >
+              <option value="0">--Seleccione--</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input
+              type="submit"
+              value="Agregar al carrito"
+            />
+          </form>
         </div>
       </div>
     </Layout>
