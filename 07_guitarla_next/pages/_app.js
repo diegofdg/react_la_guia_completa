3f2d06cc61d92e235c03368942c8895176f226a2
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-02-23 20:34:08 -0300

    07_guitarla_next-react 18, localstorage, doble render y next.js

--- 07_guitarla_next/pages/_app.js
+++ 07_guitarla_next/pages/_app.js
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  const [ carrito, setCarrito ] = useState([]);
+  const carritoLS = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('carrito')) ?? [] : [];
+  const [ carrito, setCarrito ] = useState(carritoLS);
+  useEffect(() => {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }, [carrito])
 
   const agregarCarrito = guitarra => {
     // Comprobar si la guitarra ya esta en el carrito...
