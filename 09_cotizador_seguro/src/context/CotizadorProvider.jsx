d292b78f521e188331159bc224fcc930a5b9208c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-04 17:21:47 -0300

    09_cotizador_seguro-trabajando con el algoritmo de cotizacion (parte 1 de 2)

--- 09_cotizador_seguro/src/context/CotizadorProvider.jsx
+++ 09_cotizador_seguro/src/context/CotizadorProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from 'react';
+import { obtenerDiferenciaYear } from '../helpers';
 
 const CotizadorContext = createContext();
 
@@ -21,10 +22,13 @@ const CotizadorProvider = ({children}) => {
 
   const cotizarSeguro = () => {
     // Una base
+    let resultado = 2000;
 
     // Obtener diferencia de años
+    const diferencia = obtenerDiferenciaYear(datos.year)
 
     // Hay que restar el 3% por año
+    resultado -= ((diferencia * 3 ) * resultado) / 100;
 
     // Americano 15% 
     // Europeo 30%
