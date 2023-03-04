cfc67e2f6b9ef25ff676c3b19d3c9ad9440be52b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-04 19:17:50 -0300

    09_cotizador_seguro-finalizando el componente resultado

--- 09_cotizador_seguro/src/components/Resultado.jsx
+++ 09_cotizador_seguro/src/components/Resultado.jsx
@@ -5,7 +5,8 @@ const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const { marca, plan, year } = datos;
 
-  const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+  const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca));
+  const [nombrePlan] = PLANES.filter(p => p.id === Number(plan));
 
   if(resultado === 0) return null;
 
@@ -17,6 +18,18 @@ const Resultado = () => {
       <p className="my-2">
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año del Auto: </span>
+        {year}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización: </span>
+        {resultado}
       </p>
     </div>
   )
