0cc7c16f06f9a0c983580403f772ee46d7155d5a
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-04 19:12:12 -0300

    09_cotizador_seguro-añadiendo más información al resultado

--- 09_cotizador_seguro/src/components/Resultado.jsx
+++ 09_cotizador_seguro/src/components/Resultado.jsx
@@ -1,12 +1,24 @@
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constants";
 
 const Resultado = () => {
-  const { resultado } = useCotizador();
+  const { resultado, datos } = useCotizador();
+  const { marca, plan, year } = datos;
+
+  const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
 
   if(resultado === 0) return null;
 
   return (
-    <div>{resultado}</div>
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">
+        Resumen
+      </h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+    </div>
   )
 }
 
