31d365878557324aa4bbe0404e2e403dd370f360
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-04 07:47:16 -0300

    09_cotizador_seguro-creando el context

---
+++ 09_cotizador_seguro/src/context/CotizadorProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({children}) => {
+  
+  return (
+    <CotizadorContext.Provider
+      value={{
+        
+      }}
+    >
+      {children}
+    </CotizadorContext.Provider>
+  )
+}
+
+export {
+  CotizadorProvider
+}
+
+export default CotizadorContext;
