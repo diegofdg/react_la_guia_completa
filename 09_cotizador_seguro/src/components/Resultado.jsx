68fa2186e07c795c680b1fb94103d7ebab623893
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-04 19:29:17 -0300

    09_cotizador_seguro-cómo utilizar usecallback y useref

--- 09_cotizador_seguro/src/components/Resultado.jsx
+++ 09_cotizador_seguro/src/components/Resultado.jsx
@@ -1,12 +1,18 @@
+import { useCallback, useMemo, useRef } from "react";
 import useCotizador from "../hooks/useCotizador";
 import { MARCAS, PLANES } from "../constants";
 
 const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const { marca, plan, year } = datos;
+  const yearRef = useRef(year);
 
-  const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca));
-  const [nombrePlan] = PLANES.filter(p => p.id === Number(plan));
+  const [nombreMarca] = useCallback(
+    MARCAS.filter(m => m.id === Number(marca)), [resultado]
+  );
+  const [nombrePlan] = useCallback(
+    PLANES.filter(p => p.id === Number(plan)), [resultado]
+  );
 
   if(resultado === 0) return null;
 
@@ -25,7 +31,7 @@ const Resultado = () => {
       </p>
       <p className="my-2">
         <span className="font-bold">Año del Auto: </span>
-        {year}
+        {yearRef.current}
       </p>
       <p className="my-2 text-2xl">
         <span className="font-bold">Total Cotización: </span>
