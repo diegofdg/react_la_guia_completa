3dafc8678174add31fc88eb93d3565c2f1513957
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-05 20:48:00 -0300

    10_clima_app-consultando el clima en la api

--- 10_clima_app/src/context/ClimaProvider.jsx
+++ 10_clima_app/src/context/ClimaProvider.jsx
@@ -1,14 +1,16 @@
 import { useState, createContext } from 'react';
+import axios from 'axios';
 
 const ClimaContext = createContext();
 
 const ClimaProvider = ({children}) => {
-  console.log(import.meta.env.VITE_API_KEY)
 
   const [ busqueda, setBusqueda ] = useState({
     ciudad: '',
     pais: ''
   });
+
+  const [ resultado, setResultado ] = useState({});
 
   const datosBusqueda = e => {
     setBusqueda({
@@ -17,8 +19,22 @@ const ClimaProvider = ({children}) => {
     })
   }
 
-  const consultarClima = datos => {
-    console.log(datos);
+  const consultarClima = async datos => {
+    try {
+      const { ciudad, pais } = datos;
+      const appId = import.meta.env.VITE_API_KEY;
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`;
+      
+      const { data } = await axios(url);
+      const { lat, lon } = data[0];
+
+      const urlClima = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      const { data: clima } = await axios(urlClima);
+      setResultado(clima);
+      
+    } catch (error) {
+      console.log(error);
+    }    
   }
 
   return (
@@ -26,7 +42,8 @@ const ClimaProvider = ({children}) => {
       value={{
         busqueda,
         datosBusqueda,
-        consultarClima
+        consultarClima,
+        resultado
       }}
     >
       {children}
