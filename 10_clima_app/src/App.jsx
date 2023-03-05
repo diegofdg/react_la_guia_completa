4461ad5d560aec870b8202deca7218d1e6a5fa71
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-05 19:56:29 -0300

    10_clima_app-creando el provider

--- 10_clima_app/src/App.jsx
+++ 10_clima_app/src/App.jsx
@@ -1,7 +1,14 @@
 import AppClima from "./components/AppClima";
+import { ClimaProvider } from "./context/ClimaProvider";
+
 function App() {
   return (
-    <AppClima />    
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de Clima</h1>
+      </header>
+      <AppClima />
+    </ClimaProvider>
   )
 }
 
