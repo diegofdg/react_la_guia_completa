33805e11477f81ed89b0cb89c70a2f3f43598d51
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-05 19:46:42 -0300

    10_clima_app-creando el formulario

--- 10_clima_app/src/components/Formulario.jsx
+++ 10_clima_app/src/components/Formulario.jsx
@@ -1,6 +1,37 @@
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input
+            type="text"
+            id="ciudad"
+            name="ciudad"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+          <select
+            name="pais"
+            id="pais"
+          >
+            <option value="">-- Seleccione un país --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">Mexico</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          value="Consultar Clima"
+        />
+      </form>
+    </div>
   )
 }
 
