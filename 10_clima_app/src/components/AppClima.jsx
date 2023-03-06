ff8d78d230a19ab03bd3370b7c988c67077be45d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-06 06:11:52 -0300

    10_clima_app-accediendo y mostrando el resultado de la consulta

--- 10_clima_app/src/components/AppClima.jsx
+++ 10_clima_app/src/components/AppClima.jsx
@@ -1,10 +1,15 @@
 import Formulario from "./Formulario";
+import Resultado from "./Resultado";
+import useClima from "../hooks/useClima";
 
 const AppClima = () => {
+  const { resultado } = useClima();
+
   return (
     <>
       <main className="dos-columnas">
         <Formulario />
+        {resultado?.name && <Resultado />}
       </main>
     </>
   )
