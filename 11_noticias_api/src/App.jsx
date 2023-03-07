9581fe6d4fbec2986b2a7f362bb5f34bfd316411
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-07 04:55:29 -0300

    11_noticias_api-creando el context

--- 11_noticias_api/src/App.jsx
+++ 11_noticias_api/src/App.jsx
@@ -1,36 +1,39 @@
 import { Container, Grid, Typography } from '@mui/material';
 import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 function App() {
 
   return (
-    <Container>
-        <header>
-          <Typography
-            align="center"
-            marginY={5}
-            component="h1"
-            variant="h3"
-          >
-            Buscador de Noticias
-          </Typography>
-        </header>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
+    <NoticiasProvider>
+      <Container>
+          <header>
+            <Typography
+              align="center"
+              marginY={5}
+              component="h1"
+              variant="h3"
+            >
+              Buscador de Noticias
+            </Typography>
+          </header>
           <Grid
-            item
-            xs={12}
-            md={6}
-            lg={4}
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
           >
-            <Formulario />
+            <Grid
+              item
+              xs={12}
+              md={6}
+              lg={4}
+            >
+              <Formulario />
+            </Grid>
           </Grid>
-        </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   )
 }
 
