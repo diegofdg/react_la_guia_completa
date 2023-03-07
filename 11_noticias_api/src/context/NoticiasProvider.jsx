c2cc03d7534fce8b046e9ab70e9944e036f5846c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-07 05:11:23 -0300

    11_noticias_api-consultando las noticias según la categoría seleccionada

--- 11_noticias_api/src/context/NoticiasProvider.jsx
+++ 11_noticias_api/src/context/NoticiasProvider.jsx
@@ -1,9 +1,20 @@
 import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 
 const NoticiasContext = createContext();
 
 const NoticiasProvider = ({children}) => {
-  const [categoria, setCategoria] = useState('general');
+  const [ categoria, setCategoria ] = useState('general');
+  const [ noticias, setNoticias ] = useState([]);
+
+  useEffect(()=>{
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+    }
+    consultarApi();
+  },[categoria]);
 
   const handleChangeCategoria = e => {
     setCategoria(e.target.value);
