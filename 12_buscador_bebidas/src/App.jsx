79185730761f0071d205550377e78ec46465563c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-08 00:24:09 -0300

    12_buscador_bebidas-primeros pasos con el proyecto

---
+++ 12_buscador_bebidas/src/App.jsx
@@ -0,0 +1,17 @@
+import { Container } from 'react-bootstrap';
+
+function App() {  
+
+  return (
+    <>
+      <header className="py-5">
+        <h1>Buscador de Bebidas</h1>
+      </header>
+      <Container className='mt-5'>
+        1
+      </Container>
+    </>
+  )
+}
+
+export default App;
