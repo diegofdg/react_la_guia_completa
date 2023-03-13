61aadb337f91d9d0d49602e5335deec35e882133
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-13 07:32:56 -0300

    12_buscador_bebidas-añadiendo los datos del formulario al state y validando

--- 12_buscador_bebidas/src/components/Formulario.jsx
+++ 12_buscador_bebidas/src/components/Formulario.jsx
@@ -1,10 +1,30 @@
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Form, Row, Col, Alert } from 'react-bootstrap';
 import useCategorias from '../hooks/useCategorias';
 
 const Formulario = () => {
+  const [ busqueda, setBusqueda ] = useState({
+    nombre: '',
+    categoria: ''
+  });  
+  const [ alerta, setAlerta ] = useState('')
   const { categorias } = useCategorias();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if(Object.values(busqueda).includes('')) {
+      setAlerta('Todos los campos son obligatorios');
+      return;
+    }
+    setAlerta('');
+  }
+
   return (
-    <Form>
+    <Form
+      onSubmit={handleSubmit}
+    >
+      {alerta && <Alert variant="danger" className="text-center">{alerta}</Alert>}
       <Row>
         <Col md={6}>
           <Form.Group className="mb-3">
@@ -14,6 +34,11 @@ const Formulario = () => {
               type="text"
               placeholder="Ej: Tequila, Vodka, etc"
               name="nombre"
+              value={busqueda.nombre}
+              onChange={e => setBusqueda({
+                ...busqueda,
+                [e.target.name] : e.target.value
+              })}
             />
           </Form.Group>
         </Col>
@@ -23,6 +48,11 @@ const Formulario = () => {
               <Form.Select
                 id="categoria"
                 name="categoria"
+                value={busqueda.categoria}
+                onChange={e => setBusqueda({
+                  ...busqueda,
+                  [e.target.name] : e.target.value
+                })}
               >
                 <option>-- Selecciona categoría --</option>
                 {categorias.map(categoria => (
@@ -41,6 +71,7 @@ const Formulario = () => {
           <Button
             variant="danger"
             className="text-uppercase w-100"
+            type="submit"
           >
             Buscar Bebidas
           </Button>
