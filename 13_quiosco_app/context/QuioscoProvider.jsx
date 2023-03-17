41534334c251a4e6f8e77de416f6c0a6535da5f9
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-17 05:42:56 -0300

    13_quiosco_app-obtener las categorías de la api

--- 13_quiosco_app/context/QuioscoProvider.jsx
+++ 13_quiosco_app/context/QuioscoProvider.jsx
@@ -1,12 +1,24 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({children}) => {
+  const [ categorias, setCategorias ] = useState([]);
+
+  const obtenerCategorias = async () => {
+    const { data } = await axios('/api/categorias');
+    setCategorias(data);
+  }
+
+  useEffect(()=>{
+    obtenerCategorias();
+  },[]);
+
   return (
     <QuioscoContext.Provider
       value={{
-
+        categorias
       }}
     >
       {children}
