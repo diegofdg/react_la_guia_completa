f70550c1a050059bbf3ccdcf0e40b3ee6ae4ccb0
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-17 05:28:54 -0300

    13_quiosco_app-creando el provider para next.js

---
+++ 13_quiosco_app/context/QuioscoProvider.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect, createContext } from "react";
+
+const QuioscoContext = createContext();
+
+const QuioscoProvider = ({children}) => {
+  return (
+    <QuioscoContext.Provider
+      value={{
+
+      }}
+    >
+      {children}
+    </QuioscoContext.Provider>
+  )
+}
+
+export {
+  QuioscoProvider
+}
+
+export default QuioscoContext;
--- 13_quiosco_app/pages/_app.js
+++ 13_quiosco_app/pages/_app.js
@@ -1,5 +1,10 @@
+import { QuioscoProvider } from '@/context/QuioscoProvider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QuioscoProvider>
+      <Component {...pageProps} />
+    </QuioscoProvider>
+  )
 }
