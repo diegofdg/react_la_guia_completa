e90f97f1de82aa01db1f825836305ed43fa0bf4e
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-18 06:43:09 -0300

    13_quiosco_app-eager loading in prisma

--- 13_quiosco_app/pages/api/productos.js
+++ 13_quiosco_app/pages/api/productos.js
@@ -3,10 +3,10 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const categorias = await prisma.categoria.findMany({
-    include: {
-      productos: true
+  const productos = await prisma.producto.findMany({
+    where: {
+      categoriaId: 1
     }
   });
-  res.status(200).json(categorias); 
+  res.status(200).json(productos); 
 }
