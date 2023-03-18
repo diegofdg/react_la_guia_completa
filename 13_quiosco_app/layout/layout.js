57675b0a5e8362a3657f0e0f519ca1d22e38bf0f
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-18 07:25:20 -0300

    13_quiosco_app-instalando react-modal

--- 13_quiosco_app/layout/layout.js
+++ 13_quiosco_app/layout/layout.js
@@ -1,7 +1,23 @@
 import Head from "next/head"
+import Modal from "react-modal"
 import Sidebar from "@/components/Sidebar"
+import useQuiosco from "@/hooks/useQuiosco"
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transition: "translate(-50%, -50%)"
+  }
+}
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useQuiosco();
   return (
     <>
       <Head>
@@ -18,6 +34,14 @@ export default function Layout({ children, pagina }) {
           </div>
         </main>
       </div>
+      {modal && (
+        <Modal
+          isOpen={modal}
+          style={customStyles}
+        >
+          <h1>Modal</h1>
+        </Modal>
+      )}
     </>
   )
 }
