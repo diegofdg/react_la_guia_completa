e46407c9fefdf700c72b701ed4e8a696c4dbce68
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-21 06:09:54 -0300

    13_quiosco_app-evitar que el usuario pueda enviar el formulario si no está completo el pedido

--- 13_quiosco_app/pages/total.js
+++ 13_quiosco_app/pages/total.js
@@ -1,13 +1,33 @@
+import useQuiosco from "@/hooks/useQuiosco";
 import Layout from "@/layout/layout"
+import { useEffect, useCallback } from "react";
 
 export default function Total() {
+  const { pedido } = useQuiosco();
+
+  const comprobarPedido = useCallback(() => {
+    return pedido.length === 0;
+  }, [pedido]);
+
+  useEffect(()=>{
+    comprobarPedido();
+  }, [pedido, comprobarPedido]);
+
+  const colocarOrden = e => {
+    e.preventDefault();
+    console.log('colocando')
+
+  }
+
   return (
     <Layout
       pagina={'Total y Confirmar Pedido'}
     >
       <h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
       <p className="text-2xl my-10">Confirma tu Pedido a Continuación</p>
-      <form>
+      <form
+        onSubmit={colocarOrden}
+      >
         <div>
           <label
             htmlFor="nombre"
@@ -29,10 +49,10 @@ export default function Total() {
         <div className="mt-5">
           <input
             type="submit"
-            className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center"
+            className={`${comprobarPedido() ? 'bg-indigo-100' : 'bg-indigo-600 hover:bg-indigo-800'} w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center`}
             value="Confirmar Pedido"
+            disabled={comprobarPedido()}
           />
-
         </div>
       </form>
     </Layout>   
