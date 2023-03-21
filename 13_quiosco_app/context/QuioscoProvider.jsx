284d92e7a1510161ffa094ed4fa69b3995e6bcbb
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-21 06:57:04 -0300

    13_quiosco_app-reiniciar app una vez que el pedido ha sido completado

--- 13_quiosco_app/context/QuioscoProvider.jsx
+++ 13_quiosco_app/context/QuioscoProvider.jsx
@@ -75,8 +75,19 @@ const QuioscoProvider = ({children}) => {
   const colocarOrden = async e => {
     e.preventDefault();
     try {
-      const { data } = await axios.post('/api/ordenes', {pedido, nombre, total, fecha: Date.now().toString()});
-      console.log(data);
+      await axios.post('/api/ordenes', {pedido, nombre, total, fecha: Date.now().toString()});
+      
+      // Resetear la app
+      setCategoriaActual(categorias[0]);
+      setPedido([]);
+      setNombre('');
+      setTotal(0);
+
+      toast.success('Pedido realizado correctamente');
+      setTimeout(()=>{
+        router.push('/');
+      },3000);
+
     } catch (error) {
       console.log(error);
     }
