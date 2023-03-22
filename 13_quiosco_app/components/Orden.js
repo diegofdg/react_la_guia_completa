367e76cee3c409bdd18b56bdb7b876a4e6123c42
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-22 19:29:06 -0300

    13_quiosco_app-mostrando el resto de información de las ordenes

--- 13_quiosco_app/components/Orden.js
+++ 13_quiosco_app/components/Orden.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { formatearDinero } from "@/helpers";
+
 export default function Orden({orden}) {
   const { id, nombre, total, pedido } = orden;
 
@@ -6,7 +9,32 @@ export default function Orden({orden}) {
       <h3 className="text-2xl font-bold">Orden: {id}</h3>
       <p className="text-lg font-bold">CLiente: {nombre}</p>
       <div>
-        
+        {pedido.map(platillo => (
+          <div
+            key={platillo.id}
+            className="py-3 flex border-b last-of-type:border-0 items-center"
+          >
+            <div className="w-32">
+              <Image
+                src={`/assets/img/${platillo.imagen}.jpg`}
+                alt={`imagen platillo ${platillo.nombre}`}
+                width={400}
+                height={500}
+                style={{ width: 'auto', height: 'auto' }}
+                priority={true}
+              />
+            </div>
+            <div className="p-5 space-y-2">
+              <h4 className="text-xl font-bold text-amber-500">{platillo.nombre}</h4>
+              <p className="text-lg font-bold">Cantidad: {platillo.cantidad}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="md:flex md:items-center md:justify-between my-10">
+        <p className="mt-5 font-black text-4xl text-amber-500">
+          Total a Pagar: {formatearDinero(total)}
+        </p>
       </div>
     </div>
   )
