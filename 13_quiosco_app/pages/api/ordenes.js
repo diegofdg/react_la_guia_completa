4df2214fe57bf680c6f73fee24426bff88723e17
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-22 07:06:15 -0300

    13_quiosco_app-creando un endpoint para obtener las ordenes

--- 13_quiosco_app/pages/api/ordenes.js
+++ 13_quiosco_app/pages/api/ordenes.js
@@ -3,6 +3,15 @@ import { PrismaClient } from "@prisma/client";
 export default async function handler(req, res) {
   const prisma = new PrismaClient();
 
+  // Obtener ordenes
+  const ordenes = await prisma.orden.findMany({
+    where: {
+      estado: false
+    }
+  })
+  res.status(200).json(ordenes);
+
+  // Crear ordenes
   if(req.method === 'POST') {
     const orden = await prisma.orden.create({
       data: {
@@ -13,6 +22,6 @@ export default async function handler(req, res) {
       }
     })
     
-    res.json(orden);
+    res.status(200).json(orden);
   }
 }
