b6cb713d744c9aba925b0048f8b7e6af016dce79
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-23 18:37:39 -0300

    14_uptask_mern-previniendo usuarios duplicados

--- 14_uptask_mern/backend/controllers/usuarioController.js
+++ 14_uptask_mern/backend/controllers/usuarioController.js
@@ -1,6 +1,15 @@
 import Usuario from "../models/Usuario.js";
 
 const registrar = async (req,res) => {
+  // Evitar registros duplicados
+  const { email } = req.body;
+  const existeUsuario = await Usuario.findOne({ email });
+  if(existeUsuario) {
+     const error = new Error('Usuario ya registrado');
+     return res.status(400).json({ msg: error.message });
+  }
+
+
   try {
     const usuario = new Usuario(req.body);
     const usuarioAlmacenado = await usuario.save();
