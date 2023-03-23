e01054d74842c7becf4f1ffeff1f1d411eb12df6
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-23 06:53:00 -0300

    14_uptask_mern-ocultando la conexión de la bd en variables de entorno

--- 14_uptask_mern/backend/config/db.js
+++ 14_uptask_mern/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const connection = await mongoose.connect('mongodb+srv://usuario:password@cluster0.rofwoii.mongodb.net/uptask?retryWrites=true&w=majority',{
+    const connection = await mongoose.connect(process.env.MONGO_URI,{
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
--- 14_uptask_mern/backend/index.js
+++ 14_uptask_mern/backend/index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 
 const app = express();
 
+dotenv.config();
+
 conectarDB();
 
-app.listen(4000, () => {
-  console.log("Servidor corriendo en el puerto 4000");
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
