7fdd6a56363cac8140ba52875b3f1e8c418fd969
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-23 16:05:47 -0300

    14_uptask_mern-introduciendo controladores y cómo se comunican routes y controllers

---
+++ 14_uptask_mern/backend/controllers/usuarioController.js
@@ -0,0 +1,7 @@
+const registrar = (req,res) => { 
+  res.json({msg: "Registrando nuevo usuario"});
+}
+
+export {
+  registrar
+}
--- 14_uptask_mern/backend/routes/usuarioRoutes.js
+++ 14_uptask_mern/backend/routes/usuarioRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
+import { registrar } from "../controllers/usuarioController.js";
+
 const router = express.Router();
 
-router.get('/', (req,res) => {
-  res.send('Desde api/usuarios')
-});
-
-router.post('/', (req,res) => {
-  res.send('Usando POST en api/usuarios')
-});
+// Autenticación, Registro y Confirmación de Usuarios
+router.post('/registrar', registrar); 
 
 export default router;
