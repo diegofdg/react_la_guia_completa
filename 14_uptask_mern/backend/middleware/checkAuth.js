d984e7121aa74f149b6c94701c8b55957052d9f0
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-24 17:54:06 -0300

    14_uptask_mern-revisando que el usuario esté autenticado para acceder a un endpoint

--- 14_uptask_mern/backend/middleware/checkAuth.js
+++ 14_uptask_mern/backend/middleware/checkAuth.js
@@ -1,5 +1,24 @@
-const checkAuth = (req,res,next) => {
-  console.log('desde checkAuth');
+import jwt from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
+
+const checkAuth = async (req,res,next) => {
+  let token;
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.usuario = await Usuario.findById(decoded.id).select("-password -confirmado -token -createdAt -updatedAt -__v");
+      console.log(req.usuario)
+      return next();
+    } catch (error) {
+      return res.status(404).json({ msg: error.message });   
+    }
+  }
+
+  if(!token) {
+    const error = new Error('Token no válido');
+    res.status(401).json({ msg: error.message });
+  }
   next();
 }
 
