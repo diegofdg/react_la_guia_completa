14cdcc693845f4d1bec57a156f91427a20293478
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-25 10:54:56 -0300

    14_uptask_mern-creando el modelo de tareas

---
+++ 14_uptask_mern/backend/models/Tarea.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const tareaSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    descripcion: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    estado: {
+      type: Boolean,
+      default: false
+    },
+    fechaEntrega: {
+      type: Date,
+      required: true,
+      default: Date.now()
+    },
+    prioridad: {
+      type: String,
+      required: true,
+      enum: ['Baja', 'Media', 'Alta']
+    },
+    proyecto: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Proyecto'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Tarea = mongoose.model("Proyecto", tareaSchema);
+
+export default Tarea;
