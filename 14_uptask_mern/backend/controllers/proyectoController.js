d5f82e14b1767ff0bd3243df16628ef268fa2e6e
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-25 10:38:30 -0300

    14_uptask_mern-obtener y validar un proyecto por su id

--- 14_uptask_mern/backend/controllers/proyectoController.js
+++ 14_uptask_mern/backend/controllers/proyectoController.js
@@ -16,7 +16,24 @@ const nuevoProyecto = async (req,res) => {
   }  
 }
 
-const obtenerProyecto = async (req,res) => {}
+const obtenerProyecto = async (req,res) => {
+  const { id } = req.params;
+  try {
+    const proyecto = await Proyecto.findById(id);
+    if(!proyecto) {
+      const error = new Error('No Encontrado')
+      return res.status(404).json({ msg: error.message });
+    }
+    if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error('Acción No Válida')
+      return res.status(401).json({ msg: error.message });
+    }
+    res.json(proyecto);
+    
+  } catch (error) {
+    console.log(error);    
+  }  
+}
 
 const editarProyecto = async (req,res) => {}
 
