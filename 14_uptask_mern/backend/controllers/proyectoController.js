48def93eab34e627c2e71aa6626f9f7516367a55
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-25 10:46:18 -0300

    14_uptask_mern-actualizar un proyecto

--- 14_uptask_mern/backend/controllers/proyectoController.js
+++ 14_uptask_mern/backend/controllers/proyectoController.js
@@ -35,7 +35,31 @@ const obtenerProyecto = async (req,res) => {
   }  
 }
 
-const editarProyecto = async (req,res) => {}
+const editarProyecto = async (req,res) => {
+  const { id } = req.params;
+  try {
+    const proyecto = await Proyecto.findById(id);
+    if(!proyecto) {
+      const error = new Error('No Encontrado')
+      return res.status(404).json({ msg: error.message });
+    }
+    if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error('Acción No Válida')
+      return res.status(401).json({ msg: error.message });
+    }
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+    proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+    
+  } catch (error) {
+    console.log(error);    
+  }  
+  
+}
 
 const eliminarProyecto = async (req,res) => {}
 
