921b43f2c1a4c507c2136535863708694e4a1d0e
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-25 10:50:04 -0300

    14_uptask_mern-eliminar un proyecto

--- 14_uptask_mern/backend/controllers/proyectoController.js
+++ 14_uptask_mern/backend/controllers/proyectoController.js
@@ -57,11 +57,27 @@ const editarProyecto = async (req,res) => {
     
   } catch (error) {
     console.log(error);    
-  }  
-  
+  }
 }
 
-const eliminarProyecto = async (req,res) => {}
+const eliminarProyecto = async (req,res) => {
+  const { id } = req.params;
+  try {
+    const proyecto = await Proyecto.findById(id);
+    if(!proyecto) {
+      const error = new Error('No Encontrado')
+      return res.status(404).json({ msg: error.message });
+    }
+    if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+      const error = new Error('Acción No Válida')
+      return res.status(401).json({ msg: error.message });
+    }
+    await proyecto.deleteOne();
+    res.json({ msg: 'Proyecto Eliminado'});    
+  } catch (error) {
+    console.log(error);    
+  }
+}
 
 const agregarColaborador = async (req,res) => {}
 
