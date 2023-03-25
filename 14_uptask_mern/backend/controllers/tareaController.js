405553aa24c21e27b3a54520339f2dcd2904c220
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-25 11:42:21 -0300

    14_uptask_mern-obtener una tarea y validación

--- 14_uptask_mern/backend/controllers/tareaController.js
+++ 14_uptask_mern/backend/controllers/tareaController.js
@@ -11,7 +11,7 @@ const agregarTarea = async (req,res) => {
   }
   if(existeProyecto.creador.toString() !== req.usuario._id.toString()) {
     const error = new Error('No tienes los permisos para añadir tareas');
-    return res.status(401).json({ msg: error.message });
+    return res.status(403).json({ msg: error.message });
   }
 
   try {
@@ -22,7 +22,19 @@ const agregarTarea = async (req,res) => {
   }
 }
 
-const obtenerTarea = async (req,res) => {}
+const obtenerTarea = async (req,res) => {
+  const { id } = req.params;
+  const tarea = await Tarea.findById(id).populate('proyecto');
+  if(!tarea) {
+    const error = new Error('Tarea No Encontrada');
+    return res.status(404).json({ msg: error.message });
+  }
+  if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error('Acción No Válida');
+    return res.status(403).json({ msg: error.message });
+  }
+  res.json(tarea);
+}
 
 const actualizarTarea = async (req,res) => {}
 
