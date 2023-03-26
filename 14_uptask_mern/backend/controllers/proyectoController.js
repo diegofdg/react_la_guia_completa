7c0a08e03a8c4c6a5539b179ac198d519a59384f
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-26 06:57:48 -0300

    14_uptask_mern-obtener tareas

--- 14_uptask_mern/backend/controllers/proyectoController.js
+++ 14_uptask_mern/backend/controllers/proyectoController.js
@@ -1,4 +1,5 @@
 import Proyecto from '../models/Proyecto.js';
+import Tarea from '../models/Tarea.js';
 
 const obtenerProyectos = async (req,res) => {
   const proyectos = await Proyecto.find().where('creador').equals(req.usuario);
@@ -83,7 +84,19 @@ const agregarColaborador = async (req,res) => {}
 
 const eliminarColaborador = async (req,res) => {}
 
-const obtenerTareas = async (req,res) => {}
+const obtenerTareas = async (req,res) => {
+  const { id } = req.params;
+  const existeProyecto = await Proyecto.findById(id);
+  if(!existeProyecto) {
+    const error = new Error('No Encontrado')
+    return res.status(404).json({ msg: error.message });
+  }
+
+  // Tienes que ser el creador del proyecto o colaborador
+
+  const tareas = await Tarea.find().where('proyecto').equals(id);
+  res.json(tareas);
+}
 
 export {
   obtenerProyectos,
