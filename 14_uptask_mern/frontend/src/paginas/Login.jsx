9950683aeb0bd6ce86bc76d4279ffb9060887d92
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-28 06:45:27 -0300

    14_uptask_mern-creando el formulario de autenticación

--- 14_uptask_mern/frontend/src/paginas/Login.jsx
+++ 14_uptask_mern/frontend/src/paginas/Login.jsx
@@ -1,6 +1,45 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Inicia sesión y administra tus {' '}<span className="text-slate-700">proyectos</span>
+      </h1>
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
   )
 }
 
