791d3c2807993b3526edc164f01fd759957451dc
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-28 20:05:43 -0300

    14_uptask_mern-validando el formulario

--- 14_uptask_mern/frontend/src/paginas/Registrar.jsx
+++ 14_uptask_mern/frontend/src/paginas/Registrar.jsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 const Registrar = () => {
   const [ nombre, setNombre ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ repetirPassword, setRepetirPassword ] = useState('');
+  const [ alerta, setAlerta ] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if([nombre, email, password, repetirPassword].includes('')) {
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+      return;
+    }
+  }
+
+  const { msg } = alerta;
 
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Crea tu cuenta y administra tus {' '}<span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+      { msg && <Alerta alerta={alerta}/> }
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="my-5">
           <label
             htmlFor="nombre"
