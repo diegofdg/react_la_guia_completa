1d85bf5b386480c707aef14d8b823c0ccf8fdc48
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-28 06:50:15 -0300

    14_uptask_mern-añadiendo routing a las otras páginas del área pública

--- 14_uptask_mern/frontend/src/paginas/Login.jsx
+++ 14_uptask_mern/frontend/src/paginas/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <>
@@ -39,6 +41,20 @@ const Login = () => {
           className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
         />
       </form>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/registrar"
+        >
+          ¿No tienes una cuenta? Regístrate
+        </Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/olvide-password"
+        >
+          Olvide mi password
+        </Link>
+      </nav>
     </>
   )
 }
