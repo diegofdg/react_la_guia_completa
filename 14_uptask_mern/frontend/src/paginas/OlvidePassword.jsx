91ef7589eb3ece31809a6e55509886fa2e368f14
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-30 05:49:07 -0300

    14_uptask_mern-primeros pasos con reestablecer password

--- 14_uptask_mern/frontend/src/paginas/OlvidePassword.jsx
+++ 14_uptask_mern/frontend/src/paginas/OlvidePassword.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 const OlvidePassword = () => {
+  const [ email, setEmail ] = useState('');
+  const [ alerta, setAlerta ] = useState({});
+  
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if(email === '' || email.length < 6) {
+      setAlerta({
+        msg: 'El email es obligatorio',
+        error: true
+      });
+      return;
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Recupera tu acceso y no pierdas tus {' '}<span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">      
+      { msg && <Alerta alerta={alerta} />}
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg p-10"
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -19,6 +41,8 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
           />
         </div>
         
