9c6fc22da2a71c9b39f3d5139176054cbeadc8a7
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-30 06:34:40 -0300

    14_uptask_mern-validando el token para resetear el password

--- 14_uptask_mern/frontend/src/paginas/NuevoPassword.jsx
+++ 14_uptask_mern/frontend/src/paginas/NuevoPassword.jsx
@@ -1,30 +1,60 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const NuevoPassword = () => {
+  const [ tokenValido, setTokenValido ] = useState(false);
+  const [ alerta, setAlerta ] = useState({});
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(()=>{
+    const comprobarToken = async () => {
+      try {
+        await axios(`http://localhost:4000/api/usuarios/olvide-password/${token}`);
+        setTokenValido(true);
+      } catch (error) {
+        setAlerta({
+          msg: error.response.data.msg,
+          error: true
+        });        
+      }
+    }
+    comprobarToken();
+  },[]);
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Restablece tu password y no pierdas acceso a tus {' '}<span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            htmlFor="password"
-            className="uppercase text-gray-600 block text-xl font-bold"
-          >
-            Nuevo Password
-          </label>
+      { msg && <Alerta alerta={alerta} />}
+      { tokenValido && (
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+          <div className="my-5">
+            <label
+              htmlFor="password"
+              className="uppercase text-gray-600 block text-xl font-bold"
+            >
+              Nuevo Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Escribe tu nuevo Password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            />
+          </div>        
           <input
-            id="password"
-            type="password"
-            placeholder="Escribe tu nuevo Password"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            type="submit"
+            value="Guardar Nuevo Password"
+            className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
           />
-        </div>        
-        <input
-          type="submit"
-          value="Guardar Nuevo Password"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
-        />
-      </form>
+        </form>
+      )}
     </>
   )
 }
