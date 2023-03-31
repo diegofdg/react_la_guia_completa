00c3dccffa528ff88e786003b55e6315ecec7cc2
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-31 05:14:00 -0300

    14_uptask_mern-validando la autenticación

--- 14_uptask_mern/frontend/src/paginas/Login.jsx
+++ 14_uptask_mern/frontend/src/paginas/Login.jsx
@@ -1,12 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
 
 const Login = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ alerta, setAlerta ] = useState({});
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    if([email, password].includes('')){
+      setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      });
+      return;
+    }
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Inicia sesión y administra tus {' '}<span className="text-slate-700">proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg p-10">
+
+      { msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -19,6 +50,8 @@ const Login = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -33,6 +66,8 @@ const Login = () => {
             type="password"
             placeholder="Password de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={password}
+            onChange={ e => setPassword(e.target.value)}
           />
         </div>
         <input
