93cbb6637de2aeecbe3f2d1b18c4945e1b85dbbe
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-31 06:00:33 -0300

    14_uptask_mern-creando el state para la autenticación

--- 14_uptask_mern/frontend/src/context/AuthProvider.jsx
+++ 14_uptask_mern/frontend/src/context/AuthProvider.jsx
@@ -3,10 +3,12 @@ import { useState, useEffect, createContext } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [ auth, setAuth ] = useState({});
+
   return (
     <AuthContext.Provider
       value={{
-
+        setAuth
       }}
     >
       {children}
