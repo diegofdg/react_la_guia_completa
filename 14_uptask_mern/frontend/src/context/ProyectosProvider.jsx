e43a2de459c1f06d6879311b67abd1774b2fcfe5
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-31 07:04:18 -0300

    14_uptask_mern-creando un hook para el context de proyectos

--- 14_uptask_mern/frontend/src/context/ProyectosProvider.jsx
+++ 14_uptask_mern/frontend/src/context/ProyectosProvider.jsx
@@ -4,10 +4,11 @@ import clienteAxios from "../config/clienteAxios";
 const ProyectosContext = createContext();
 
 const ProyectosProvider = ({ children }) => {
+  const [ proyectos, SetProyectos ] = useState([]);
   return (
     <ProyectosContext.Provider
       value={{
-
+        proyectos
       }}
     >
       {children}
--- 14_uptask_mern/frontend/src/paginas/Proyectos.jsx
+++ 14_uptask_mern/frontend/src/paginas/Proyectos.jsx
@@ -1,4 +1,8 @@
+import useProyectos from "../hooks/useProyectos";
+
 const Proyectos = () => {
+  const { proyectos } = useProyectos();
+  
   return (
     <>
       <h1 className="text-4xl font-black">Proyectos</h1>
