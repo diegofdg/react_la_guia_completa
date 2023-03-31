f84b91d505bd74027c89bd7a0f2ddbbc3960b59c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-31 06:31:20 -0300

    14_uptask_mern-redireccionar al usuario si está autenticado

--- 14_uptask_mern/frontend/src/context/AuthProvider.jsx
+++ 14_uptask_mern/frontend/src/context/AuthProvider.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect, createContext } from "react";
+import { useNavigate } from "react-router-dom";
 import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [ auth, setAuth ] = useState({});
+  const [ cargando, setCargando ] = useState(true);
+  const navigate = useNavigate();
+
   useEffect(()=>{
     const autenticarUsuario = async () => {
       const token = localStorage.getItem('token');
       if(!token) {
+        setCargando(false);
         return;
       }
 
@@ -22,8 +27,12 @@ const AuthProvider = ({ children }) => {
       try {
         const { data } = await clienteAxios('/usuarios/perfil', config);
         setAuth(data);
+        navigate('/proyectos');
       } catch (error) {
         console.log(error);
+        setAuth({});
+      } finally {
+        setCargando(false);
       }
     }
     autenticarUsuario();
@@ -33,7 +42,8 @@ const AuthProvider = ({ children }) => {
     <AuthContext.Provider
       value={{
         auth,
-        setAuth
+        setAuth,
+        cargando
       }}
     >
       {children}
--- 14_uptask_mern/frontend/src/layouts/RutaProtegida.jsx
+++ 14_uptask_mern/frontend/src/layouts/RutaProtegida.jsx
@@ -2,10 +2,13 @@ import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 const RutaProtegida = () => {
-  const { auth } = useAuth();
+  const { auth, cargando } = useAuth();
+  if(cargando) {
+    return 'cargando'
+  }
   return (
     <>
-      {auth._id ? 'Autenticado' : <Navigate to={"/"} />}
+      {auth._id ? <Outlet /> : <Navigate to={"/"} />}
     </>
   )
 }
