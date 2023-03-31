6aa5ef4133c39cc46369bc00515f2149c1071a1d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-03-31 06:10:47 -0300

    14_uptask_mern-revisando el token para autenticar al usuario

--- 14_uptask_mern/frontend/src/context/AuthProvider.jsx
+++ 14_uptask_mern/frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,33 @@
 import { useState, useEffect, createContext } from "react";
+import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [ auth, setAuth ] = useState({});
+  useEffect(()=>{
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem('token');
+      if(!token) {
+        return;
+      }
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      try {
+        const { data } = await clienteAxios('/usuarios/perfil', config);
+        setAuth(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    autenticarUsuario();
+  },[]);
 
   return (
     <AuthContext.Provider
