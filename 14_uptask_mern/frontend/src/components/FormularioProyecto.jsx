651d4b98483dc87bcfd3fa70146e30e09cdf0648
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-04-02 13:35:34 -0300

    14_uptask_mern-llenando un formulario con la información del proyecto

--- 14_uptask_mern/frontend/src/components/FormularioProyecto.jsx
+++ 14_uptask_mern/frontend/src/components/FormularioProyecto.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import Alerta from "./Alerta";
 
 const FormularioProyecto = () => {
+  const [ id, setId ] = useState(null);
   const [ nombre, setNombre ] = useState('');
   const [ descripcion, setDescripcion ] = useState('');
   const [ fechaEntrega, setFechaEntrega ] = useState('');
   const [ cliente, setCliente ] = useState('');
-  const { mostrarAlerta, alerta, submitProyecto } = useProyectos();
 
+  const params = useParams();
+  const { mostrarAlerta, alerta, submitProyecto, proyecto } = useProyectos();
+
+  useEffect(()=>{
+    if(params.id) {
+      setId(proyecto._id);
+      setNombre(proyecto.nombre);
+      setDescripcion(proyecto.descripcion);
+      setFechaEntrega(proyecto.fechaEntrega?.split('T')[0]);
+      setCliente(proyecto.cliente);
+    }
+  },[params]);
   const handleSubmit = async e => {
     e.preventDefault();
     if([nombre, descripcion, fechaEntrega, cliente].includes('')) {
@@ -101,7 +114,7 @@ const FormularioProyecto = () => {
       </div>
       <input
         type="submit"
-        value="Crear Proyecto"
+        value={id ? 'Actualizar Proyecto' : 'Crear Proyecto'}
         className="bg-sky-600 w-full p-3 uppercase font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors"
       />
     </form>
