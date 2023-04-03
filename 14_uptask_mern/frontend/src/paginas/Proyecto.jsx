a36ca600b0c16367923f9bb2c7b09723acffd195
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-04-03 05:51:16 -0300

    14_uptask_mern-mostrando el modal

--- 14_uptask_mern/frontend/src/paginas/Proyecto.jsx
+++ 14_uptask_mern/frontend/src/paginas/Proyecto.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import ModalFormularioTarea from "../components/ModalFormularioTareas";
@@ -6,6 +6,7 @@ import ModalFormularioTarea from "../components/ModalFormularioTareas";
 const Proyecto = () => {
   const params = useParams();
   const { obtenerProyecto, proyecto, cargando } = useProyectos();
+  const [ modal, setModal ] = useState(false);
   
   useEffect(()=>{
     obtenerProyecto(params.id);
@@ -34,6 +35,7 @@ const Proyecto = () => {
         </div>
       </div>
       <button
+        onClick={() => setModal(true)}
         type="button"
         className="text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center"
       >
@@ -43,7 +45,10 @@ const Proyecto = () => {
 
         Nueva Tarea
       </button>
-      <ModalFormularioTarea />
+      <ModalFormularioTarea
+        modal={modal}
+        setModal={setModal}
+      />
     </>
   )
 }
