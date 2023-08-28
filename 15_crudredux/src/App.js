27fd516c2566a795e16d426390ccadaff005057d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-08-28 19:05:17 -0300

    15_crudredux-creando el header

--- 15_crudredux/src/App.js
+++ 15_crudredux/src/App.js
@@ -1,6 +1,11 @@
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1>Redux</h1>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
