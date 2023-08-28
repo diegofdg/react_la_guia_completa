1c015c0c33d775b85708b354c1058174e1556336
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-08-28 20:34:24 -0300

    15_crudredux-configurando redux, instalando dev tools y creando el store

--- 15_crudredux/src/App.js
+++ 15_crudredux/src/App.js
@@ -5,18 +5,25 @@ import NuevoProducto from "./components/NuevoProducto";
 import EditarProducto from "./components/EditarProducto";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="container mt-5">
-        <Switch>
-            <Route exact path="/" component={Productos} />
-            <Route exact path="/productos/nuevo" component={NuevoProducto} />
-            <Route exact path="/productos/editar/:id" component={EditarProducto} />
-        </Switch>
-      </div>
+      <Provider store={store}>
+        <Header />
+        <div className="container mt-5">
+          <Switch>
+              <Route exact path="/" component={Productos} />
+              <Route exact path="/productos/nuevo" component={NuevoProducto} />
+              <Route exact path="/productos/editar/:id" component={EditarProducto} />
+          </Switch>
+        </div>
+      </Provider>
     </Router>
+    
   );
 }
 
