4e9e92e51c2bb161810fc57e4ef5be0d380b1d66
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-08-30 16:38:12 -0300

    15_crudredux-definiendo el action y utilizando usedispach

--- 15_crudredux/src/actions/productosActions.js
+++
@@ -1,5 +0,0 @@
-import {
-  AGREGAR_PRODUCTO,
-  AGREGAR_PRODUCTO_EXITO,
-  AGREGAR_PRODUCTO_ERROR
-} from '../types';
--- 15_crudredux/src/components/NuevoProducto.js
+++ 15_crudredux/src/components/NuevoProducto.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Actions de Redux
+import { crearNuevoProductoAction } from '../actions/productoActions';
+
 const NuevoProducto = () => {
+
+  // utilizar use dispatch y te crea una función
+  const dispatch = useDispatch();
+
+  // mandar llamar el action de productoAction
+  const agregarProducto = () => dispatch( crearNuevoProductoAction() );
+
+  // cuando el usuario haga submit
+  const submitNuevoProducto = e => {
+    e.preventDefault();
+
+    // validar formulario
+    
+    // si no hay errores
+    
+    // crear el nuevo producto
+    agregarProducto();
+
+  }
   return ( 
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -9,7 +33,9 @@ const NuevoProducto = () => {
               Agregar Nuevo Producto
             </h2>
 
-            <form>
+            <form
+              onSubmit={submitNuevoProducto}
+            >
               <div className="form-group">
                 <label>Nombre Producto</label>
                 <input
