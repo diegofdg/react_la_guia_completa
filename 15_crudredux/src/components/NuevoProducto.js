567b2e22af04bb39e61be137aab425bb38ba14c6
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-08-30 16:43:57 -0300

    15_crudredux-pasando información del formulario hacia el action

--- 15_crudredux/src/components/NuevoProducto.js
+++ 15_crudredux/src/components/NuevoProducto.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 // Actions de Redux
@@ -6,22 +6,29 @@ import { crearNuevoProductoAction } from '../actions/productoActions';
 
 const NuevoProducto = () => {
 
+  // state del componente
+  const [nombre, guardarNombre] = useState('');
+  const [precio, guardarPrecio] = useState(0);
+
   // utilizar use dispatch y te crea una función
   const dispatch = useDispatch();
 
   // mandar llamar el action de productoAction
-  const agregarProducto = () => dispatch( crearNuevoProductoAction() );
+  const agregarProducto = producto => dispatch( crearNuevoProductoAction(producto) );
 
   // cuando el usuario haga submit
   const submitNuevoProducto = e => {
     e.preventDefault();
 
     // validar formulario
+    if(nombre.trim() === '' || precio <= 0) {
+      return;
+    }
     
     // si no hay errores
     
     // crear el nuevo producto
-    agregarProducto();
+    agregarProducto({nombre, precio});
 
   }
   return ( 
@@ -43,6 +50,8 @@ const NuevoProducto = () => {
                   className="form-control"
                   placeholder="Nombre Producto"
                   name="nombre"
+                  value={nombre}
+                  onChange={e => guardarNombre(e.target.value)}
                 />
               </div>
 
@@ -53,6 +62,8 @@ const NuevoProducto = () => {
                   className="form-control"
                   placeholder="Precio Producto"
                   name="precio"
+                  value={precio}
+                  onChange={e =>  guardarPrecio( Number(e.target.value) )}
                 />
               </div>
 
