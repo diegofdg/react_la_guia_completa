9b288703d4662f8d5883d01ab08af82da4cc59a4
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-08-31 00:03:53 -0300

    15_crudredux-mostrando errores y texto de cargando...

--- 15_crudredux/src/components/Productos.js
+++ 15_crudredux/src/components/Productos.js
@@ -23,6 +23,10 @@ const Productos = () => {
     <Fragment>
       <h2 className="text-center my-5">Listado de Productos</h2>
 
+      { error ? <p className="font-weight-bold alert alert-danger text-center mt-4">Hubo un error</p> : null }
+           
+      { cargando ? <p className="text-center">Cargando....</p> : null }
+
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
