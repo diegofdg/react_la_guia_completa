aa0d06bd670a9c729311e68a8e7a9cf445c2daa4
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 20:31:47 -0300

    16_producthunt_next-agregando elementos al header

---
+++ 16_producthunt_next/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Buscar from '../ui/Buscar';
+
+const Header = () => {
+
+    return ( 
+        <header>
+          <div>
+            <div>
+              <p>P</p>
+              <Buscar />
+
+              {/* Nav aqui */}
+
+            </div>
+            <div>
+              {/* Menu de administración */}
+            </div>
+          </div>
+            
+        </header>
+     );
+}
+ 
+export default Header;
