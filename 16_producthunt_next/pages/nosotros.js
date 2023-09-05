c2eb24df8a208bc8096cd822a763c216ee431c2a
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 20:10:29 -0300

    16_producthunt_next-routing en next

---
+++ 16_producthunt_next/pages/nosotros.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/Layout';
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        <h1>Nosotros</h1>
+      </Layout>
+    </div>
+  )
+}
