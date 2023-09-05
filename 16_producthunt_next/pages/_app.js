3718427948d0a690b9b105b7015f441b6f09a36d
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 19:17:43 -0300

    16_producthunt_next-creando la app y primeros pasos

--- 16_producthunt_next/pages/_app.js
+++ 16_producthunt_next/pages/_app.js
@@ -1,21 +1,5 @@
-import App from 'next/app';
-import firebase, { FirebaseContext } from '../firebase';
-import useAutenticacion from '../hooks/useAutenticacion';
-
-const MyApp = props => {
-    const usuario = useAutenticacion();
-    const { Component, pageProps } = props;
-
-    return (
-        <FirebaseContext.Provider
-            value={{
-                firebase,
-                usuario
-            }}
-        >
-            <Component {...pageProps} />
-        </FirebaseContext.Provider>
-    )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- 16_producthunt_next/pages/index.js
+++ 16_producthunt_next/pages/index.js
@@ -1,30 +1,5 @@
-import React  from 'react'
-import Layout from '../components/layout/Layout';
-import DetallesProducto from '../components/layout/DetallesProducto';
-import useProductos from '../hooks/useProductos';
-
-const Home = () => {
-
-  const { productos } = useProductos('creado');
-
+export default function Home() {
   return (
-    <div>
-      <Layout>
-        <div className="listado-productos">
-            <div className="contenedor">
-              <ul className="bg-white">
-                  {productos.map(producto => (
-                      <DetallesProducto
-                          key={producto.id}
-                          producto={producto}
-                      />
-                  ))}
-              </ul>
-            </div>
-        </div>
-      </Layout>
-    </div>
+    <h1>Inicio</h1>
   )
 }
-
-export default Home
