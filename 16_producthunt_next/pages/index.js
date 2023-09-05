94af079c4b123b3d94ec107853e0bb69e7d81237
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 20:02:27 -0300

    16_producthunt_next-creando un layout principal

--- 16_producthunt_next/pages/index.js
+++ 16_producthunt_next/pages/index.js
@@ -1,12 +1,11 @@
-import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
 
-const Heading = styled.h1`
-  color: red;
-`
 export default function Home() {
   return (
     <div>
-      <Heading>Inicio</Heading>
+      <Layout>
+        <h1>Inicio</h1>
+      </Layout>
     </div>
   )
 }
