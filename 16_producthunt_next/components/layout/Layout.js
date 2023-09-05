8769cd61fe253c9b57af8cbd1be8f220d0cb735f
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 20:51:00 -0300

    16_producthunt_next-agregando fuentes de google fonts y más

--- 16_producthunt_next/components/layout/Layout.js
+++ 16_producthunt_next/components/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Head from 'next/head';
 import Header from './Header';
 
 const Layout = props => {
@@ -58,7 +59,14 @@ const Layout = props => {
           }
         `}
       />
-      
+
+      <Head>
+        <title>Product Hunt Firebase y Next.js</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto+Slab:400,700&display=swap" rel="stylesheet" />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+
       <Header />
 
       <main>
