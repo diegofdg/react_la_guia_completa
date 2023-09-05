4c1c1ea5a6c102ffde0074a36ea83ef385a2c445
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-05 20:43:15 -0300

    16_producthunt_next-agregando css global

--- 16_producthunt_next/components/layout/Layout.js
+++ 16_producthunt_next/components/layout/Layout.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { Global, css } from '@emotion/core';
 import Header from './Header';
 
 const Layout = props => {
   return ( 
     <>
+      <Global 
+        styles={css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6F6F6F;
+            --gris3: #e1e1e1;
+            --naranja: #DA552F;
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+
+          *, *:before, *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.6rem; 
+            line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          h1, h2, h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+
+          h1, h2 {
+            font-family: 'Roboto Slab', serif;
+            font-weight: 700;
+          }
+
+          h3 {
+            font-family: 'PT Sans', sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding:0;
+          }
+
+          a {
+            text-decoration: none;
+          }
+
+          img {
+            max-width: 100%;
+          }
+        `}
+      />
+      
       <Header />
 
       <main>
