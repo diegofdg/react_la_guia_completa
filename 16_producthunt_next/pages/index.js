9d6a456056f96721923f92db88806c2cb57f677c
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-07 06:44:27 -0300

    16_producthunt_next-consultando la base de datos

--- 16_producthunt_next/pages/index.js
+++ 16_producthunt_next/pages/index.js
@@ -1,6 +1,30 @@
+import React, { useEffect, useState, useContext }  from 'react';
 import Layout from '../components/layout/Layout';
+import { FirebaseContext } from '../firebase';
 
 export default function Home() {
+  const [ productos, guardarProductos ] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot);
+    }
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapShot) {
+    const productos = snapShot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+    guardarProductos(productos);
+    
+  }
+
   return (
     <div>
       <Layout>
