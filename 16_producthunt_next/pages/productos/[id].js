d4945f63d1ec2c7b75212c6c1653b7e148e94ecb
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-09 06:54:37 -0300

    16_producthunt_next-primeros pasos con el routing dinámico

--- 16_producthunt_next/pages/productos/[id].js
+++ 16_producthunt_next/pages/productos/[id].js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Producto = () => {
 
+  // Routing para obtener el id actual
+  const router = useRouter();
+  const { query: { id } } = router;
+
+  useEffect(() => {
+    if(id) {
+      console.log(id);
+    }
+}, [id]);
+
   return ( 
-   <h1>Desde [id].js</h1>
+   <h1>Desde {id}.js</h1>
   );
 }
  
