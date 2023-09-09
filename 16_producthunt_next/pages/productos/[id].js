a517487e04869c184312b6cc32b99051586d492e
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-09 07:30:18 -0300

    16_producthunt_next-finalizando el sidebar

--- 16_producthunt_next/pages/productos/[id].js
+++ 16_producthunt_next/pages/productos/[id].js
@@ -8,6 +8,7 @@ import Error404 from '../../components/layout/404';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { Campo, InputSubmit } from '../../components/ui/Formulario';
+import Boton from '../../components/ui/Boton';
 
 const Producto = () => {
 
@@ -97,7 +98,30 @@ const Producto = () => {
               </div>
 
               <aside>
-                2
+              <Boton
+                target="_blank"
+                bgColor="true"
+                href={url}
+              >
+                Visitar URL
+              </Boton>             
+
+              <div
+                css={css`
+                  margin-top: 5rem;
+                `}
+              >
+                <p
+                  css={css`
+                    text-align: center;
+                  `}
+                >
+                  {votos} Votos
+                </p>                
+                
+                <Boton>Votar</Boton>
+                
+                </div>
               </aside>
             </ContenedorProducto>            
           </div>
