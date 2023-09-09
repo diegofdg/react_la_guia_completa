94e9c7bddeccfa1404ca2cc498d2c2642b088154
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-09 19:54:10 -0300

    16_producthunt_next-primeros pasos con los comentarios

--- 16_producthunt_next/pages/productos/[id].js
+++ 16_producthunt_next/pages/productos/[id].js
@@ -10,19 +10,20 @@ import styled from '@emotion/styled';
 import { Campo, InputSubmit } from '../../components/ui/Formulario';
 import Boton from '../../components/ui/Boton';
 
-const Producto = () => {
+const ContenedorProducto = styled.div`
+  @media (min-width:768px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    column-gap: 2rem;
+  }
+`;
 
-  const ContenedorProducto = styled.div`
-   @media (min-width:768px) {
-      display: grid;
-      grid-template-columns: 2fr 1fr;
-      column-gap: 2rem;
-   }
-  `;
+const Producto = () => {  
 
   // state del componente
   const [producto, guardarProducto] = useState({});
   const [error, guardarError] = useState(false);
+  const [comentario, guardarComentario ] = useState({});
 
   // Routing para obtener el id actual
   const router = useRouter();
@@ -44,7 +45,7 @@ const Producto = () => {
       }
       obtenerProducto();
     }
-  }, [id, producto]);
+  }, [id]);
 
   if(Object.keys(producto).length === 0 && !error)  return 'Cargando...';
 
@@ -78,6 +79,40 @@ const Producto = () => {
     });
   }
 
+  // Funciones para crear comentarios
+  const comentarioChange = e => {
+    guardarComentario({
+      ...comentario,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  const agregarComentario = e => {
+    e.preventDefault();
+
+    if(!usuario) {
+      return router.push('/login');
+    }
+
+    // información extra al comentario
+    comentario.usuarioId = usuario.uid;
+    comentario.usuarioNombre = usuario.displayName;
+
+    // Tomar copia de comentarios y agregarlos al arreglo
+    const nuevosComentarios = [...comentarios, comentario];
+
+    // Actualizar la BD
+    firebase.db.collection('productos').doc(id).update({
+      comentarios: nuevosComentarios
+    })
+
+    // Actualizar el state
+    guardarProducto({
+      ...producto,
+      comentarios: nuevosComentarios
+    });
+  }
+
   return (
     <Layout>
       <>
@@ -94,42 +129,63 @@ const Producto = () => {
 
             <ContenedorProducto>
               <div>
-              <p>Publicado hace: { formatDistanceToNow( new Date(creado), {locale: es} )} </p>
-              <p>Por: {creador.nombre} de {empresa} </p>             
-                <img src={urlimagen} />
-                <p>{descripcion}</p>
-                { usuario && (
-                  <>
-                    <h2>Agrega tu comentario</h2>
-                    <form>
-                      <Campo>
-                        <input
-                          type="text"
-                          name="mensaje"
+                <p>Publicado hace: { formatDistanceToNow( new Date(creado), {locale: es} )} </p>
+                <p>Por: {creador.nombre} de {empresa} </p>             
+                  <img src={urlimagen} />
+                  <p>{descripcion}</p>
+                  { usuario && (
+                    <>
+                      <h2>Agrega tu comentario</h2>
+                      <form
+                        onSubmit={agregarComentario}
+                      >
+                        <Campo>
+                          <input
+                            type="text"
+                            name="mensaje"
+                            onChange={comentarioChange}
+                          />
+                        </Campo>
+                        <InputSubmit
+                          type="submit"
+                          value="Agregar Comentario"
                         />
-                      </Campo>
-                      <InputSubmit
-                        type="submit"
-                        value="Agregar Comentario"
-                      />
-                    </form>
-                  </>
-                )}
+                      </form>
+                    </>
+                  )}
+                  
+                  <h2
+                    css={css`
+                      margin: 2rem 0;
+                    `}
+                  >
+                    Comentarios
+                  </h2>
 
-                
-                <h2
-                  css={css`
-                    margin: 2rem 0;
-                  `}
-                >
-                  Comentarios
-                </h2>                
-                  {comentarios.map( comentario  => (
-                    <li>
-                      <p>{comentario.nombre}</p>
-                      <p>Escrito por: {comentario.usuarioNombre}</p>
-                    </li>
-                  ))}                
+                  {comentarios.length === 0 ? "Aún no hay comentarios" : (
+                    <ul>
+                      {comentarios.map((comentario, i) => (
+                        <li 
+                          key={`${comentario.usuarioId}-${i}`}
+                          css={css`
+                            border: 1px solid #e1e1e1;
+                            padding: 2rem;
+                          `}
+                        >
+                          <p>{comentario.mensaje}</p>
+                          <p>Escrito por: 
+                            <span
+                              css={css`
+                                font-weight:bold;
+                              `}
+                            >
+                              {''} {comentario.usuarioNombre}
+                            </span>
+                          </p>                        
+                        </li>
+                      ))}
+                    </ul>
+                  )}
               </div>
 
               <aside>
