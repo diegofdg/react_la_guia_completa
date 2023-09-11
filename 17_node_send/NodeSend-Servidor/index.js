dcc24adef63de967562d78405042946840860531
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 06:40:03 -0300

    17_node_send-routing de usuarios

--- 17_node_send/NodeSend-Servidor/index.js
+++ 17_node_send/NodeSend-Servidor/index.js
@@ -10,6 +10,9 @@ conectarDB();
 // Puerto de la app
 const port = process.env.PORT || 4000;
 
+// Rutas de la app
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
