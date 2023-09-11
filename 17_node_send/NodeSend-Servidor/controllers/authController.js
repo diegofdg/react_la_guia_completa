e73f888e4244f3f60e06c364b38167891eba4e80
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 07:36:11 -0300

    17_node_send-creando un json web token-jwt

--- 17_node_send/NodeSend-Servidor/controllers/authController.js
+++ 17_node_send/NodeSend-Servidor/controllers/authController.js
@@ -1,5 +1,7 @@
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: 'variables.env'});
 
 exports.autenticarUsuario = async (req, res, next) => {
   // Revisar si hay errores
@@ -16,10 +18,19 @@ exports.autenticarUsuario = async (req, res, next) => {
 
   // Verificar el password y autenticar el usuario
   if(bcrypt.compareSync(password, usuario.password )) {
-    console.log('El password es correcto');
 
     // Crear JWT
-    
+    const token = jwt.sign({
+      id: usuario._id,
+      nombre: usuario.nombre,
+      email: usuario.email
+    }, 
+      process.env.SECRETA, {
+        expiresIn: '8h'
+    });
+
+    res.json({ token });
+
   } else {
     res.status(401).json({msg: "Password Incorrecto"});
     return next();
