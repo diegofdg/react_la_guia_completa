14d2cca78b99b82600b884308d06779ab3b9593f
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 07:07:49 -0300

    17_node_send-hasheando passwords

--- 17_node_send/NodeSend-Servidor/controllers/usuarioController.js
+++ 17_node_send/NodeSend-Servidor/controllers/usuarioController.js
@@ -1,9 +1,10 @@
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
 
 exports.nuevoUsuario = async (req, res) => {
   
   // Verificar si el usuario ya estuvo registrado
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   let usuario = await Usuario.findOne({ email });
 
@@ -12,8 +13,16 @@ exports.nuevoUsuario = async (req, res) => {
   }
 
   // Crear un nuevo usuario
-  usuario = await new Usuario(req.body);
-  usuario.save();
-
-  res.json({msg : 'Usuario Creado Correctamente'});
+  usuario = new Usuario(req.body);
+    
+  // Hashear el password
+  const salt = await bcrypt.genSalt(10);
+  usuario.password = await bcrypt.hash(password, salt);
+  
+  try {
+    await usuario.save();
+    res.json({msg : 'Usuario Creado Correctamente'});
+  } catch (error) {
+    console.log(error);
+  }
 }
