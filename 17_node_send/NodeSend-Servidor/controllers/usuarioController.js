f53f8fd894b96d80e92298288a96861d098f5200
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 07:01:24 -0300

    17_node_send-cómo evitar registros duplicados

--- 17_node_send/NodeSend-Servidor/controllers/usuarioController.js
+++ 17_node_send/NodeSend-Servidor/controllers/usuarioController.js
@@ -1,10 +1,18 @@
 const Usuario = require('../models/Usuario');
 
 exports.nuevoUsuario = async (req, res) => {
-  console.log(req.body);
+  
+  // Verificar si el usuario ya estuvo registrado
+  const { email } = req.body;
+
+  let usuario = await Usuario.findOne({ email });
+
+  if(usuario) {
+    return res.status(400).json({ msg: 'El usuario ya esta registrado' });
+  }
 
   // Crear un nuevo usuario
-  const usuario = await new Usuario(req.body);
+  usuario = await new Usuario(req.body);
   usuario.save();
 
   res.json({msg : 'Usuario Creado Correctamente'});
