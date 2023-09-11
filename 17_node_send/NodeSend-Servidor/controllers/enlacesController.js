3d3bb4397cd6e16806bb20d4ddcea3672834bc7b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 16:09:41 -0300

    17_node_send-definiendo número de descargas y passwords a enlaces

--- 17_node_send/NodeSend-Servidor/controllers/enlacesController.js
+++ 17_node_send/NodeSend-Servidor/controllers/enlacesController.js
@@ -1,20 +1,37 @@
 const Enlaces = require('../models/Enlace');
 const shortid = require('shortid');
+const bcrypt = require('bcrypt')
 
 exports.nuevoEnlace = async (req, res, next) => {
   
   // Revisar si hay errores
   
   // Crear un objeto de Enlace
-  const { nombre_original, password } = req.body;
+  const { nombre_original } = req.body;
   const enlace = new Enlaces();
   enlace.url = shortid.generate();
   enlace.nombre = shortid.generate();;
   enlace.nombre_original = nombre_original;
-  enlace.password = password;
   
   // Si el usuario esta autenticado
-  
+  if(req.usuario) {
+    const { password, descargas } = req.body;
+
+    // Asignar a enlace el número de descargas
+    if(descargas) {
+      enlace.descargas = descargas;
+    }
+
+    // asignar un password
+    if(password) {
+      const salt = await bcrypt.genSalt(10);
+      enlace.password = await bcrypt.hash( password, salt );
+    }
+
+    // Asignar el autor
+    enlace.autor = req.usuario.id
+  }
+
   // Almacenar en la BD
   try {
     await enlace.save();
