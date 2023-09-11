327534bc18f9827a43a1044637f0d3494bf1e906
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 07:39:06 -0300

    17_node_send-agregando express validator

--- 17_node_send/NodeSend-Servidor/controllers/authController.js
+++ 17_node_send/NodeSend-Servidor/controllers/authController.js
@@ -2,14 +2,18 @@ const Usuario = require('../models/Usuario');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env'});
+const { validationResult } = require('express-validator');
 
 exports.autenticarUsuario = async (req, res, next) => {
   // Revisar si hay errores
+  const errores = validationResult(req);
+    if(!errores.isEmpty()) {
+      return res.status(400).json({errores: errores.array()});
+    }
   
   // Buscar el usuario para ver si esta registrado
   const { email, password } = req.body;
   const usuario = await Usuario.findOne({ email });
-  //console.log(usuario);
 
   if(!usuario) {
     res.status(401).json({msg : 'El Usuario No Existe'});
--- 17_node_send/NodeSend-Servidor/routes/auth.js
+++ 17_node_send/NodeSend-Servidor/routes/auth.js
@@ -4,6 +4,10 @@ const authController = require('../controllers/authController');
 const { check } = require('express-validator');
 
 router.post('/',
+  [
+    check('email', 'Agrega un email válido').isEmail(),
+    check('password', 'El password no puede ir vacio').not().isEmpty()
+  ],
   authController.autenticarUsuario
 );
 
