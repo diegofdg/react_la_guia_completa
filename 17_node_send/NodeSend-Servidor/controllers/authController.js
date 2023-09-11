540b59ab5ebc4bcda1fc2761b2691426f1c519b9
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-11 07:26:25 -0300

    17_node_send-verificando si el usuario existe o no

--- 17_node_send/NodeSend-Servidor/controllers/authController.js
+++ 17_node_send/NodeSend-Servidor/controllers/authController.js
@@ -1,4 +1,22 @@
+const Usuario = require('../models/Usuario');
+
 exports.autenticarUsuario = async (req, res, next) => {
+  // Revisar si hay errores
+  
+  // Buscar el usuario para ver si esta registrado
+  const { email } = req.body;
+  const usuario = await Usuario.findOne({ email });
+  console.log(usuario);
+
+  if(!usuario) {
+    res.status(401).json({msg : 'El Usuario No Existe'});
+    return next();
+  } 
+
+  // Verificar el password y autenticar el usuario
+  console.log('El usuario sí existe');
+
+  
 
 }
 
