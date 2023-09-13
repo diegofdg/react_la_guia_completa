8f0821249b56a4823524d0c6b3793b7162ee847b
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-13 06:27:59 -0300

    17_node_send-routing hacia login y crear cuenta

--- 17_node_send/nodesend_cliente/components/Header.js
+++ 17_node_send/nodesend_cliente/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return ( 
     <header className="py-8 flex flex-col md:flex-row items-center justify-between">
       <img className="w-64 mb-8 md:mb-0 cursor-pointer" src="/logo.svg" />
+      <div className="flex items-center">
+        <Link href="/login">
+          <p className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2">Iniciar Sesión</p>
+       </Link>
+       <Link href="/crearcuenta">
+        <p className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase">Crear Cuenta</p>
+       </Link>
+      </div>
     </header>
   );
 }
