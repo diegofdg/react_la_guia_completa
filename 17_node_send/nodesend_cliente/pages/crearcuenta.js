e1ee1c47afc3a48ac4ecb29ac523bef8ca5d6c12
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-13 15:44:40 -0300

    17_node_send-primeros pasos con crear cuenta

--- 17_node_send/nodesend_cliente/pages/crearcuenta.js
+++ 17_node_send/nodesend_cliente/pages/crearcuenta.js
@@ -4,7 +4,29 @@ import Layout from '../components/Layout';
 export default function CrearCuenta() {
   return (
     <Layout>
-      <h1>CrearCuenta</h1>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">Crear Cuenta</h2>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4">
+                <label 
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de Usuario"
+                />
+              </div>              
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
