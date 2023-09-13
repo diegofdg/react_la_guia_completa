e937358145126da5155615aa7a2a8347ebe7aa97
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-13 20:07:51 -0300

    17_node_send-creando el state para el context

--- 17_node_send/nodesend_cliente/context/auth/authState.js
+++ 17_node_send/nodesend_cliente/context/auth/authState.js
@@ -1,11 +1,27 @@
 import React, { useReducer } from 'react';
 import authContext from './authContext';
+import authReducer from './authReducer';
 
 const AuthState = ({children}) => {
+  
+  // Definir un state inicial
+  const initialState = {
+    token: '',
+    autenticado: null,
+    usuario: null,
+    mensaje: null
+  }
+
+  // Definir el reducer
+  const [ state, dispatch ] = useReducer(authReducer, initialState);
+
   return (
     <authContext.Provider
-      value={{ 
-        
+      value={{
+        token: state.token,
+        autenticado: state.autenticado,
+        usuario: state.usuario,
+        mensaje: state.mensaje
       }}
     >
       {children}
