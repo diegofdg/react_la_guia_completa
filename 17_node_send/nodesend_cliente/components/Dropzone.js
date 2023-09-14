280a32901a7319c2c89d4cde9ba31d1becf3cf68
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-14 15:48:05 -0300

    17_node_send-agregando react dropzone

--- 17_node_send/nodesend_cliente/components/Dropzone.js
+++ 17_node_send/nodesend_cliente/components/Dropzone.js
@@ -3,9 +3,24 @@ import { useDropzone } from 'react-dropzone';
 import clienteAxios from '../config/axios';
 
 const Dropzone = () => {
+  
+  // Extraer contenido de Dropzone
+  const { getRootProps, getInputProps, isDragActive } = useDropzone();  
+
   return ( 
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-      <p>Dropzone aquí</p>
+      <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
+        <input className="h-100 " { ...getInputProps() } />
+          {
+            isDragActive ? <p className="text-2xl text-center text-gray-600"> Suelta el archivo </p> :
+            <div className="text-center">
+              <p className="text-2xl text-center text-gray-600">Selecciona un archivo y arrastralo aquí</p>
+              <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
+                Selecciona archivos para subir
+              </button>
+            </div>
+          }
+      </div>
     </div>
   );
 }
