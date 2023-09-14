833c5e782ee824aca698851305b3c53b7c968604
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-14 15:52:07 -0300

    17_node_send-leyendo los archivos que se suben

--- 17_node_send/nodesend_cliente/components/Dropzone.js
+++ 17_node_send/nodesend_cliente/components/Dropzone.js
@@ -3,9 +3,13 @@ import { useDropzone } from 'react-dropzone';
 import clienteAxios from '../config/axios';
 
 const Dropzone = () => {
+
+  const onDrop = useCallback( (acceptedFiles) => {
+    console.log(acceptedFiles);    
+  });
   
   // Extraer contenido de Dropzone
-  const { getRootProps, getInputProps, isDragActive } = useDropzone();  
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop});  
 
   return ( 
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
