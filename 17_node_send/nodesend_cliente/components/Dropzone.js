66e8cd2d30afd19597c1b1bfd7f91452116c44ac
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-15 05:38:30 -0300

    17_node_send-mostrar el botón crear enlace una vez que se sube el archivo

--- 17_node_send/nodesend_cliente/components/Dropzone.js
+++ 17_node_send/nodesend_cliente/components/Dropzone.js
@@ -14,7 +14,8 @@ const Dropzone = () => {
   }, []);
   
   // Extraer contenido de Dropzone
-  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop});  
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop}); 
+
   const archivos = acceptedFiles.map( archivo => (
     <li key={archivo.lastModified} className="bg-white flex-1 p-3 mb-4 shadow-lg rounded">
       <p className="font-bold text-xl">{archivo.path}</p> 
@@ -22,23 +23,40 @@ const Dropzone = () => {
     </li>
   ));
 
+  const crearEnlace = () => {
+    console.log('creando el enlace');
+  }
+
   return ( 
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
-      <ul>
-        {archivos}
-      </ul>
-      <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
-        <input className="h-100 " { ...getInputProps() } />
-          {
-            isDragActive ? <p className="text-2xl text-center text-gray-600"> Suelta el archivo </p> :
-            <div className="text-center">
-              <p className="text-2xl text-center text-gray-600">Selecciona un archivo y arrastralo aquí</p>
-              <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
-                Selecciona archivos para subir
-              </button>
-            </div>
-          }
-      </div>
+      { acceptedFiles.length > 0 ? (
+        <div className="mt-10 w-full">
+          <h4 className="text-2xl font-bold text-center mb-4">Archivos</h4>
+          <ul>
+            {archivos}
+          </ul>
+          <button
+            type="button"
+            className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+            onClick={ () => crearEnlace()  }
+          >
+            Crear Enlace
+          </button>
+        </div>
+      ) : (
+        <div { ...getRootProps({ className: 'dropzone w-full py-32' }) }>
+          <input className="h-100 " { ...getInputProps() } />
+            {
+              isDragActive ? <p className="text-2xl text-center text-gray-600"> Suelta el archivo </p> :
+              <div className="text-center">
+                <p className="text-2xl text-center text-gray-600">Selecciona un archivo y arrastralo aquí</p>
+                <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
+                  Selecciona archivos para subir
+                </button>
+              </div>
+            }
+        </div>
+      )}
     </div>
   );
 }
