edf89a938f254a1d8750be07847c3b2cb7d41884
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-16 06:58:37 -0300

    17_node_send-mostrando un formulario para passwords

--- 17_node_send/nodesend_cliente/components/Formulario.js
+++ 17_node_send/nodesend_cliente/components/Formulario.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+  const [tienePassword, setTienePassword] = useState(false);
 
   return ( 
     <div className="w-full mt-20">
@@ -13,6 +15,21 @@ const Formulario = () => {
           <option value="10">10 Descargas</option>
           <option value="20">20 Descargas</option>
         </select>
+      </div>
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <label className="text-lg text-gray-800 mr-2">Proteger con Contraseña</label>
+          <input 
+            type="checkbox"
+            onChange={() => setTienePassword(!tienePassword)}
+          />
+        </div>
+        { tienePassword ? (
+          <input 
+            type="password" 
+            class="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500" 
+          />
+        ) : null }
       </div>
     </div>
   );
