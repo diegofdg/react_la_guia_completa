32f69a81e4cc4c3a315639d52d1bcb1ac38d4087
Diego Fabián Di Giorgio <diegofdg@gmail.com>
2023-09-21 16:22:37 -0300

    18_citas_react_testing-seleccionar elementos de componentes

--- 18_citas_react_testing/src/__tests__/Formulario.js
+++ 18_citas_react_testing/src/__tests__/Formulario.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import Formulario from '../components/Formulario';
+import '@testing-library/jest-dom/extend-expect';
+
 test('<Formulario /> Cargar el formulario y revisar que todo sea correcto', () => {
+  //const wrapper = render(<Formulario />);
+  //wrapper.debug();
+
+  render(<Formulario />);
+  expect( screen.getByText('Crear Cita')).toBeInTheDocument();
 
 });
