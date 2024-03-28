f6269ea5dd1957df1f9902a9475bf8c93b65cfc5
diegofdg <diegofdg@gmail.com>
2024-03-28 10:37:27 -0300

    01_carrito_guitarla-iterando sobre los productos en react

--- 01_carrito_guitarla/src/App.jsx
+++ 01_carrito_guitarla/src/App.jsx
@@ -14,18 +14,12 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
-          <Guitar />
+          {
+            data.map(()=>(
+              <Guitar />
+            ))
+          }
+          
         </div>
       </main>
 
