fab7e2d23c24afdd5e979ca7216b4d9a96f43518
diegofdg <diegofdg@gmail.com>
2024-03-28 10:14:16 -0300

    01_carrito_guitarla-qué son los componentes en react

--- 01_carrito_guitarla/src/App.jsx
+++ 01_carrito_guitarla/src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <>
+      <Header />
       <header className="py-5 header">
         <div className="container-xl">
           <div className="row justify-content-center justify-content-md-between">
