e3b1425f6311d4bcad268c260d7bfdee02fa84cb
diegofdg <diegofdg@gmail.com>
2024-03-28 10:54:18 -0300

    01_carrito_guitarla-eventos en jsx

--- 01_carrito_guitarla/src/components/Guitar.jsx
+++ 01_carrito_guitarla/src/components/Guitar.jsx
@@ -1,5 +1,10 @@
 export default function Guitar({ guitar }) {
   const { id, name, image, description, price } = guitar
+
+  const handleClick = (guitar) => {
+    console.log("diste click...", guitar.id)
+  }
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
@@ -12,6 +17,7 @@ export default function Guitar({ guitar }) {
         <button
           type="button"
           className="btn btn-dark w-100"
+          onClick={() => handleClick(guitar)}
         >Agregar al Carrito</button>
       </div>
     </div>
