54b264240a31012e243d4f9cbf0e191755fcf4af
diegofdg <diegofdg@gmail.com>
2024-03-29 08:14:04 -0300

    01_carrito_guitarla-useMemo para mejorar el performance de nuestro código

--- 01_carrito_guitarla/src/components/Header.jsx
+++ 01_carrito_guitarla/src/components/Header.jsx
@@ -1,8 +1,10 @@
+import { useMemo } from "react"
+
 export default function Header({ cart }) {
 
   // State derivado
-  const isEmpty = () => cart.length === 0
-  const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
+  const isEmpty = useMemo(() => cart.length === 0, [cart])
+  const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
 
   return (
     <header className="py-5 header">
@@ -20,7 +22,7 @@ export default function Header({ cart }) {
               <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
               <div id="carrito" className="bg-white p-3">
-                {isEmpty() ? (
+                {isEmpty ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
                   <>
@@ -75,7 +77,7 @@ export default function Header({ cart }) {
                         ))}
                       </tbody>
                     </table>
-                    <p className="text-end">Total pagar: <span className="fw-bold">$ {cartTotal()}</span></p>
+                    <p className="text-end">Total pagar: <span className="fw-bold">$ {cartTotal}</span></p>
                   </>
                 )}
 
