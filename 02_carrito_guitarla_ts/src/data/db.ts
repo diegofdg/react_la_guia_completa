def7336249751e2a03198de44d3f3bf3802edde2
diegofdg <diegofdg@gmail.com>
2024-03-30 16:24:05 -0300

    02_carrito_guitarla_ts-types e interfaces

--- 02_carrito_guitarla_ts/src/data/db.ts
+++ 02_carrito_guitarla_ts/src/data/db.ts
@@ -1,4 +1,20 @@
-export const db = [
+type Guitar = {
+  id: number,
+  name: string,
+  image: string,
+  description: string,
+  price: number
+}
+
+/* interface Guitar {
+  id: number,
+  name: string,
+  image: string,
+  description: string,
+  price: number
+} */
+
+export const db : Guitar[] = [
   {
     id: 1,
     name: 'Lukather',
