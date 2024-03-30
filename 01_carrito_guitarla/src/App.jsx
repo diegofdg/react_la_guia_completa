38e8862a6481369c11d0a85a94df73cf12a8aaa8
diegofdg <diegofdg@gmail.com>
2024-03-30 08:27:17 -0300

    01_carrito_guitarla-migrando el código hacia useCart

--- 01_carrito_guitarla/src/App.jsx
+++ 01_carrito_guitarla/src/App.jsx
@@ -1,74 +1,11 @@
-import { useState, useEffect } from "react"
-import { db } from "./data/db"
+
+
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 import { useCart } from "./hooks/useCart"
 
 function App() {
-  const { auth, carrito } = useCart()
-  console.log(auth, carrito)
-
-  const initialCart = () => {
-    const localStorageCart = localStorage.getItem('cart')
-    return localStorageCart ? JSON.parse(localStorageCart) : []
-  }
-
-  const [data] = useState(db)
-  const [cart, setCart] = useState(initialCart)
-  const MAX_ITEMS = 5
-  const MIN_ITEMS = 1
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart])
-
-  function addToCart(item) {
-    const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
-    if (itemExist >= 0) {
-      if (cart[itemExist].quantity >= MAX_ITEMS) return
-      const updatedCart = [...cart]
-      updatedCart[itemExist].quantity++
-      setCart(updatedCart)
-    } else {
-      item.quantity = 1
-      setCart([...cart, item])
-    }
-  }
-
-  function removeFromCart(id) {
-    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
-  }
-
-  function decreaseQuantity(id) {
-    const updatedCart = cart.map(item => {
-      if (item.id === id && item.quantity > MIN_ITEMS) {
-        return {
-          ...item,
-          quantity: item.quantity - 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-
-  }
-
-  function increaseQuantity(id) {
-    const updatedCart = cart.map(item => {
-      if (item.id === id && item.quantity < MAX_ITEMS) {
-        return {
-          ...item,
-          quantity: item.quantity + 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function clearCart() {
-    setCart([])
-  }
+  const { data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity,clearCart } = useCart()
 
   return (
     <>
@@ -89,7 +26,6 @@ function App() {
               <Guitar
                 key={guitar.id}
                 guitar={guitar}
-                setCart={setCart}
                 addToCart={addToCart}
               />
             ))
--- 01_carrito_guitarla/src/hooks/useCart.js
+++ 01_carrito_guitarla/src/hooks/useCart.js
@@ -1,9 +1,76 @@
+import { useState, useEffect } from "react"
+import { db } from "../data/db"
+
 export const useCart = () => {
-  const auth = true
-  const carrito = []
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
+    if (itemExist >= 0) {
+      if (cart[itemExist].quantity >= MAX_ITEMS) return
+      const updatedCart = [...cart]
+      updatedCart[itemExist].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map(item => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
 
   return {
-    auth,
-    carrito
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart
   }
 }
