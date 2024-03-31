efc904cb8f5c3ec52e0d045e91e2513655b3337d
diegofdg <diegofdg@gmail.com>
2024-03-31 08:52:07 -0300

    03_calculadora_propinas-calculando el consumo y mas typescript

--- 03_calculadora_propinas/src/App.tsx
+++ 03_calculadora_propinas/src/App.tsx
@@ -33,7 +33,9 @@ function App() {
             order={order}
             removeItem={removeItem}
           />
-          <OrderTotals />
+          <OrderTotals
+            order={order}
+          />
         </div>
       </main>
     </>
--- 03_calculadora_propinas/src/components/OrderTotal.tsx
+++ 03_calculadora_propinas/src/components/OrderTotal.tsx
@@ -1,10 +1,20 @@
-export default function OrderTotals() {
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+  order: OrderItem[]
+}
+
+export default function OrderTotals({ order }: OrderTotalsProps) {
+  const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totales y Propina:</h2>
         <p>Subtotal a pagar: {''}
-          <span className="font-bold">$ 0</span>
+          <span className="font-bold">{ formatCurrency(subtotalAmount) }</span>
         </p>
         <p>Propina: {''}
           <span className="font-bold">$ 0</span>
