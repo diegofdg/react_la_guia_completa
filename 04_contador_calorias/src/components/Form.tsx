007698eeab31e61b1699a2836b656a41db8ef5c0
diegofdg <diegofdg@gmail.com>
2024-03-31 16:26:17 -0300

    04_contador_calorias-escribiendo en el state

--- 04_contador_calorias/src/components/Form.tsx
+++ 04_contador_calorias/src/components/Form.tsx
@@ -8,6 +8,13 @@ export default function Form() {
     calories: 0
   })
 
+  const handleChange = (e) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value
+    })
+  }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -16,6 +23,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
           id="category"
           value={activity.category}
+          onChange={handleChange}
         >
           {categories.map(category => (
             <option
@@ -36,6 +44,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
           value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -47,6 +56,7 @@ export default function Form() {
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias. ej. 300 o 500"
           value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
