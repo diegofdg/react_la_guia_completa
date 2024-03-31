f9902078e982065adf204111d5946973344008b2
diegofdg <diegofdg@gmail.com>
2024-03-31 16:06:45 -0300

    04_contador_calorias-agregando el formulario de entrada de datos

--- 04_contador_calorias/src/components/Form.tsx
+++ 04_contador_calorias/src/components/Form.tsx
@@ -1,5 +1,50 @@
+import { categories } from "../data/categories"
+
 export default function Form() {
   return (
-    <div>Form</div>
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">Categoría:</label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name" className="font-bold">Actividad:</label>
+        <input
+          id="name"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">Calorias:</label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorias. ej. 300 o 500"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value="Guardar comida o Guardar Ejercicio"
+      />
+    </form>
   )
 }
