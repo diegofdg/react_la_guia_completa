3322b40c3388c703e4f0fbf7a6aeb2f98682ab4d
diegofdg <diegofdg@gmail.com>
2024-03-31 06:54:16 -0300

    03_calculadora_propinas-listando los productos del menú

--- 03_calculadora_propinas/src/App.tsx
+++ 03_calculadora_propinas/src/App.tsx
@@ -5,18 +5,22 @@ function App() {
 
   return (
     <>
-      <header className=" bg-teal-400 py-5">
+      <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
-          {menuItems.map(item => (
-            <MenuItem
-              key={item.id}
-            />
-          ))}
+        <div className="p-5">
+          <h2 className="font-black text-4xl">Menú</h2>
+
+          <div className="mt-10 space-y-3">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+              />
+            ))}
+          </div>
         </div>
 
         <div>
