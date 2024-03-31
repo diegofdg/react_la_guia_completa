3fc00026ee1fc3712f79b4080b7825a113904d93
diegofdg <diegofdg@gmail.com>
2024-03-31 06:40:29 -0300

    03_calculadora_propinas-agregando los contenedores principales

--- 03_calculadora_propinas/src/App.tsx
+++ 03_calculadora_propinas/src/App.tsx
@@ -1,7 +1,7 @@
 import { menuItems } from "./data/db"
 
 function App() {
-  
+
   console.log(menuItems)
 
   return (
@@ -9,6 +9,16 @@ function App() {
       <header className=" bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
