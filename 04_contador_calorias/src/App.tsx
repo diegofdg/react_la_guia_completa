2e11fdc574f5f26912d488bb732e43c64e624825
diegofdg <diegofdg@gmail.com>
2024-03-31 15:55:02 -0300

    04_contador_calorias-creando los contenedores principales

--- 04_contador_calorias/src/App.tsx
+++ 04_contador_calorias/src/App.tsx
@@ -1,7 +1,21 @@
+import Form from "./components/Form"
+
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Contador de Calorías</h1>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between items-center">
+          <h1 className="text-center text-lg font-bold text-white uppercase">
+            Contador de Calorias
+          </h1>
+        </div>
+      </header>
+
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form />
+        </div>
+      </section>
     </>
   )
 }
