c16b3a9948325def82027748dee39aaf10822694
diegofdg <diegofdg@gmail.com>
2024-03-31 07:15:01 -0300

    03_calculadora_propinas-añadiendo elementos al state de la orden

--- 03_calculadora_propinas/src/hook/useOrder.ts
+++ 03_calculadora_propinas/src/hook/useOrder.ts
@@ -6,8 +6,11 @@ export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([])
 
   const addItem = (item: MenuItem) => {
-    console.log(item)
+    const newItem = {...item, quantity: 1}
+    setOrder([...order, newItem])
   }
+
+  console.log(order)
 
   return {
     addItem
