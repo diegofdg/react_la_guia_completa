de9e8af1914dd8dd1b76ea54e054b70d63fa9a0e
diegofdg <diegofdg@gmail.com>
2024-03-31 07:39:31 -0300

    03_calculadora_propinas-formateando el consumo y mostrando cantidades

--- 03_calculadora_propinas/src/components/OrderContents.tsx
+++ 03_calculadora_propinas/src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 import { OrderItem } from "../types"
 
 type OrderContentsProps = {
@@ -7,15 +8,31 @@ type OrderContentsProps = {
 export default function OrderContents({ order }: OrderContentsProps) {
   return (
     <div>
-      <h2 className='font-black text-4xl'>Consumo</h2>
+      <h2 className="font-black text-4xl">Consumo</h2>
 
       <div className="space-y-3 mt-10">
         {order.length === 0 ?
           <p>La orden está vacía</p> :
           (order.map(item => (
-            <div key={item.id}>
-              <p>{item.name}</p>
+            <div
+              key={item.id}
+              className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
+            >
+              <div>
+                <p className="text-lg">
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className="font-black">
+                  Cantidad: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+              <button
+                className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+              >
+                X
+              </button>
             </div>
+
           )))
         }
       </div>
