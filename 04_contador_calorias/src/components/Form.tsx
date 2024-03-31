ca0040448df65ce7665d26dcaa1a2bf1bccebc85
diegofdg <diegofdg@gmail.com>
2024-03-31 16:34:43 -0300

    04_contador_calorias-creando el type de actividad y seteando los valores en el state

--- 04_contador_calorias/src/components/Form.tsx
+++ 04_contador_calorias/src/components/Form.tsx
@@ -1,17 +1,19 @@
 import { useState, ChangeEvent } from "react"
 import { categories } from "../data/categories"
+import { Activity } from "../types"
 
 export default function Form() {
-  const [activity, setActivity] = useState({
+  const [activity, setActivity] = useState<Activity>({
     category: 1,
     name: "",
     calories: 0
   })
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id)
     setActivity({
       ...activity,
-      [e.target.id]: e.target.value
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
     })
   }
 
