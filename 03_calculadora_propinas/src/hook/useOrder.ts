060dd69a6a0c402ad7cf4f452bb1e102dc4bde75
diegofdg <diegofdg@gmail.com>
2024-03-31 07:01:51 -0300

    03_calculadora_propinas-creando un hook para la orden

---
+++ 03_calculadora_propinas/src/hook/useOrder.ts
@@ -0,0 +1,10 @@
+import { useState } from "react"
+import type { OrderItem } from "../types"
+
+export default function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([])
+
+  return {
+
+  }
+}
