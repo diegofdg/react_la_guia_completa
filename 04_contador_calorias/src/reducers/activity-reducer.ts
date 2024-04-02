4dd0e8edd4ca1a56bf49ef49d1cb6d8a11927720
diegofdg <diegofdg@gmail.com>
2024-04-02 08:30:26 -0300

    04_contador_calorias-creando la acción para seleccionar la actividad a editar

--- 04_contador_calorias/src/reducers/activity-reducer.ts
+++ 04_contador_calorias/src/reducers/activity-reducer.ts
@@ -1,15 +1,17 @@
 import { Activity } from "../types"
 
-export type ActivityActions = {
-  type: 'save-activity', payload: { newActivity: Activity }
-}
+export type ActivityActions =
+  { type: 'save-activity', payload: { newActivity: Activity } } |
+  { type: 'set-activeId', payload: { id: Activity['id'] } }
 
 export type ActivityState = {
-  activities: Activity[]
+  activities: Activity[],
+  activeId: Activity['id']
 }
 
 export const initialState: ActivityState = {
-  activities: []
+  activities: [],
+  activeId: ''
 }
 
 export const activityReducer = (
@@ -18,11 +20,16 @@ export const activityReducer = (
 
 ) => {
   if (action.type === "save-activity") {
-    // Este código maneja la lógica para actualizar el state
-    console.log(action.payload.newActivity)
     return {
       ...state,
       activities: [...state.activities, action.payload.newActivity]
+    }
+  }
+
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id
     }
   }
   return state
