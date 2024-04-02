34ab9b3372be4ff3519105c3df32133b37a4313c
diegofdg <diegofdg@gmail.com>
2024-04-02 08:37:48 -0300

    04_contador_calorias-almacenando el id a editar

--- 04_contador_calorias/src/App.tsx
+++ 04_contador_calorias/src/App.tsx
@@ -27,6 +27,7 @@ function App() {
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList
           activities={state.activities}
+          dispatch={dispatch}
         />
       </section>
     </>
--- 04_contador_calorias/src/components/ActivityList.tsx
+++ 04_contador_calorias/src/components/ActivityList.tsx
@@ -1,13 +1,15 @@
-import { useMemo } from "react"
+import { useMemo, Dispatch } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 import { PencilSquareIcon } from "@heroicons/react/24/outline"
+import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
-  activities: Activity[]
+  activities: Activity[],
+  dispatch: Dispatch<ActivityActions>
 }
 
-export default function ActivityList({ activities }: ActivityListProps) {
+export default function ActivityList({ activities, dispatch }: ActivityListProps) {
   const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
 
   return (
@@ -30,7 +32,9 @@ export default function ActivityList({ activities }: ActivityListProps) {
 
           <div>
             <div className="flex gap-5 items-center">
-              <button>
+              <button
+                onClick={() => dispatch({type: "set-activeId", payload: {id: activity.id}})}
+              >
                 <PencilSquareIcon
                   className="h-8 w-8 text-gray-800"
                 />
