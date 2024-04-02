42ecd05476e372df4c72b05d7a91890c151aed3e
diegofdg <diegofdg@gmail.com>
2024-04-02 08:54:00 -0300

    04_contador_calorias-creando la acción para eliminar actividades

--- 04_contador_calorias/src/reducers/activity-reducer.ts
+++ 04_contador_calorias/src/reducers/activity-reducer.ts
@@ -1,17 +1,18 @@
 import { Activity } from "../types"
 
 export type ActivityActions =
-  { type: 'save-activity', payload: { newActivity: Activity } } |
-  { type: 'set-activeId', payload: { id: Activity['id'] } }
+  { type: "save-activity", payload: { newActivity: Activity } } |
+  { type: "set-activeId", payload: { id: Activity["id"] } } |
+  { type: "delete-activity", payload: { id: Activity["id"] } }
 
 export type ActivityState = {
   activities: Activity[],
-  activeId: Activity['id']
+  activeId: Activity["id"]
 }
 
 export const initialState: ActivityState = {
   activities: [],
-  activeId: ''
+  activeId: ""
 }
 
 export const activityReducer = (
@@ -22,7 +23,7 @@ export const activityReducer = (
   if (action.type === "save-activity") {
     let updatedActivities: Activity[] = []
     if (state.activeId) {
-      updatedActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity : activity )
+      updatedActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
 
     } else {
       updatedActivities = [...state.activities, action.payload.newActivity]
@@ -30,7 +31,7 @@ export const activityReducer = (
     return {
       ...state,
       activities: updatedActivities,
-      activeId: ''
+      activeId: ""
     }
   }
 
@@ -38,6 +39,13 @@ export const activityReducer = (
     return {
       ...state,
       activeId: action.payload.id
+    }
+  }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(activity => activity.id !== action.payload.id)
     }
   }
   return state
