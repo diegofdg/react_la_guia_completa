95b72d3a4d12672760f371128730e583aefbca53
diegofdg <diegofdg@gmail.com>
2024-04-02 08:51:56 -0300

    04_contador_calorias-actualizando las actividades

--- 04_contador_calorias/src/reducers/activity-reducer.ts
+++ 04_contador_calorias/src/reducers/activity-reducer.ts
@@ -20,9 +20,17 @@ export const activityReducer = (
 
 ) => {
   if (action.type === "save-activity") {
+    let updatedActivities: Activity[] = []
+    if (state.activeId) {
+      updatedActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity : activity )
+
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity]
+    }
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity]
+      activities: updatedActivities,
+      activeId: ''
     }
   }
 
