47d2a77c500b84fb95031f3bac1a7e2d33a75c1a
diegofdg <diegofdg@gmail.com>
2024-04-02 07:44:43 -0300

    04_contador_calorias-reiniciando el formulario despues de añadir una actividad

--- 04_contador_calorias/src/components/Form.tsx
+++ 04_contador_calorias/src/components/Form.tsx
@@ -7,12 +7,14 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>
 }
 
+const initialState = {
+  category: 1,
+  name: "",
+  calories: 0
+}
+
 export default function Form({ dispatch }: FormProps) {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: "",
-    calories: 0
-  })
+  const [activity, setActivity] = useState<Activity>(initialState)
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ["category", "calories"].includes(e.target.id)
@@ -30,6 +32,7 @@ export default function Form({ dispatch }: FormProps) {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     dispatch({ type: 'save-activity', payload: { newActivity: activity } })
+    setActivity(initialState)
   }
 
   return (
