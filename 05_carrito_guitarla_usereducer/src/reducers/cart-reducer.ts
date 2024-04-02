57fa341f956769a715c717cc04518a4257022321
diegofdg <diegofdg@gmail.com>
2024-04-02 12:15:29 -0300

    05_carrito_guitarla_usereducer-definiendo el state inicial

--- 05_carrito_guitarla_usereducer/src/reducers/cart-reducer.ts
+++ 05_carrito_guitarla_usereducer/src/reducers/cart-reducer.ts
@@ -1,4 +1,5 @@
-import { Guitar } from "../types";
+import { db } from "../data/db";
+import { CartItem, Guitar } from "../types";
 
 export type CartActions =
   { type: 'add-to-cart', payload: { item: Guitar } } |
@@ -6,3 +7,13 @@ export type CartActions =
   { type: 'decrease-quantity', payload: { id: Guitar['id'] } } |
   { type: 'increase-quantity', payload: { id: Guitar['id'] } } |
   { type: 'clear-cart' }
+
+export type CartState = {
+  data: Guitar[]
+  cart: CartItem[]
+}
+
+export const initialState: CartState = {
+  data: db,
+  cart: []
+}
