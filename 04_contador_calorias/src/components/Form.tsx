123246d558141bdc76ae3e781dd03a2a06606446
diegofdg <diegofdg@gmail.com>
2024-04-02 07:51:44 -0300

    04_contador_calorias-creando ids únicos

--- 04_contador_calorias/src/components/Form.tsx
+++ 04_contador_calorias/src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { v4 as uuidv4 } from "uuid"
 import { categories } from "../data/categories"
 import { Activity } from "../types"
 import { ActivityActions } from "../reducers/activity-reducer"
@@ -7,7 +8,8 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>
 }
 
-const initialState = {
+const initialState: Activity = {
+  id: uuidv4(),
   category: 1,
   name: "",
   calories: 0
@@ -32,7 +34,10 @@ export default function Form({ dispatch }: FormProps) {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     dispatch({ type: 'save-activity', payload: { newActivity: activity } })
-    setActivity(initialState)
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
 
   return (
