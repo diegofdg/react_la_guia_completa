0918f1e68cfb909646c5a454385f1c0d9f0a5959
diegofdg <diegofdg@gmail.com>
2024-04-02 12:13:15 -0300

    05_carrito_guitarla_usereducer-creando el reducer y el type de acciones

---
+++ 05_carrito_guitarla_usereducer/src/reducers/cart-reducer.ts
@@ -0,0 +1,8 @@
+import { Guitar } from "../types";
+
+export type CartActions =
+  { type: 'add-to-cart', payload: { item: Guitar } } |
+  { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
+  { type: 'decrease-quantity', payload: { id: Guitar['id'] } } |
+  { type: 'increase-quantity', payload: { id: Guitar['id'] } } |
+  { type: 'clear-cart' }
