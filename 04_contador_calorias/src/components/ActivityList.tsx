b11e2755ca03a279c7b9a51ab06de199d33e883e
diegofdg <diegofdg@gmail.com>
2024-04-02 08:17:11 -0300

    04_contador_calorias-añadiendo mas información al listado de actividades

--- 04_contador_calorias/src/components/ActivityList.tsx
+++ 04_contador_calorias/src/components/ActivityList.tsx
@@ -1,11 +1,14 @@
+import { useMemo } from "react"
 import { Activity } from "../types"
+import { categories } from "../data/categories"
 
 type ActivityListProps = {
   activities: Activity[]
 }
 
 export default function ActivityList({ activities }: ActivityListProps) {
-  console.log(activities)
+  const categoryName = useMemo(() => (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : ''), [activities])
+
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -14,7 +17,9 @@ export default function ActivityList({ activities }: ActivityListProps) {
       {activities.map(activity => (
         <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between shadow">
           <div className="space-y-2 relative">
-            <p>{activity.category}</p>
+            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+              {categoryName(+activity.category)}
+            </p>
             <p className="text-2xl font-bold pt-5">{activity.name}</p>
             <p className="font-black text-4xl text-lime-500">
               {activity.calories} {''}
