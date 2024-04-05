95ad88a1506359f2dc91fca4f9efeddfbb22b848
diegofdg <diegofdg@gmail.com>
2024-04-05 15:34:35 -0300

    07_control_gastos-validando el presupuesto

--- 07_control_gastos/src/components/BudgetForm.tsx
+++ 07_control_gastos/src/components/BudgetForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 export default function BudgetForm() {
   const [budget, setBudget] = useState(0)
@@ -6,6 +6,10 @@ export default function BudgetForm() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setBudget(e.target.valueAsNumber)
   }
+
+  const isValid = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
 
   return (
     <form className="space-y-5">
@@ -28,6 +32,7 @@ export default function BudgetForm() {
         type="submit"
         value="Definir Presupuesto"
         className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+        disabled={isValid}
       />
     </form>
   )
