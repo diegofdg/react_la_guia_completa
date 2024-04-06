f6c2c527ed94f7080677d57f3f8fe38bab031292
diegofdg <diegofdg@gmail.com>
2024-04-06 07:12:12 -0300

    07_control_gastos-creando el componente con las cantidades y la gráfica

--- 07_control_gastos/src/components/BudgetTracker.tsx
+++ 07_control_gastos/src/components/BudgetTracker.tsx
@@ -1,5 +1,34 @@
+import AmountDisplay from "./AmountDisplay"
+
 export default function BudgetTracker() {
   return (
-    <div>BudgetTracker</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="Gráfico de gastos" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-8">
+        <button
+          type="button"
+          className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+        >
+          Resetear App
+        </button>
+        <AmountDisplay
+          label="Presupuesto"
+          amount={300}
+        />
+
+        <AmountDisplay
+          label="Disponible"
+          amount={200}
+        />
+
+        <AmountDisplay
+          label="Gastado"
+          amount={100}
+        />
+      </div>
+    </div>
   )
 }
