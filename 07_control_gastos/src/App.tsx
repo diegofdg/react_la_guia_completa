e441353d1da91589c152bfdf43046ff9d5239598
diegofdg <diegofdg@gmail.com>
2024-04-06 07:18:01 -0300

    07_control_gastos-instalando headless ui para tener una ventana modal

--- 07_control_gastos/src/App.tsx
+++ 07_control_gastos/src/App.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react"
 import BudgetForm from "./components/BudgetForm"
 import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 import { useBudget } from "./hooks/useBudget"
 
 function App() {
@@ -19,6 +20,12 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   )
 }
