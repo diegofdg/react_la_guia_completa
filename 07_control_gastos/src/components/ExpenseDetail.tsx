5f55d08e66e86aad5b3c3755d366911f17cdc0a3
diegofdg <diegofdg@gmail.com>
2024-04-08 20:19:38 -0300

    07_control_gastos-mostrando los gastos

--- 07_control_gastos/src/components/ExpenseDetail.tsx
+++ 07_control_gastos/src/components/ExpenseDetail.tsx
@@ -1,11 +1,27 @@
+import AmountDisplay from "./AmountDisplay"
 import { Expense } from "../types"
+import { formatDate } from "../helpers"
 
 type ExpenseDetailProps = {
-  expense : Expense
+  expense: Expense
 }
 
-export default function ExpenseDetail({expense} : ExpenseDetailProps) {
-    return (
-        <div>ExpenseDetail</div>
-    )
+export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+  return (
+    <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div>
+        
+      </div>
+
+      <div className="flex-1 space-y-2">
+        <p className=" text-sm font-bold uppercase text-slate-500">{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+      </div>
+
+      <AmountDisplay
+        amount={expense.amount}
+      />
+
+    </div>
+  )
 }
