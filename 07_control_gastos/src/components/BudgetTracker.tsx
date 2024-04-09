923ccf0be7b9a5f424b621663073c56a5b90c49d
diegofdg <diegofdg@gmail.com>
2024-04-09 18:43:44 -0300

    07_control_gastos-trabajando con el componente que muestra presupuesto, gastado y disponible

--- 07_control_gastos/src/components/BudgetTracker.tsx
+++ 07_control_gastos/src/components/BudgetTracker.tsx
@@ -1,6 +1,12 @@
+import { useMemo } from "react"
+import { useBudget } from "../hooks/useBudget"
 import AmountDisplay from "./AmountDisplay"
 
 export default function BudgetTracker() {
+  const { state } = useBudget()
+  const totalExpenses = useMemo( () => state.expenses.reduce((total, expense) => expense.amount + total, 0) , [state.expenses])
+  const remainingBudget = state.budget - totalExpenses
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
@@ -16,17 +22,17 @@ export default function BudgetTracker() {
         </button>
         <AmountDisplay
           label="Presupuesto"
-          amount={300}
+          amount={state.budget}
         />
 
         <AmountDisplay
           label="Disponible"
-          amount={200}
+          amount={remainingBudget}
         />
 
         <AmountDisplay
           label="Gastado"
-          amount={100}
+          amount={totalExpenses}
         />
       </div>
     </div>
