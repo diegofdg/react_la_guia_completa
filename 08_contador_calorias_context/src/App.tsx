062697b6d205e61a127c1f9d47aef230ff8602c4
diegofdg <diegofdg@gmail.com>
2024-04-10 18:34:31 -0300

    08_contador_calorias_context-creando un hook para la comunicación del context y el reducer

--- 08_contador_calorias_context/src/App.tsx
+++ 08_contador_calorias_context/src/App.tsx
@@ -1,11 +1,12 @@
-import { useReducer, useEffect, useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
 import CalorieTracker from "./components/CalorieTracker"
-import { activityReducer, initialState } from "./reducers/activity-reducer"
+import { useActivity } from "./hooks/useActivity"
 
 function App() {
-  const [state, dispatch] = useReducer(activityReducer, initialState)
+  
+  const { state, dispatch} = useActivity()  
 
   useEffect(() => {
     localStorage.setItem("activities", JSON.stringify(state.activities))
