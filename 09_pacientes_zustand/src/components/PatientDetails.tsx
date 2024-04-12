aca5e7c4a91d215df60bde90b68398c8267e1b71
diegofdg <diegofdg@gmail.com>
2024-04-12 06:33:12 -0300

    09_pacientes_zustand-eliminar pacientes en zustand

--- 09_pacientes_zustand/src/components/PatientDetails.tsx
+++ 09_pacientes_zustand/src/components/PatientDetails.tsx
@@ -1,11 +1,13 @@
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
+import { usePatientStore } from "../store"
 
 type PatientDetailsProps = {
   patient: Patient
 }
 
 export default function PatientDetails({ patient }: PatientDetailsProps) {
+  const deletePatient = usePatientStore((state) => state.deletePatient)
 
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -26,6 +28,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => deletePatient(patient.id)}
         >
           Eliminar
         </button>
--- 09_pacientes_zustand/src/store.ts
+++ 09_pacientes_zustand/src/store.ts
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid"
 type PatientState = {
   patients: Patient[]
   addPatient: (data: DraftPatient) => void
+  deletePatient: (id: Patient["id"]) => void
 }
 
 const createPatient = (patient: DraftPatient): Patient => {
@@ -19,4 +20,9 @@ export const usePatientStore = create<PatientState>((set) => ({
       patients: [...state.patients, newPatient]
     }))
   },
+  deletePatient: (id) => {
+    set((state) => ({
+      patients: state.patients.filter(patient => patient.id !== id)
+    }))
+  }
 }))
