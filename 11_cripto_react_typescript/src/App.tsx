2696e84c37c29672b61dafecc2cbbd284137e32d
diegofdg <diegofdg@gmail.com>
2024-04-13 19:48:54 -0300

    11_cripto_react_typescript-primeros pasos con código css

--- 11_cripto_react_typescript/src/App.tsx
+++ 11_cripto_react_typescript/src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1>Cripto App</h1>
+       <div className="container">
+          <h1 className="app-title">
+              Cotizador de <span>Criptomonedas</span>
+          </h1>
+
+          <div className="content">
+              
+          </div>
+      </div>
     </>
   )
 }
