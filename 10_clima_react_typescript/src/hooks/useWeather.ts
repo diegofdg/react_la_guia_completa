f93d0707b8bf15d933d805d970d00b2c6a5884ef
diegofdg <diegofdg@gmail.com>
2024-04-13 07:55:05 -0300

    10_clima_react_typescript-segundo llamado a la api para obtener el clima

--- 10_clima_react_typescript/src/hooks/useWeather.ts
+++ 10_clima_react_typescript/src/hooks/useWeather.ts
@@ -6,10 +6,14 @@ export default function useWeather() {
   const fetchWeather = async (search: SearchType) => {
     try {
       const appId = import.meta.env.VITE_API_KEY
-      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
-      
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`      
       const {data} = await axios(geoUrl)
-      console.log(data)
+      
+      const lat = data[0].lat
+      const lon = data[0].lon
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+      const {data: weatherResult} = await axios(weatherUrl)
+      console.log(weatherResult)
     } catch (error) {
       console.log(error)
     }
