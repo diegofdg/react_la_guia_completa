6db3a3b69ca9f5d5063afacca382bcbd18b3c590
diegofdg <diegofdg@gmail.com>
2024-04-13 07:50:36 -0300

    10_clima_react_typescript-qué son las variables de entorno

--- 10_clima_react_typescript/src/hooks/useWeather.ts
+++ 10_clima_react_typescript/src/hooks/useWeather.ts
@@ -5,7 +5,7 @@ export default function useWeather() {
 
   const fetchWeather = async (search: SearchType) => {
     try {
-      const appId = "c45b5dd4c6b0c916817eed3d608c4b11"
+      const appId = import.meta.env.VITE_API_KEY
       const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
       
       const {data} = await axios(geoUrl)
