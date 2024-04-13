b5f1f7f13c4f6890c47a5d531610ea5a71a052c3
diegofdg <diegofdg@gmail.com>
2024-04-12 21:20:18 -0300

    10_clima_react_typescript-agregando los formularios para consultar el clima

--- 10_clima_react_typescript/src/App.tsx
+++ 10_clima_react_typescript/src/App.tsx
@@ -1,11 +1,12 @@
 import styles from "./App.module.css"
+import Form from "./components/Form/Form"
 
 function App() {
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
-        <p>1</p>
+        <Form />
         <p>2</p>
       </div>
     </>
---
+++ 10_clima_react_typescript/src/components/Form/Form.tsx
@@ -0,0 +1,38 @@
+import styles from "./Form.module.css"
+import { countries } from "../../data/countries"
+
+export default function Form() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="city">Ciudad:</label>
+        <input
+          id="city"
+          type="text"
+          name="city"
+          placeholder="Ciudad"
+        />
+      </div>
+
+      <div>
+        <label htmlFor="country">País:</label>
+        <select
+          id="country"
+          name="country"
+        >
+          <option value="">-- Seleccione un País ---</option>
+          {countries.map(country => (
+            <option
+              key={country.code}
+              value={country.code}
+            >
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input className={styles.submit} type="submit" value="Consultar Clima" />
+    </form>
+  )
+}
