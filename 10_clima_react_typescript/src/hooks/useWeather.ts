591d00f93185a5c238e50f6d618d5522de0f0d6f
diegofdg <diegofdg@gmail.com>
2024-04-13 12:36:12 -0300

    10_clima_react_typescript-tipar resultados de una api-type guards

--- 10_clima_react_typescript/src/hooks/useWeather.ts
+++ 10_clima_react_typescript/src/hooks/useWeather.ts
@@ -1,19 +1,37 @@
 import axios from "axios"
 import { SearchType, Weather } from "../types"
 
+function isWeatherResponse(weather: unknown) {
+  return (
+    Boolean(weather) &&
+    typeof weather === "object" &&
+    typeof (weather as Weather).name === "string" &&
+    typeof (weather as Weather).main.temp === "number" &&
+    typeof (weather as Weather).main.temp_max === "number" &&
+    typeof (weather as Weather).main.temp_min === "number"
+  )
+
+}
+
 export default function useWeather() {
 
   const fetchWeather = async (search: SearchType) => {
     try {
       const appId = import.meta.env.VITE_API_KEY
-      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`      
-      const {data} = await axios(geoUrl)
-      
+      const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+      const { data } = await axios(geoUrl)
+
       const lat = data[0].lat
       const lon = data[0].lon
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
-      const {data: weatherResult} = await axios<Weather>(weatherUrl)
-      console.log(weatherResult)
+
+      const { data: weatherResult } = await axios(weatherUrl)
+      const result = isWeatherResponse(weatherResult)
+      if (result) {
+        console.log(weatherResult.name)
+      } else {
+        console.log("Respuesta mal formada")
+      }
     } catch (error) {
       console.log(error)
     }
