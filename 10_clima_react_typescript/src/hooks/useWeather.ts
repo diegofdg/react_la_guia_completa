cf89659456860c36d71c9428e8a54e99064989eb
diegofdg <diegofdg@gmail.com>
2024-04-13 12:44:12 -0300

    10_clima_react_typescript-tipar resultados de una api-validar con zod

--- 10_clima_react_typescript/src/hooks/useWeather.ts
+++ 10_clima_react_typescript/src/hooks/useWeather.ts
@@ -1,17 +1,16 @@
 import axios from "axios"
-import { SearchType, Weather } from "../types"
+import { z } from "zod"
+import { SearchType } from "../types"
 
-function isWeatherResponse(weather: unknown) {
-  return (
-    Boolean(weather) &&
-    typeof weather === "object" &&
-    typeof (weather as Weather).name === "string" &&
-    typeof (weather as Weather).main.temp === "number" &&
-    typeof (weather as Weather).main.temp_max === "number" &&
-    typeof (weather as Weather).main.temp_min === "number"
-  )
-
-}
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  })
+})
+export type Weather = z.infer<typeof Weather>
 
 export default function useWeather() {
 
@@ -26,12 +25,11 @@ export default function useWeather() {
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
       const { data: weatherResult } = await axios(weatherUrl)
-      const result = isWeatherResponse(weatherResult)
-      if (result) {
-        console.log(weatherResult.name)
-      } else {
-        console.log("Respuesta mal formada")
+      const result = Weather.safeParse(weatherResult)
+      if (result.success) {
+        console.log(result.data)
       }
+
     } catch (error) {
       console.log(error)
     }
--- 10_clima_react_typescript/src/types/index.ts
+++ 10_clima_react_typescript/src/types/index.ts
@@ -7,12 +7,3 @@ export type Country = {
   code: string
   name: string
 }
-
-export type Weather = {
-  name: string
-  main: {
-    temp: number
-    temp_max: number
-    temp_min: number
-  }
-}
