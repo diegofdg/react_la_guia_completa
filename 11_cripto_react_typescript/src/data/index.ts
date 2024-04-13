0cc785c600f19ff76b6d84e7bce329c7247530ab
diegofdg <diegofdg@gmail.com>
2024-04-13 20:28:56 -0300

    11_cripto_react_typescript-agregando las monedas para cotizar criptos

---
+++ 11_cripto_react_typescript/src/data/index.ts
@@ -0,0 +1,8 @@
+import { Currency } from "../types";
+
+export const currencies: Currency[] = [
+  { code: "USD", name: "Dolar de Estados Unidos" },
+  { code: "ARS", nombre: "Peso Argentino" },
+  { code: "EUR", name: "Euro" },
+  { code: "GBP", name: "Libra Esterlina" },
+]
