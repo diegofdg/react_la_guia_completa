6e1e259116847133bf53a54d3bc85da424992728
diegofdg <diegofdg@gmail.com>
2024-04-12 21:14:03 -0300

    10_clima_react_typescript-introducción a css modules

--- 10_clima_react_typescript/src/App.tsx
+++ 10_clima_react_typescript/src/App.tsx
@@ -1,7 +1,13 @@
+import styles from "./App.module.css"
+
 function App() {
   return (
     <>
-      <h1>Buscador de clima</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
