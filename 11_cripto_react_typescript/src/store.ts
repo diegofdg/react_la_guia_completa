0348ce5d20d7ccff7c2ef2b450b6302a7eaeab97
diegofdg <diegofdg@gmail.com>
2024-04-13 20:51:00 -0300

    11_cripto_react_typescript-obteniendo las criptomonedas en zustand

--- 11_cripto_react_typescript/src/store.ts
+++ 11_cripto_react_typescript/src/store.ts
@@ -1,7 +1,14 @@
-import { create } from 'zustand'
+import axios from "axios"
+import { create } from "zustand"
+
+async function getCryptos() {
+  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+  const { data: { Data } } = await axios(url)
+  console.log(Data)
+}
 
 export const useCryptoStore = create(() => ({
   fetchCryptos: () => {
-    console.log("desde fetchcryptos")
+    getCryptos()
   }
 }))
