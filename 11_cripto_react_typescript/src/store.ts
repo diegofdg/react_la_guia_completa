a0d51df0f723c9c123808a41786b00f49b41d0d8
diegofdg <diegofdg@gmail.com>
2024-04-14 09:02:22 -0300

    11_cripto_react_typescript-finalizando la función que obtiene las criptos

--- 11_cripto_react_typescript/src/store.ts
+++ 11_cripto_react_typescript/src/store.ts
@@ -1,16 +1,28 @@
 import axios from "axios"
 import { create } from "zustand"
-import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema"
+import { CryptoCurrenciesResponseSchema, } from "./schema/crypto-schema"
+import { Cryptocurrency } from './types'
+
+type CryptoStore = {
+  cryptocurrencies: Cryptocurrency[]
+  fetchCryptos: () => Promise<void>
+}
 
 async function getCryptos() {
   const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
   const { data: { Data } } = await axios(url)
   const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-  console.log(result)
+  if (result.success) {
+    return result.data
+  }
 }
 
-export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {
-    getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+  cryptocurrencies: [],
+  fetchCryptos: async () => {
+    const cryptocurrencies = await getCryptos()
+    set(() => ({
+      cryptocurrencies
+    }))
   }
 }))
--- 11_cripto_react_typescript/src/types/index.ts
+++ 11_cripto_react_typescript/src/types/index.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod'
-import { CurrencySchema } from '../schema/crypto-schema'
+import { CurrencySchema, CryptoCurrencyResponseSchema } from '../schema/crypto-schema'
 
 export type Currency = z.infer<typeof CurrencySchema>
+export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>
