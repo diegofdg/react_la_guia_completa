407fe06129803a872e9823b4489a1d42260ae8a7
diegofdg <diegofdg@gmail.com>
2024-04-14 19:43:11 -0300

    12_bebidas_react_typescript-resaltando la página actual

--- 12_bebidas_react_typescript/src/components/Header.tsx
+++ 12_bebidas_react_typescript/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
   return (
@@ -10,18 +10,18 @@ export default function Header() {
           </div>
 
           <nav className="flex gap-4">
-            <Link
+            <NavLink
               to="/"
-              className="text-white uppercase font-bold"
+              className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"}
             >
               Inicio
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/favoritos"
-              className="text-white uppercase font-bold"
+              className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"}
             >
               Favoritos
-            </Link>
+            </NavLink>
           </nav>
         </div>
       </div>
