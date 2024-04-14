92473ac4f523a4079df793303891c5ec2ba2dfd8
diegofdg <diegofdg@gmail.com>
2024-04-14 09:25:37 -0300

    11_cripto_react_typescript-mostrando las criptomonedas en el formulario

--- 11_cripto_react_typescript/src/components/CriptoSearchForm.tsx
+++ 11_cripto_react_typescript/src/components/CriptoSearchForm.tsx
@@ -1,6 +1,9 @@
+import { useCryptoStore } from "../store"
 import { currencies } from "../data"
 
 export default function CriptoSearchForm() {
+  const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+
   return (
     <form className="form">
       <div className="field">
@@ -23,7 +26,12 @@ export default function CriptoSearchForm() {
           id="criptocurrency"
         >
           <option value="">-- Seleccione --</option>
-
+          {cryptocurrencies.map(crypto => (
+            <option
+              key={crypto.CoinInfo.FullName}
+              value={crypto.CoinInfo.Name}
+            >{crypto.CoinInfo.FullName}</option>
+          ))}
         </select>
       </div>
 
