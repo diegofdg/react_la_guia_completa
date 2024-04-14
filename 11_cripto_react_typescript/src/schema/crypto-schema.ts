2d708cb498304c7b8cd3cf1089af7bd89b831073
diegofdg <diegofdg@gmail.com>
2024-04-14 08:46:38 -0300

    11_cripto_react_typescript-validando la respuesta de la api con zod

--- 11_cripto_react_typescript/src/schema/crypto-schema.ts
+++ 11_cripto_react_typescript/src/schema/crypto-schema.ts
@@ -4,3 +4,12 @@ export const CurrencySchema = z.object({
   code: z.string(),
   name: z.string()
 })
+
+export const CryptoCurrencyResponseSchema = z.object({
+  CoinInfo: z.object({
+    FullName: z.string(),
+    Name: z.string()
+  })
+})
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
--- 11_cripto_react_typescript/src/store.ts
+++ 11_cripto_react_typescript/src/store.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { create } from "zustand"
+import { CryptoCurrenciesResponseSchema } from "./schema/crypto-schema"
 
 async function getCryptos() {
   const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
   const { data: { Data } } = await axios(url)
-  console.log(Data)
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+  console.log(result)
 }
 
 export const useCryptoStore = create(() => ({
