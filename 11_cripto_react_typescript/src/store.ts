6b5237616b4f7fb9bd6f760ea1edef439a071408
diegofdg <diegofdg@gmail.com>
2024-04-14 15:32:09 -0300

    11_cripto_react_typescript-colocando la respuesta en el state

--- 11_cripto_react_typescript/src/store.ts
+++ 11_cripto_react_typescript/src/store.ts
@@ -1,16 +1,25 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import { Cryptocurrency, Pair } from "./types"
+import { CryptoPrice, Cryptocurrency, Pair } from "./types"
 import { getCryptos, fetchCurrentCryptoPrice } from "./services/CryptoService"
 
 type CryptoStore = {
   cryptocurrencies: Cryptocurrency[]
+  result: CryptoPrice
   fetchCryptos: () => Promise<void>
   fetchData: (pair: Pair) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
   cryptocurrencies: [],
+  result: {
+    IMAGEURL: "",
+    PRICE: "",
+    HIGHDAY: "",
+    LOWDAY: "",
+    CHANGEPCT24HOUR: "",
+    LASTUPDATE: ""
+  },
   fetchCryptos: async () => {
     const cryptocurrencies = await getCryptos()
     set(() => ({
@@ -19,6 +28,8 @@ export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
   },
   fetchData: async (pair) => {
     const result = await fetchCurrentCryptoPrice(pair)
-    console.log(result)
+    set(() => ({
+      result
+    }))
   }
 })))
--- 11_cripto_react_typescript/src/types/index.ts
+++ 11_cripto_react_typescript/src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
-import { CurrencySchema, CryptoCurrencyResponseSchema, PairSchema } from "../schema/crypto-schema"
+import { CurrencySchema, CryptoCurrencyResponseSchema, PairSchema, CryptoPriceSchema } from "../schema/crypto-schema"
 
 export type Currency = z.infer<typeof CurrencySchema>
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>
 export type Pair = z.infer<typeof PairSchema>
+export type CryptoPrice = z.infer<typeof CryptoPriceSchema>
