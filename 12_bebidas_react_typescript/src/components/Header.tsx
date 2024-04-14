74cb1ead3d51c3bc6c6545afb6d3a49a19126103
diegofdg <diegofdg@gmail.com>
2024-04-14 19:40:58 -0300

    12_bebidas_react_typescript-agregando navegación entre páginas

--- 12_bebidas_react_typescript/src/components/Header.tsx
+++ 12_bebidas_react_typescript/src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
   return (
     <header className="bg-slate-800">
@@ -7,8 +9,19 @@ export default function Header() {
             <img className="w-32" src="/logo.svg" alt="logotipo" />
           </div>
 
-          <nav>
-
+          <nav className="flex gap-4">
+            <Link
+              to="/"
+              className="text-white uppercase font-bold"
+            >
+              Inicio
+            </Link>
+            <Link
+              to="/favoritos"
+              className="text-white uppercase font-bold"
+            >
+              Favoritos
+            </Link>
           </nav>
         </div>
       </div>
