0adaff8dad9c42488b409804eec733a5151d750f
diegofdg <diegofdg@gmail.com>
2024-04-14 09:10:35 -0300

    11_cripto_react_typescript-moviendo la función que obtiene la api hacia otro archivo

---
+++ 11_cripto_react_typescript/src/services/CryptoService.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+import { CryptoCurrenciesResponseSchema } from "../schema/crypto-schema"
+
+export async function getCryptos() {
+  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+  const { data: { Data } } = await axios(url)
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+  if (result.success) {
+    return result.data
+  }
+}
--- 11_cripto_react_typescript/src/store.ts
+++ 11_cripto_react_typescript/src/store.ts
@@ -1,21 +1,11 @@
-import axios from "axios"
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import { CryptoCurrenciesResponseSchema, } from "./schema/crypto-schema"
 import { Cryptocurrency } from "./types"
+import { getCryptos } from "./services/CryptoService"
 
 type CryptoStore = {
   cryptocurrencies: Cryptocurrency[]
   fetchCryptos: () => Promise<void>
-}
-
-async function getCryptos() {
-  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
-  const { data: { Data } } = await axios(url)
-  const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-  if (result.success) {
-    return result.data
-  }
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
