1455785ccdbcb333e463390e6737fa9ead7ecedc
diegofdg <diegofdg@gmail.com>
2024-04-15 19:43:02 -0300

    12_bebidas_react_typescript-colocando en el state la receta y su contenido

--- 12_bebidas_react_typescript/src/stores/recipeSlice.ts
+++ 12_bebidas_react_typescript/src/stores/recipeSlice.ts
@@ -1,10 +1,11 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
-import type { Categories, Drink, Drinks, SearchFilter } from "../types"
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
   categories: Categories
   drinks: Drinks
+  selectedRecipe: Recipe
   fetchCategories: () => Promise<void>
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>
   selectRecipe: (id: Drink["idDrink"]) => Promise<void>
@@ -17,6 +18,7 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
   drinks: {
     drinks: []
   },
+  selectedRecipe: {} as Recipe,
   fetchCategories: async () => {
     const categories = await getCategories()
     set({
@@ -31,6 +33,8 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
   },
   selectRecipe: async (id) => {
     const selectedRecipe = await getRecipeById(id)
-    console.log(selectedRecipe)
+    set({
+      selectedRecipe
+    })
   },
 })
