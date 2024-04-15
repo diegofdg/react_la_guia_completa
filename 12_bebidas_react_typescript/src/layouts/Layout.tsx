7e6a706c5935edd37fa0703a227dc62f1138b083
diegofdg <diegofdg@gmail.com>
2024-04-15 19:52:49 -0300

    12_bebidas_react_typescript-agregando headless ui para mostrar las recetas

--- 12_bebidas_react_typescript/src/layouts/Layout.tsx
+++ 12_bebidas_react_typescript/src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
+import Modal from "../components/Modal"
 
 export default function Layout() {
 
@@ -9,6 +10,7 @@ export default function Layout() {
       <main className="container mx-auto py-16">
         <Outlet />
       </main>
+      <Modal />
     </>
   )
 }
--- 12_bebidas_react_typescript/src/stores/recipeSlice.ts
+++ 12_bebidas_react_typescript/src/stores/recipeSlice.ts
@@ -6,9 +6,11 @@ export type RecipesSliceType = {
   categories: Categories
   drinks: Drinks
   selectedRecipe: Recipe
+  modal: boolean
   fetchCategories: () => Promise<void>
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>
   selectRecipe: (id: Drink["idDrink"]) => Promise<void>
+  closeModal: () => void
 }
 
 export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
@@ -19,6 +21,7 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     drinks: []
   },
   selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async () => {
     const categories = await getCategories()
     set({
@@ -34,7 +37,14 @@ export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
   selectRecipe: async (id) => {
     const selectedRecipe = await getRecipeById(id)
     set({
-      selectedRecipe
+      selectedRecipe,
+      modal: true
     })
   },
+  closeModal: () => {
+    set({
+      modal: false,
+      selectedRecipe: {} as Recipe
+    })
+  }
 })
