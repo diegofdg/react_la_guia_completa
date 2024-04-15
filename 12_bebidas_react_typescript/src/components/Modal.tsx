b28c9ad019b35b38bf0bb4de3607dd677a0f482d
diegofdg <diegofdg@gmail.com>
2024-04-15 20:00:18 -0300

    12_bebidas_react_typescript-mostrando el contenido de la receta en el modal

--- 12_bebidas_react_typescript/src/components/Modal.tsx
+++ 12_bebidas_react_typescript/src/components/Modal.tsx
@@ -5,7 +5,8 @@ import { useAppStore } from "../stores/useAppStore";
 export default function Modal() {
   const modal = useAppStore((state) => state.modal)
   const closeModal = useAppStore((state) => state.closeModal)
-  
+  const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+
   return (
     <>
       <Transition appear show={modal} as={Fragment}>
@@ -35,14 +36,20 @@ export default function Modal() {
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
                   <Dialog.Title as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
-                    Titulo Aquí
+                    {selectedRecipe.strDrink}
                   </Dialog.Title>
+                  <img
+                    src={selectedRecipe.strDrinkThumb}
+                    alt={`Imagen de ${selectedRecipe.strDrink}`}
+                    className="mx-auto w-96"
+                  />
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Ingredientes y Cantidades
                   </Dialog.Title>
                   <Dialog.Title as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Instrucciones
                   </Dialog.Title>
+                  <p className="text-lg">{selectedRecipe.strInstructions}</p>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
