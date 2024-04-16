3b44dd37f29d69c525290994bb3a2658cfa45b0a
diegofdg <diegofdg@gmail.com>
2024-04-16 20:02:59 -0300

    12_bebidas_react_typescript-mostrando de forma condicional el texto agregar o eliminar favoritos

--- 12_bebidas_react_typescript/src/components/Modal.tsx
+++ 12_bebidas_react_typescript/src/components/Modal.tsx
@@ -8,6 +8,7 @@ export default function Modal() {
   const closeModal = useAppStore((state) => state.closeModal)
   const selectedRecipe = useAppStore((state) => state.selectedRecipe)
   const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+  const favoriteExists = useAppStore((state) => state.favoriteExists)
 
   const renderIngredients = () => {
     const ingredients: JSX.Element[] = []
@@ -85,7 +86,7 @@ export default function Modal() {
                       className="w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500"                      
                       onClick={() => handleClickFavorite(selectedRecipe)}
                     >
-                      Agregar a Favoritos
+                      {favoriteExists(selectedRecipe.idDrink) ? "Eliminar Favorito" : "Agregar a Favoritos"}
                     </button>
                   </div>
                 </Dialog.Panel>
--- 12_bebidas_react_typescript/src/stores/favoritesSlice.ts
+++ 12_bebidas_react_typescript/src/stores/favoritesSlice.ts
@@ -4,22 +4,24 @@ import { Recipe } from "../types"
 export type FavoritesSliceType = {
   favorites: Recipe[]
   handleClickFavorite: (recipe: Recipe) => void
+  favoriteExists: (id: Recipe["idDrink"]) => boolean
 }
 
 export const createFavoritesSlice: StateCreator<FavoritesSliceType> = (set, get) => ({
   favorites: [],
   handleClickFavorite: (recipe) => {
-    if (get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)) {
-      console.log("Si está")
+    if(get().favoriteExists(recipe.idDrink)) {
       set((state) => ({
         favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
       }))
     } else {
-      console.log("No está")
       set((state) => ({
         favorites: [...state.favorites, recipe]
       }))
     }
-  }
+  },
+  favoriteExists: (id) => {
+    return get().favorites.some(favorite => favorite.idDrink === id)
+  },
 
 })
