4991dfa4d92425392cd59d544297e57672984b3f
diegofdg <diegofdg@gmail.com>
2024-04-18 15:53:27 -0300

    12_bebidas_react_typescript-llamando el slice de notificación para mostrarla

--- 12_bebidas_react_typescript/src/stores/notificationSlice.ts
+++ 12_bebidas_react_typescript/src/stores/notificationSlice.ts
@@ -1,4 +1,5 @@
 import { StateCreator } from "zustand"
+import { FavoritesSliceType } from "./favoritesSlice"
 
 type Notification = {
   text: string
@@ -8,12 +9,22 @@ type Notification = {
 
 export type NotificationSliceType = {
   notification: Notification
+  showNotification: (payload: Pick<Notification, "text" | "error">) => void
 }
 
-export const createNotificationSlice: StateCreator<NotificationSliceType> = (set, get) => ({
+export const createNotificationSlice: StateCreator<NotificationSliceType & FavoritesSliceType, [], [], NotificationSliceType> = (set, get) => ({
   notification: {
     text: "",
     error: false,
     show: false
+  },
+  showNotification: (payload) => {
+    set({
+      notification: {
+        text: payload.text,
+        error: payload.error,
+        show: true
+      }
+    })
   },
 })
