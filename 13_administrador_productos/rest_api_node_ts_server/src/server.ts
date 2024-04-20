8998f31159f9cc61bb88e0678aba98a7225b58df
diegofdg <diegofdg@gmail.com>
2024-04-20 15:53:39 -0300

    13_administrador_productos-diferentes métodos http

--- 13_administrador_productos/rest_api_node_ts_server/src/server.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/server.ts
@@ -5,7 +5,23 @@ const server = express()
 
 // Routing
 server.get('/', (req, res) => {
-  res.send("Hola Mundo en express")
+  res.send("Desde GET")
+})
+
+server.post('/', (req, res) => {
+  res.send("Desde POST")
+})
+
+server.put('/', (req, res) => {
+  res.send("Desde PUT")
+})
+
+server.patch('/', (req, res) => {
+  res.send("Desde PATCH")
+})
+
+server.delete('/', (req, res) => {
+  res.send("Desde DELETE")
 })
 
 export default server
