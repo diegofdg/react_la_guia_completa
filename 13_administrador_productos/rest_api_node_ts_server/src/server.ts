eef93f6a9b3c9e0c24d3a5b22414b100aac338a8
diegofdg <diegofdg@gmail.com>
2024-04-21 07:30:06 -0300

    13_administrador_productos-agregando productos a la base de datos

--- 13_administrador_productos/rest_api_node_ts_server/src/server.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/server.ts
@@ -20,6 +20,9 @@ connectDB()
 // Instancia de express
 const server = express()
 
+// Leer datos de formularios
+server.use(express.json())
+
 server.use("/api/products", router)
 
 export default server
