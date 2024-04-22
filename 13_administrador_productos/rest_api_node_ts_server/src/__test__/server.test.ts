9fb898d84b4f910a144bf4fae27a82813cfd0b53
diegofdg <diegofdg@gmail.com>
2024-04-22 20:09:56 -0300

    13_administrador_productos-negando una condición para evitar falsos positivos

--- 13_administrador_productos/rest_api_node_ts_server/src/__test__/server.test.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/__test__/server.test.ts
@@ -7,6 +7,10 @@ describe("GET /api", () => {
     expect(res.status).toBe(200)
     expect(res.headers["content-type"]).toMatch(/json/)
     expect(res.body.msg).toBe("Desde API")
+
+    expect(res.status).not.toBe(404)
+    expect(res.body.msg).not.toBe("desde api")
+
   })
 })
 
