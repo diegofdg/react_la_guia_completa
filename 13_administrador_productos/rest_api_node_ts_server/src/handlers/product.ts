2b39f6e62e3a786f334bf1a7c74ca642e17b54aa
diegofdg <diegofdg@gmail.com>
2024-04-22 06:33:37 -0300

    13_administrador_productos-moviendo la validación hacia el router

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/product.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/product.ts
@@ -3,20 +3,11 @@ import Product from "../models/Product.model"
 import { check, validationResult } from 'express-validator'
 
 export const createProduct = async (req: Request, res: Response) => {
-  await check("name")
-    .notEmpty()
-    .withMessage("El nombre del producto no puede ir vacío")
-    .run(req)
-  await check("precio")
-    .isNumeric().withMessage("Valor no válido")
-    .notEmpty().withMessage("El precio del producto no puede ir vacío")
-    .custom(value => value > 0).withMessage("Precio no válido")
-    .run(req)
   let errors = validationResult(req)
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() })
   }
-
+  
   try {
     const product = await Product.create(req.body)
     res.json({ data: product })
--- 13_administrador_productos/rest_api_node_ts_server/src/router.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { body } from "express-validator"
 import { createProduct } from "./handlers/product"
 
 const router = Router()
@@ -7,7 +8,16 @@ router.get("/", (req, res) => {
   res.send("Desde GET")
 })
 
-router.post("/", createProduct)
+router.post("/",
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del producto no puede ir vacío"),
+  body("precio")
+    .isNumeric().withMessage("Valor no válido")
+    .notEmpty().withMessage("El precio del producto no puede ir vacío")
+    .custom(value => value > 0).withMessage("Precio no válido"),
+  createProduct
+)
 
 router.put("/", (req, res) => {
   res.send("Desde PUT")
