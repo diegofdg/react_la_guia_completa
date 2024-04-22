5c04ac43e25486447290ab9cf8b1ea22760180de
diegofdg <diegofdg@gmail.com>
2024-04-22 07:04:45 -0300

    13_administrador_productos-creando el endpoint para el endpoint para actualizar

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/product.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/product.ts
@@ -39,3 +39,19 @@ export const createProduct = async (req: Request, res: Response) => {
     console.log(error)
   }
 }
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const product = await Product.findByPk(id)
+
+  if (!product) {
+    return res.status(404).json({
+      error: "Producto No Encontrado"
+    })
+  }
+
+  // Actualizar
+  await product.update(req.body)
+  await product.save()
+  res.json({ data: product })
+}
--- 13_administrador_productos/rest_api_node_ts_server/src/router.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { body, param } from "express-validator"
-import { getProducts, getProductById, createProduct } from "./handlers/product"
+import { getProducts, getProductById, createProduct, updateProduct } from "./handlers/product"
 import { handleInputErrors } from "./middleware"
 
 const router = Router()
@@ -23,9 +23,19 @@ router.post("/",
   createProduct
 )
 
-router.put("/", (req, res) => {
-  res.send("Desde PUT")
-})
+router.put("/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  body("name")
+    .notEmpty().withMessage("El nombre de Producto no puede ir vacio"),
+  body("price")
+    .isNumeric().withMessage("Valor no válido")
+    .notEmpty().withMessage("El precio de Producto no puede ir vacio")
+    .custom(value => value > 0).withMessage("Precio no válido"),
+  body("availability")
+    .isBoolean().withMessage("Valor para disponibilidad no válido"),
+  handleInputErrors,
+  updateProduct
+)
 
 router.patch("/", (req, res) => {
   res.send("Desde PATCH")
