3ba0f772fde0eb4131c647f0b775b059fb09e015
diegofdg <diegofdg@gmail.com>
2024-04-22 20:31:29 -0300

    13_administrador_productos-probando validación de crear producto

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
@@ -2,12 +2,22 @@ import request from "supertest"
 import server from "../../server"
 
 describe("POST /api/products", () => {
+  it("should display validation errors", async () => {
+    const response = await request(server).post("/api/products").send({})
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty("errors")
+    expect(response.body.errors).toHaveLength(4)
+
+    expect(response.status).not.toBe(404)
+    expect(response.body.errors).not.toHaveLength(1)
+  })
+
   it("should create a new product", async () => {
     const response = await request(server).post("/api/products").send({ name: "Producto Test", price: 100 })
     expect(response.status).toBe(201)
     expect(response.body).toHaveProperty("data")
 
     expect(response.status).not.toBe(404)
-    expect(response.body).not.toHaveProperty("error")
+    expect(response.body).not.toHaveProperty("errors")
   })
 })
