3ec89976234f579fc470d64d7777f73abbe7f563
diegofdg <diegofdg@gmail.com>
2024-04-23 07:34:44 -0300

    13_administrador_productos-probando producto por id

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
@@ -57,3 +57,27 @@ describe("GET /api/products", () => {
     expect(response.body).not.toHaveProperty("errors")
   })
 })
+
+describe("GET /api/products/:id", () => {
+  it("should return a 404 response for a non-existent product", async () => {
+    const productId = 2000
+    const response = await request(server).get(`/api/products/${productId}`)
+    expect(response.status).toBe(404)
+    expect(response.body).toHaveProperty("error")
+    expect(response.body.error).toBe("Producto No Encontrado")
+  })
+
+  it("should check a valid ID in the URL", async () => {
+    const response = await request(server).get("/api/products/not-valid-url")
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty("errors")
+    expect(response.body.errors).toHaveLength(1)
+    expect(response.body.errors[0].msg).toBe("ID no válido")
+  })
+
+  it("get a JSON response for a single product", async () => {
+    const response = await request(server).get("/api/products/1")
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("data")
+  })
+})
