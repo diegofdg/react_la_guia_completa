f3912e5deaf37d4d08b542deacef7f0dfb976d4b
diegofdg <diegofdg@gmail.com>
2024-04-23 15:19:56 -0300

    13_administrador_productos-probar otras funciones

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
@@ -131,6 +131,26 @@ describe("PUT /api/products/:id", () => {
   })
 })
 
+describe("PATCH /api/products/:id", () => {
+  it("should return a 404 response for a non-existing product", async () => {
+    const productId = 2000
+    const response = await request(server).patch(`/api/products/${productId}`)
+    expect(response.status).toBe(404)
+    expect(response.body.error).toBe("Producto No Encontrado")
+    expect(response.status).not.toBe(200)
+    expect(response.body).not.toHaveProperty("data")
+  })
+
+  it("should update the product availability", async () => {
+    const response = await request(server).patch("/api/products/1")
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("data")
+    expect(response.body.data.availability).toBe(false)
+    expect(response.body).not.toHaveProperty("error")
+    expect(response.status).not.toBe(400)
+  })
+})
+
 describe("DELETE /api/products/:id", () => {
   it("should check a valid id", async () => {
     const response = await request(server).delete("/api/products/not-valid-url")
