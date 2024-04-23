7bf58ad92e9f45e436071f0b7c2fac4bfd45577f
diegofdg <diegofdg@gmail.com>
2024-04-23 06:58:01 -0300

    13_administrador_productos-validar que el precio sea mayor a cero

--- 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/handlers/__test__/product.test.ts
@@ -12,6 +12,26 @@ describe("POST /api/products", () => {
     expect(response.body.errors).not.toHaveLength(1)
   })
 
+  it("should validate that the price is greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({ name: "Monitor", price: 0})
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty("errors")
+    expect(response.body.errors).toHaveLength(1)
+
+    expect(response.status).not.toBe(404)
+    expect(response.body.errors).not.toHaveLength(4)
+  })
+
+  it("should validate that the price is a number and greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({ name: "Monitor", price: "Hola"})
+    expect(response.status).toBe(400)
+    expect(response.body).toHaveProperty("errors")
+    expect(response.body.errors).toHaveLength(2)
+
+    expect(response.status).not.toBe(404)
+    expect(response.body.errors).not.toHaveLength(1)
+  })
+
   it("should create a new product", async () => {
     const response = await request(server).post("/api/products").send({ name: "Producto Test", price: 100 })
     expect(response.status).toBe(201)
