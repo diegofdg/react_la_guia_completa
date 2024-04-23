801cd269a5cd73e23034fb4f309b66df6b04b757
diegofdg <diegofdg@gmail.com>
2024-04-23 16:11:48 -0300

    13_administrador_productos-documentando todos los productos

--- 13_administrador_productos/rest_api_node_ts_server/src/router.ts
+++ 13_administrador_productos/rest_api_node_ts_server/src/router.ts
@@ -31,6 +31,24 @@ const router = Router()
 *           example: true
 */
 
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Get a list of products
+ *     tags:
+ *       - Products
+ *     description: Return a list of products
+ *     responses:
+ *       200:
+ *         description: Succesfull response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 router.get("/", getProducts)
 router.get("/:id",
   param("id").isInt().withMessage("ID no válido"),
