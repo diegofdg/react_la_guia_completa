56bfea4d886be78eff641189205c6791cee58f33
diegofdg <diegofdg@gmail.com>
2024-04-24 07:16:39 -0300

    13_administrador_productos-validando los datos con valibot antes de enviarlos a la rest api

--- 13_administrador_productos/client/src/services/ProductService.ts
+++ 13_administrador_productos/client/src/services/ProductService.ts
@@ -1,7 +1,25 @@
+import { safeParse } from "valibot"
+import { DraftProductSchema } from "../types"
+
 type ProductData = {
   [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data: ProductData) {
-  console.log(data)
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price
+    })
+    if (result.success) {
+      console.log("Pasó la validación")
+    } else {
+      throw new Error("
+      Datos no válidos"
+        
+      )
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
