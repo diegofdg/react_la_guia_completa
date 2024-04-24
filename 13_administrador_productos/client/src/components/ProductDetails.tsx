8d8b9da22259deef4064b3993aff31e7c729b2a5
diegofdg <diegofdg@gmail.com>
2024-04-24 19:10:49 -0300

    13_administrador_productos-mostrando los productos

---
+++ 13_administrador_productos/client/src/components/ProductDetails.tsx
@@ -0,0 +1,23 @@
+import { Product } from "../types"
+import { formatCurrency } from "../utils"
+
+type ProductDetailsProps = {
+  product: Product
+}
+
+export default function ProductDetails({ product }: ProductDetailsProps) {
+  const isAvailable = product.availability
+  return (
+    <tr className="border-b ">
+      <td className="p-3 text-lg text-gray-800">
+        {product.name}
+      </td>
+      <td className="p-3 text-lg text-gray-800">
+        {formatCurrency(product.price)}
+      </td>
+      <td className="p-3 text-lg text-gray-800">
+        {isAvailable ? "Disponible" : "No Disponible"}
+      </td>
+    </tr>
+  )
+}
--- 13_administrador_productos/client/src/views/Products.tsx
+++ 13_administrador_productos/client/src/views/Products.tsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom"
 import { getProducts } from "../services/ProductService"
+import ProductDetails from "../components/ProductDetails"
+import { Product } from "../types"
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function loader() {
   const products = await getProducts()
-  console.log(products)
-  return {}
+  return products
 }
 
 export default function Products() {
+  const data = useLoaderData() as Product[]
+
   return (
     <>
       <div className="flex justify-between">
@@ -19,6 +22,26 @@ export default function Products() {
         >
           Agregar Producto
         </Link>
+      </div>
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(product => (
+              <ProductDetails
+                key={product.id}
+                product={product}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   )
