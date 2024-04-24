c5e082fb373473f79f42925f0f1a32d0d1f8b981
diegofdg <diegofdg@gmail.com>
2024-04-24 06:47:06 -0300

    13_administrador_productos-routing entre diferentes páginas

--- 13_administrador_productos/client/src/views/NewProduct.tsx
+++ 13_administrador_productos/client/src/views/NewProduct.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar Producto</h2>
+        <Link
+          to="/"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+          Volver a Productos
+        </Link>
+      </div>
+    </>
   )
 }
--- 13_administrador_productos/client/src/views/Products.tsx
+++ 13_administrador_productos/client/src/views/Products.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to="productos/nuevo"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+          Agregar Producto
+        </Link>
+      </div>
+    </>
   )
 }
