f1bad48df3604057a14b2777f23f09a309f4ee0b
diegofdg <diegofdg@gmail.com>
2024-04-24 07:37:14 -0300

    13_administrador_productos-agregando morgan para el logging en el backend

--- 13_administrador_productos/server/src/server.ts
+++ 13_administrador_productos/server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import colors from "colors"
 import cors, { CorsOptions } from "cors"
+import morgan from "morgan"
 import swaggerUi from "swagger-ui-express"
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger"
 import router from "./router"
@@ -37,6 +38,8 @@ server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
+
+server.use(morgan("dev"))
 
 server.use("/api/products", router)
 
