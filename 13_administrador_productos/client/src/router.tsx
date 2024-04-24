81ca6081cda950de4954d2912845728859b826eb
diegofdg <diegofdg@gmail.com>
2024-04-24 06:54:15 -0300

    13_administrador_productos-qué son las acciones del data router

--- 13_administrador_productos/client/src/router.tsx
+++ 13_administrador_productos/client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import Products from "./views/Products"
-import NewProduct from "./views/NewProduct"
+import NewProduct, { action as newProductAction } from "./views/NewProduct"
 
 export const router = createBrowserRouter([
   {
@@ -13,8 +13,9 @@ export const router = createBrowserRouter([
         element: <Products />
       },
       {
-        path: 'productos/nuevo',
-        element: <NewProduct />
+        path: "productos/nuevo",
+        element: <NewProduct />,
+        action: newProductAction
       }
     ],
   }
--- 13_administrador_productos/client/src/views/NewProduct.tsx
+++ 13_administrador_productos/client/src/views/NewProduct.tsx
@@ -1,4 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, Form } from "react-router-dom"
+
+export async function action() {
+  console.log("Desde action")
+  return {}
+}
 
 export default function NewProduct() {
   return (
@@ -12,6 +17,44 @@ export default function NewProduct() {
           Volver a Productos
         </Link>
       </div>
+
+      <Form
+        className="mt-10"
+        method="POST"
+      >
+
+        <div className="mb-4">
+          <label
+            className="text-gray-800"
+            htmlFor="name"
+          >Nombre Producto:</label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="text-gray-800"
+            htmlFor="price"
+          >Precio:</label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
   )
 }
