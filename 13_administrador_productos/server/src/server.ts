58054a8cf75c30ff352986b34d2a50d8b4cb1e76
diegofdg <diegofdg@gmail.com>
2024-04-24 07:30:53 -0300

    13_administrador_productos-habilitando cors en el backend

--- 13_administrador_productos/server/src/server.ts
+++ 13_administrador_productos/server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import colors from "colors"
+import cors, { CorsOptions } from "cors"
 import swaggerUi from "swagger-ui-express"
 import swaggerSpec, { swaggerUiOptions } from "./config/swagger"
 import router from "./router"
@@ -21,6 +22,18 @@ connectDB()
 
 // Instancia de express
 const server = express()
+
+// Permitir conexiones
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error("Error de CORS"))
+    }
+  }
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
