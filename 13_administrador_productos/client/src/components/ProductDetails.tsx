4a8a9c89f8bafdfe8ec0b1fdb5bdc0364843810d
diegofdg <diegofdg@gmail.com>
2024-04-25 07:28:41 -0300

    13_administrador_productos-habilitando editar internamente

--- 13_administrador_productos/client/src/components/ProductDetails.tsx
+++ 13_administrador_productos/client/src/components/ProductDetails.tsx
@@ -24,7 +24,11 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       <td className="p-3 text-lg text-gray-800 ">
         <div className="flex gap-2 items-center">
           <button
-            onClick={() => navigate(`/productos/${product.id}/editar`)}
+            onClick={() => navigate(`/productos/${product.id}/editar`, {
+              state: {
+                product
+              }
+            })}
             className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
           >Editar</button>
         </div>
--- 13_administrador_productos/client/src/views/EditProduct.tsx
+++ 13_administrador_productos/client/src/views/EditProduct.tsx
@@ -1,4 +1,4 @@
-import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom"
+import { Link, Form, useActionData, ActionFunctionArgs, redirect, useLocation } from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
 import { addProduct } from "../services/ProductService"
 
@@ -18,6 +18,7 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function EditProduct() {
   const error = useActionData() as string
+  const { state } = useLocation()
 
   return (
     <>
@@ -49,6 +50,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Nombre del Producto"
             name="name"
+            defaultValue={state.product.name}
           />
         </div>
         <div className="mb-4">
@@ -62,6 +64,7 @@ export default function EditProduct() {
             className="mt-2 block w-full p-3 bg-gray-50"
             placeholder="Precio Producto. ej. 200, 300"
             name="price"
+            defaultValue={state.product.price}
           />
         </div>
         <input
