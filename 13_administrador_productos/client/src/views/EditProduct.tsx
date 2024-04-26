9bee328a7bcded3d6c0c82a52c5bd68780273cc4
diegofdg <diegofdg@gmail.com>
2024-04-26 07:04:22 -0300

    13_administrador_productos-agregando la disponibilidad en la edición

--- 13_administrador_productos/client/src/views/EditProduct.tsx
+++ 13_administrador_productos/client/src/views/EditProduct.tsx
@@ -8,7 +8,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
   if (params.id !== undefined) {
     const product = await getProductById(+params.id)
     if (!product) {
-      return redirect('/')
+      return redirect("/")
     }
     return product
   }
@@ -29,6 +29,11 @@ export async function action({ request, params }: ActionFunctionArgs) {
     return redirect("/")
   }
 }
+
+const availabilityOptions = [
+  { name: "Disponible", value: true },
+  { name: "No Disponible", value: false }
+]
 
 export default function EditProduct() {
   const product = useLoaderData() as Product
@@ -80,6 +85,22 @@ export default function EditProduct() {
             name="price"
             defaultValue={product.price}
           />
+        </div>
+        <div className="mb-4">
+          <label
+            className="text-gray-800"
+            htmlFor="availability"
+          >Disponibilidad:</label>
+          <select
+            id="availability"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map(option => (
+              <option key={option.name} value={option.value.toString()}>{option.name}</option>
+            ))}
+          </select>
         </div>
         <input
           type="submit"
