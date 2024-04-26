cf513cf7088594165ada04ffb99488781fed587c
diegofdg <diegofdg@gmail.com>
2024-04-26 07:25:26 -0300

    13_administrador_productos-creando un componente de formulario reutilizable

--- 13_administrador_productos/client/src/views/NewProduct.tsx
+++ 13_administrador_productos/client/src/views/NewProduct.tsx
@@ -1,6 +1,7 @@
 import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom"
 import ErrorMessage from "../components/ErrorMessage"
 import { addProduct } from "../services/ProductService"
+import ProductForm from "../components/ProductForm"
 
 // eslint-disable-next-line react-refresh/only-export-components
 export async function action({ request }: ActionFunctionArgs) {
@@ -37,33 +38,7 @@ export default function NewProduct() {
         className="mt-10"
         method="POST"
       >
-
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="name"
-          >Nombre Producto:</label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="price"
-          >Precio:</label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
-        </div>
+        <ProductForm />
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
