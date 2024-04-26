ef06f4356c29196aa659c0150add02780dcbf071
diegofdg <diegofdg@gmail.com>
2024-04-26 19:39:17 -0300

    13_administrador_productos-primeros pasos con eliminar

--- 13_administrador_productos/client/src/components/ProductDetails.tsx
+++ 13_administrador_productos/client/src/components/ProductDetails.tsx
@@ -1,9 +1,15 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Form, ActionFunctionArgs, redirect } from "react-router-dom"
 import { Product } from "../types"
 import { formatCurrency } from "../utils"
 
 type ProductDetailsProps = {
   product: Product
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({ params }: ActionFunctionArgs) {
+  console.log(params.id)
+  return redirect("/")
 }
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
@@ -27,6 +33,17 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
             onClick={() => navigate(`/productos/${product.id}/editar`)}
             className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
           >Editar</button>
+          <Form
+            className="w-full"
+            method="POST"
+            action={`productos/${product.id}/eliminar`}
+          >
+            <input
+              type="submit"
+              value="Eliminar"
+              className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+            />
+          </Form>
         </div>
       </td>
     </tr>
--- 13_administrador_productos/client/src/router.tsx
+++ 13_administrador_productos/client/src/router.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/Layout"
 import Products, { loader as productsLoader } from "./views/Products"
 import NewProduct, { action as newProductAction } from "./views/NewProduct"
 import EditProduct, { loader as editProductLoader, action as editProductAction } from "./views/EditProduct"
+import { action as deleteProductAction} from "./components/ProductDetails"
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
         element: <EditProduct />,
         loader: editProductLoader,
         action: editProductAction
+      }, 
+      {
+          path:'productos/:id/eliminar',
+          action: deleteProductAction
       }
     ],
   }
