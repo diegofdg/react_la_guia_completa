3292b2e5f9daccd1649961e67f66f9b93372e363
diegofdg <diegofdg@gmail.com>
2024-04-27 17:56:51 -0300

    14_uptask_mern-agregando validación en el servidor a los proyectos

--- 14_uptask_mern/backend/src/routes/projectRoutes.ts
+++ 14_uptask_mern/backend/src/routes/projectRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express"
+import { body } from "express-validator"
 import { ProjectController } from "../controllers/ProjectController"
+import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
 
-router.post("/", ProjectController.createProject)
+router.post("/",
+  body("projectName")
+    .notEmpty().withMessage("El Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty().withMessage("El Nombre del Cliente es Obligatorio"),
+  body("description")
+    .notEmpty().withMessage("La Descripción del Proyecto es Obligatoria"),
+  handleInputErrors,
+  ProjectController.createProject
+)
 
 router.get("/", ProjectController.getAllProjects)
 
