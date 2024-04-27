9893dce14c0dea2e0cf242bc8899febe76a0bac6
diegofdg <diegofdg@gmail.com>
2024-04-27 18:16:08 -0300

    14_uptask_mern-creando el endpoint de actualizar un proyecto

--- 14_uptask_mern/backend/src/controllers/ProjectController.ts
+++ 14_uptask_mern/backend/src/controllers/ProjectController.ts
@@ -36,4 +36,19 @@ export class ProjectController {
       console.log(error)
     }
   }
+
+  static updateProject = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body)
+      if (!project) {
+        const error = new Error("Proyecto no encontrado")
+        return res.status(404).json({ error: error.message })
+      }
+      await project.save()
+      res.json("Proyecto actualizado")
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
--- 14_uptask_mern/backend/src/routes/projectRoutes.ts
+++ 14_uptask_mern/backend/src/routes/projectRoutes.ts
@@ -24,4 +24,17 @@ router.get("/:id",
   ProjectController.getProjectById
 )
 
+router.put("/:id",
+  param("id")
+    .isMongoId().withMessage("ID no válido"),
+  body("projectName")
+    .notEmpty().withMessage("El Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty().withMessage("El Nombre del Cliente es Obligatorio"),
+  body("description")
+    .notEmpty().withMessage("La Descripción del Proyecto es Obligatoria"),
+  handleInputErrors,
+  ProjectController.updateProject
+)
+
 export default router
