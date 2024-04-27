5d4550b905c8148218157385e7d71637455da376
diegofdg <diegofdg@gmail.com>
2024-04-27 17:30:16 -0300

    14_uptask_mern-creando el modelo

---
+++ 14_uptask_mern/backend/src/models/Project.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose"
+
+export type ProjectType = Document & {
+  projectName: string
+  clientName: string
+  description: string
+}
+
+const ProjectSchema: Schema = new Schema({
+  projectName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  clientName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+const Project = mongoose.model<ProjectType>("Project", ProjectSchema)
+export default Project
