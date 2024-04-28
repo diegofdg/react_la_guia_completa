27a178ff82529b417f27a273e43ae346b995923a
diegofdg <diegofdg@gmail.com>
2024-04-28 19:31:54 -0300

    14_uptask_mern-relacionando los modelos de tareas y proyectos

--- 14_uptask_mern/backend/src/models/Project.ts
+++ 14_uptask_mern/backend/src/models/Project.ts
@@ -1,9 +1,11 @@
-import mongoose, { Schema, Document } from "mongoose"
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose"
+import { ITask } from "./Task"
 
 export interface IProject extends Document {
   projectName: string
   clientName: string
   description: string
+  tasks: PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema = new Schema({
@@ -21,8 +23,14 @@ const ProjectSchema: Schema = new Schema({
     type: String,
     required: true,
     trim: true
-  }
-})
+  },
+  tasks: [
+    {
+      type: Types.ObjectId,
+      ref: "Task"
+    }
+  ]
+}, {timestamps: true})
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema)
 export default Project
--- 14_uptask_mern/backend/src/models/Task.ts
+++ 14_uptask_mern/backend/src/models/Task.ts
@@ -1,22 +1,27 @@
 import mongoose, { Schema, Document, Types } from "mongoose"
 
 export interface ITask extends Document {
-    name: string
-    description: string
+  name: string
+  description: string
+  project: Types.ObjectId
 }
 
-export const TaskSchema : Schema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    }
-})
+export const TaskSchema: Schema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  project: {
+    type: Types.ObjectId,
+    ref: "Project"
+  }
+}, {timestamps: true})
 
 const Task = mongoose.model<ITask>("Task", TaskSchema)
 export default Task
