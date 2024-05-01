2b405cb07715b1fd89d5a5b08850631fb2a2af19
diegofdg <diegofdg@gmail.com>
2024-05-01 06:31:23 -0300

    14_uptask_mern-obtener información del proyecto al que pertenecen las tareas

--- 14_uptask_mern/backend/src/controllers/TaskController.ts
+++ 14_uptask_mern/backend/src/controllers/TaskController.ts
@@ -17,7 +17,7 @@ export class TaskController {
 
   static getProjectTasks = async (req: Request, res: Response) => {
     try {
-      const tasks = await Task.find({ project: req.project.id })
+      const tasks = await Task.find({ project: req.project.id }).populate("project")
       res.json(tasks)
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" })
