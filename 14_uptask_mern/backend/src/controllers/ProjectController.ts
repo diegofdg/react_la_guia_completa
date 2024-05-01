79e0a9a7a72b757de466bb5e43d570cfe2001d92
diegofdg <diegofdg@gmail.com>
2024-05-01 06:33:14 -0300

    14_uptask_mern-obtener las tareas de un proyecto

--- 14_uptask_mern/backend/src/controllers/ProjectController.ts
+++ 14_uptask_mern/backend/src/controllers/ProjectController.ts
@@ -26,7 +26,7 @@ export class ProjectController {
   static getProjectById = async (req: Request, res: Response) => {
     const { id } = req.params
     try {
-      const project = await Project.findById(id)
+      const project = await Project.findById(id).populate("tasks")
       if (!project) {
         const error = new Error("Proyecto no encontrado")
         return res.status(404).json({ error: error.message })
