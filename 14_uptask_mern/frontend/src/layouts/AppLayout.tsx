2ffee35d785b74f5a81f67c0b60f0bbec53d1604
diegofdg <diegofdg@gmail.com>
2024-05-02 00:39:59 -0300

    14_uptask_mern-definiendo urls base

--- 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
+++ 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import Logo from "../components/Logo"
+import Logo from "@/components/Logo"
 
 export default function AppLayout() {
   return (
--- 14_uptask_mern/frontend/src/router.tsx
+++ 14_uptask_mern/frontend/src/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AppLayout from "./layouts/AppLayout"
-import DashboardView from "./views/DashboardView"
+import AppLayout from "@/layouts/AppLayout"
+import DashboardView from "@/views/DashboardView"
 
 export default function Router() {
   return (
