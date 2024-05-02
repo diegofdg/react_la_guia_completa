a716a89b15dabdda01bd5301159455d092f5877f
diegofdg <diegofdg@gmail.com>
2024-05-02 16:21:25 -0300

    14_uptask_mern-utilizando zod para asignar los types del formulario de crear proyecto

--- 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
+++ 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import ProjectForm from "@/components/projects/ProjectForm"
+import { ProjectFormData } from "types"
 
 export default function CreateProjectView() {
-  const initialValues = {
+  const initialValues: ProjectFormData = {
     projectName: "",
     clientName: "",
     description: ""
   }
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const handleForm = (data) => {
+  const handleForm = (data: ProjectFormData) => {
     console.log(data)
   }
 
