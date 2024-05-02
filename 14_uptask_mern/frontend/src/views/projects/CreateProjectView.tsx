4c58e9f6697b0a455143f63183e505b5d259d753
diegofdg <diegofdg@gmail.com>
2024-05-02 01:11:00 -0300

    14_uptask_mern-agregando el resto de los campos

--- 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
+++ 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
+import ProjectForm from "@/components/projects/ProjectForm"
 
 export default function CreateProjectView() {
   const initialValues = {
@@ -30,6 +31,10 @@ export default function CreateProjectView() {
           onSubmit={handleSubmit(handleForm)}
           noValidate
         >
+          <ProjectForm
+            register={register}
+            errors={errors}
+          />
           <input
             type="submit"
             value="Crear Proyecto"
