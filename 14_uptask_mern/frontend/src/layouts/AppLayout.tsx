52b732d8199c07654e28b4a21cef4ecabb8d5556
diegofdg <diegofdg@gmail.com>
2024-05-02 00:30:42 -0300

    14_uptask_mern-creando el layout de la aplicación

--- 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
+++ 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
@@ -1,10 +1,24 @@
 import { Outlet } from "react-router-dom"
+import Logo from "../components/Logo"
 
 export default function AppLayout() {
   return (
     <>
-      <div>AppLayout</div>
-      <Outlet />
+      <header className="bg-gray-800 py-5">
+        <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+          <div className="w-64">
+            <Logo />
+          </div>
+        </div>
+      </header>
+      <section className="max-w-screen-2xl mx-auto mt-10 p-5">
+        <Outlet />
+      </section>
+      <footer className="py-5">
+        <p className="text-center">
+          Todos los derechos reservados {new Date().getFullYear()}
+        </p>
+      </footer>
     </>
   )
 }
