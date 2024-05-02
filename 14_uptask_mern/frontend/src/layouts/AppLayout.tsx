c56a4284ff1f84366fcc12f920e5204e36cb9253
diegofdg <diegofdg@gmail.com>
2024-05-02 00:46:17 -0300

    14_uptask_mern-agregando headless ui

--- 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
+++ 14_uptask_mern/frontend/src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 
 export default function AppLayout() {
   return (
@@ -9,6 +10,7 @@ export default function AppLayout() {
           <div className="w-64">
             <Logo />
           </div>
+          <NavMenu />
         </div>
       </header>
       <section className="max-w-screen-2xl mx-auto mt-10 p-5">
