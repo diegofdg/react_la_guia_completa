8810303db2bf36f80992b22b02d0924bd5b63968
diegofdg <diegofdg@gmail.com>
2024-05-03 00:57:53 -0300

    14_uptask_mern-redireccionando al usuario hacia todos los proyectos después de enviar el form

--- 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
+++ 14_uptask_mern/frontend/src/views/projects/CreateProjectView.tsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import ProjectForm from "@/components/projects/ProjectForm"
 import { ProjectFormData } from "@/types/index"
 import { createProject } from "@/api/ProjectAPI"
 
 export default function CreateProjectView() {
+  const navigate = useNavigate()
   const initialValues: ProjectFormData = {
     projectName: "",
     clientName: "",
@@ -12,8 +13,9 @@ export default function CreateProjectView() {
   }
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const handleForm = (data: ProjectFormData) => {
-    createProject(data)
+  const handleForm = async (data: ProjectFormData) => {
+    await createProject(data)
+    navigate("/")
   }
 
   return (
