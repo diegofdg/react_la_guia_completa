1c449a0bbd8b217da43f84302cf379446e81a25f
diegofdg <diegofdg@gmail.com>
2024-05-03 00:54:25 -0300

    14_uptask_mern-habilitando cors en el backend para comunicarlo con el frontend

--- 14_uptask_mern/backend/src/server.ts
+++ 14_uptask_mern/backend/src/server.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import { corsConfig } from "./config/cors"
 import { connectDB } from "./config/db"
 import projectRoutes from "./routes/projectRoutes"
 
@@ -10,6 +12,7 @@ const app = express()
 
 // Leer datos de formularios
 app.use(express.json())
+app.use(cors(corsConfig))
 
 // Routes
 app.use("/api/projects", projectRoutes)
