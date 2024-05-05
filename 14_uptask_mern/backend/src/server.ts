7490d6f7a4780ed0c2f07af957047ccbb26e581d
diegofdg <diegofdg@gmail.com>
2024-05-05 18:38:06 -0300

    14_uptask_mern-instalando react query devtools

--- 14_uptask_mern/backend/src/server.ts
+++ 14_uptask_mern/backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import morgan from "morgan"
 import { corsConfig } from "./config/cors"
 import { connectDB } from "./config/db"
 import projectRoutes from "./routes/projectRoutes"
@@ -9,6 +10,8 @@ dotenv.config()
 connectDB()
 
 const app = express()
+// Logging
+app.use(morgan("dev"))
 
 // Leer datos de formularios
 app.use(express.json())
