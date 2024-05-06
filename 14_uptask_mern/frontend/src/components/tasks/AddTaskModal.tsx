be469dd437946b71c7ab56fd3dae66e4461628ed
diegofdg <diegofdg@gmail.com>
2024-05-05 23:41:32 -0300

    14_uptask_mern-creando el formulario para las tareas

--- 14_uptask_mern/frontend/src/components/tasks/AddTaskModal.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/AddTaskModal.tsx
@@ -1,6 +1,9 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useLocation, useNavigate } from "react-router-dom";
+import TaskForm from "./TaskForm";
+import { TaskFormData } from "@/types/index";
+import { useForm } from "react-hook-form";
 
 export default function AddTaskModal() {
   const navigate = useNavigate()
@@ -8,6 +11,17 @@ export default function AddTaskModal() {
   const queryParams = new URLSearchParams(location.search)
   const modalTask = queryParams.get("newTask")
   const show = modalTask ? true : false
+
+  const initialValues: TaskFormData = {
+    name: "",
+    description: ""
+  }
+  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const handleCreateTask = (formData: TaskFormData) => {
+    console.log(formData)
+  }
+
   return (
     <>
       <Transition appear show={show} as={Fragment}>
@@ -46,6 +60,22 @@ export default function AddTaskModal() {
                   <p className="text-xl font-bold">Llena el formulario y crea  {""}
                     <span className="text-fuchsia-600">una tarea</span>
                   </p>
+                  <form
+                    className="mt-10 space-y-3"
+                    onSubmit={handleSubmit(handleCreateTask)}
+                    noValidate
+                  >
+                    <TaskForm
+                      register={register}
+                      errors={errors}
+                    />
+
+                    <input
+                      type="submit"
+                      className=" bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+                      value="Guardar Tarea"
+                    />
+                  </form>
 
                 </Dialog.Panel>
               </Transition.Child>
--- 14_uptask_mern/frontend/src/types/index.ts
+++ 14_uptask_mern/frontend/src/types/index.ts
@@ -1,5 +1,19 @@
 import { z } from "zod"
 
+/** Tasks */
+export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed" ])
+
+export const taskSchema = z.object({
+  _id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  project: z.string(),
+  status: taskStatusSchema
+})
+
+export type Task = z.infer<typeof taskSchema>
+export type TaskFormData = Pick<Task, "name" | "description">
+
 /** Projects */
 export const projectSchema = z.object({
   _id: z.string(),
