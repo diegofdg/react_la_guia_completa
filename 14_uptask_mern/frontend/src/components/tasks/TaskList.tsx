8bcdda8c79782cf109616198a3c11a8c8431655e
diegofdg <diegofdg@gmail.com>
2024-05-06 00:31:50 -0300

    14_uptask_mern-aplicando diferentes colores a los estados

--- 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
@@ -17,12 +17,20 @@ const initialStatusGroups: GroupedTasks = {
   completed: [],
 }
 
+const statusStyles: { [key: string]: string } = {
+  pending: "border-t-slate-500",
+  onHold: "border-t-red-500",
+  inProgress: "border-t-blue-500",
+  underReview: "border-t-amber-500",
+  completed: "border-t-emerald-500",
+}
+
 const statusTranslations: { [key: string]: string } = {
-  pending: 'Pendiente',
-  onHold: 'En Espera',
-  inProgress: 'En Progreso',
-  underReview: 'En Revisión',
-  completed: 'Completado',
+  pending: "Pendiente",
+  onHold: "En Espera",
+  inProgress: "En Progreso",
+  underReview: "En Revisión",
+  completed: "Completado",
 }
 
 export default function TaskList({ tasks }: TaskListProps) {
@@ -40,7 +48,7 @@ export default function TaskList({ tasks }: TaskListProps) {
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
             <h3
-              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 `}
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
             >{statusTranslations[status]}</h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
