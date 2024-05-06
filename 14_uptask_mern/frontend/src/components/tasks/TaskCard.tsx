2c2ea206619ac4b60b7b66c7e4f3fccb653a3cd7
diegofdg <diegofdg@gmail.com>
2024-05-06 00:22:48 -0300

    14_uptask_mern-mostrando los detalles de las tareas

--- 14_uptask_mern/frontend/src/components/tasks/TaskCard.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/TaskCard.tsx
@@ -1,4 +1,7 @@
+import { Fragment } from "react"
 import { Task } from "@/types/index"
+import { Menu, Transition } from "@headlessui/react"
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 
 type TaskCardProps = {
   task: Task
@@ -6,6 +9,48 @@ type TaskCardProps = {
 
 export default function TaskCard({ task }: TaskCardProps) {
   return (
-    <div>{task.description}</div>
+    <li
+      className="p-5 bg-white border border-slate-300 flex justify-between gap-3">
+      <div className=" min-w-0 flex flex-col gap-y-4">
+        <button
+          type="button"
+          className="text-xl font-bold text-slate-600 text-left"
+        >{task.name}
+        </button>
+        <p className="text-slate-500">{task.description}</p>
+      </div>
+
+      <div className="flex shrink-0  gap-x-6">
+        <Menu as="div" className="relative flex-none">
+          <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+            <span className="sr-only">opciones</span>
+            <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
+          </Menu.Button>
+          <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
+            <Menu.Items
+              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <Menu.Item>
+                <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                  Ver Tarea
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                  Editar Tarea
+                </button>
+              </Menu.Item>
+
+              <Menu.Item>
+                <button type='button' className='block px-3 py-1 text-sm leading-6 text-red-500'>
+                  Eliminar Tarea
+                </button>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      </div>
+    </li>
   )
 }
