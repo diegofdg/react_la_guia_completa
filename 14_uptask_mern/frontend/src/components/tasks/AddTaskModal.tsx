d03eb088d2db9774f209ec3e298d5e24386622d9
diegofdg <diegofdg@gmail.com>
2024-05-05 23:56:47 -0300

    14_uptask_mern-creando tareas con useMutation

--- 14_uptask_mern/frontend/src/components/tasks/AddTaskModal.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/AddTaskModal.tsx
@@ -1,25 +1,50 @@
-import { Fragment } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { useLocation, useNavigate } from "react-router-dom";
-import TaskForm from "./TaskForm";
-import { TaskFormData } from "@/types/index";
-import { useForm } from "react-hook-form";
+import { Fragment } from "react"
+import { Dialog, Transition } from "@headlessui/react"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import TaskForm from "./TaskForm"
+import { TaskFormData } from "@/types/index"
+import { useForm } from "react-hook-form"
+import { useMutation } from "@tanstack/react-query"
+import { createTask } from "@/api/TaskAPI"
+import { toast } from "react-toastify"
 
 export default function AddTaskModal() {
   const navigate = useNavigate()
+
+  /** Leer Si modal existe */
   const location = useLocation()
   const queryParams = new URLSearchParams(location.search)
   const modalTask = queryParams.get("newTask")
   const show = modalTask ? true : false
 
+  /** Obtener projectId */
+  const params = useParams()
+  const projectId = params.projectId!
+
   const initialValues: TaskFormData = {
     name: "",
     description: ""
   }
-  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const { mutate } = useMutation({
+    mutationFn: createTask,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+      reset()
+      navigate(location.pathname, { replace: true })
+    }
+  })
 
   const handleCreateTask = (formData: TaskFormData) => {
-    console.log(formData)
+    const data = {
+      formData,
+      projectId
+    }
+    mutate(data)
   }
 
   return (
