f699ab5cf5b29e66a887a77730a9b7d181c78adb
diegofdg <diegofdg@gmail.com>
2024-05-05 23:47:08 -0300

    14_uptask_mern-creando la función para crear tareas

---
+++ 14_uptask_mern/frontend/src/api/TaskAPI.ts
@@ -0,0 +1,21 @@
+import { isAxiosError } from "axios";
+import api from "@/lib/axios";
+import { Project, TaskFormData } from "../types";
+
+type TaskAPI = {
+  formData: TaskFormData
+  projectId: Project["_id"]
+}
+
+export async function createTask({ formData, projectId }: Pick<TaskAPI, "formData" | "projectId">) {
+  try {
+    const url = `/projects/${projectId}/tasks`
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
