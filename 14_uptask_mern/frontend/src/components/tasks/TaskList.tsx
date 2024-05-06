6bc0a544bcd186035c6222a2ba1f92365aff0049
diegofdg <diegofdg@gmail.com>
2024-05-06 00:29:04 -0300

    14_uptask_mern-mostrando el estado de la tarea a la que pertenecen

--- 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups: GroupedTasks = {
   completed: [],
 }
 
+const statusTranslations: { [key: string]: string } = {
+  pending: 'Pendiente',
+  onHold: 'En Espera',
+  inProgress: 'En Progreso',
+  underReview: 'En Revisión',
+  completed: 'Completado',
+}
+
 export default function TaskList({ tasks }: TaskListProps) {
   const groupedTasks = tasks.reduce((acc, task) => {
     let currentGroup = acc[task.status] ? [...acc[task.status]] : []
@@ -28,10 +36,13 @@ export default function TaskList({ tasks }: TaskListProps) {
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
 
-      <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
+      <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
-          <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-            <ul className='mt-5 space-y-5'>
+          <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <h3
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 `}
+            >{statusTranslations[status]}</h3>
+            <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
               ) : (
