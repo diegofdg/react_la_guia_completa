ff281af95797f376f402e6009c84973610cee03c
diegofdg <diegofdg@gmail.com>
2024-05-09 07:24:16 -0300

    14_uptask_mern-solucionar problema de cors con thunderclient

--- 14_uptask_mern/backend/src/config/cors.ts
+++ 14_uptask_mern/backend/src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from "cors"
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [process.env.FRONTEND_URL]
-
+    
     if (process.argv[2] === "--api") {
       whitelist.push(undefined)
     }
