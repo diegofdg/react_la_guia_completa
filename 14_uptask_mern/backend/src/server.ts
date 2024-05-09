c64b49e8c4a5804274bcf34c96638a825c80d50b
diegofdg <diegofdg@gmail.com>
2024-05-09 07:16:25 -0300

    14_uptask_mern-creando el router para autenticar

--- 14_uptask_mern/backend/src/server.ts
+++ 14_uptask_mern/backend/src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors"
 import morgan from "morgan"
 import { corsConfig } from "./config/cors"
 import { connectDB } from "./config/db"
+import authRoutes from "./routes/authRoutes"
 import projectRoutes from "./routes/projectRoutes"
 
 dotenv.config()
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(cors(corsConfig))
 
 // Routes
+app.use("/api/auth", authRoutes)
 app.use("/api/projects", projectRoutes)
 
 export default app
