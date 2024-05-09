d255a63b115d0f68704029cfca9753d9842e7eec
diegofdg <diegofdg@gmail.com>
2024-05-09 19:50:00 -0300

    14_uptask_mern-algoritmo para crear cuentas

--- 14_uptask_mern/backend/src/controllers/AuthController.ts
+++ 14_uptask_mern/backend/src/controllers/AuthController.ts
@@ -1,8 +1,17 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
 
 export class AuthController {
 
-  static createAccount = async (req: Request, res: Response) => {  
-    res.send("Desde AuthController")
+  static createAccount = async (req: Request, res: Response) => {
+    try {
+      // Crea un usuario
+      const user = new User(req.body)
+
+      await user.save()
+      res.send("Cuenta creada, revisa tu email para confirmarla")
+    } catch (error) {
+      res.status(500).json({ error: "Hubo un error" })
+    }
   }
 }
