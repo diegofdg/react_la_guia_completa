d68fa8c394a77865adb9f438d99786086dffdddd
diegofdg <diegofdg@gmail.com>
2024-05-09 19:58:16 -0300

    14_uptask_mern-hashear los passwords

--- 14_uptask_mern/backend/src/controllers/AuthController.ts
+++ 14_uptask_mern/backend/src/controllers/AuthController.ts
@@ -1,14 +1,21 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password, email } = req.body
+
       // Crea un usuario
       const user = new User(req.body)
 
+      // Hash Password
+      user.password = await hashPassword(password)
+      
       await user.save()
+
       res.send("Cuenta creada, revisa tu email para confirmarla")
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" })
