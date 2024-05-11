c8598235626cf218c3aef2ce713ef43eeee38a97
diegofdg <diegofdg@gmail.com>
2024-05-11 16:52:06 -0300

    14_uptask_mern-enviando el token al email del usuario registrado

--- 14_uptask_mern/backend/src/controllers/AuthController.ts
+++ 14_uptask_mern/backend/src/controllers/AuthController.ts
@@ -3,7 +3,7 @@ import User from "../models/User"
 import { hashPassword } from "../utils/auth"
 import Token from "../models/Token"
 import { generateToken } from "../utils/token"
-import { transporter } from "../config/nodemailer"
+import { AuthEmail } from "../emails/AuthEmail"
 
 export class AuthController {
 
@@ -30,13 +30,10 @@ export class AuthController {
       token.user = user.id
 
       // enviar el email
-      await transporter.sendMail({
-        from: 'UpTask <admin@uptask.com>',
-        to: user.email,
-        subject: 'UpTask - Confirma tu cuenta',
-        text: 'UpTask - Confirma tu cuenta',
-        html: `<p>Hola Mundo</p>
-            `
+      AuthEmail.sendConfirmationEmail({
+        email: user.email,
+        name: user.name,
+        token: token.token
       })
       await Promise.allSettled([user.save(), token.save()])
 
---
+++ 14_uptask_mern/backend/src/emails/AuthEmail.ts
@@ -0,0 +1,26 @@
+import { transporter } from "../config/nodemailer"
+
+interface IEmail {
+  email: string
+  name: string
+  token: string
+}
+
+export class AuthEmail {
+  static sendConfirmationEmail = async (user: IEmail) => {
+    const info = await transporter.sendMail({
+      from: "UpTask <admin@uptask.com>",
+      to: user.email,
+      subject: "UpTask - Confirma tu cuenta",
+      text: "UpTask - Confirma tu cuenta",
+      html: `
+        <p>Hola: ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
+        <p>Visita el siguiente enlace:</p>
+        <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
+        <p>E ingresa el código: <b>${user.token}</b></p>
+        <p>Este token expira en 10 minutos</p>
+      `
+    })
+    console.log("Mensaje enviado", info.messageId)
+  }
+}
