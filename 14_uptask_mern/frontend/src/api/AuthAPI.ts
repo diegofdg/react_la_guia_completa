526fd8cd69fe81b2f58cea372d151153231b3498
diegofdg <diegofdg@gmail.com>
2024-05-12 07:34:23 -0300

    14_uptask_mern-creando la funcionalidad de autenticar

--- 14_uptask_mern/frontend/src/api/AuthAPI.ts
+++ 14_uptask_mern/frontend/src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios"
 import { isAxiosError } from "axios"
-import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types"
+import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types"
 
 export async function createAccount(formData: UserRegistrationForm) {
   try {
@@ -29,6 +29,18 @@ export async function confirmAccount(formData: ConfirmToken) {
 export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
   try {
     const url = "/auth/request-code"
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+  try {
+    const url = "/auth/login"
     const { data } = await api.post<string>(url, formData)
     return data
   } catch (error) {
--- 14_uptask_mern/frontend/src/views/auth/LoginView.tsx
+++ 14_uptask_mern/frontend/src/views/auth/LoginView.tsx
@@ -1,7 +1,10 @@
-import { useForm } from "react-hook-form";
-import { UserLoginForm } from "@/types/index";
-import ErrorMessage from "@/components/ErrorMessage";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
+import { UserLoginForm } from "@/types/index"
+import ErrorMessage from "@/components/ErrorMessage"
+import { Link } from "react-router-dom"
+import { useMutation } from "@tanstack/react-query"
+import { authenticateUser } from "@/api/AuthAPI"
+import { toast } from "react-toastify"
 
 export default function LoginView() {
 
@@ -11,7 +14,17 @@ export default function LoginView() {
   }
   const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
 
-  const handleLogin = (formData: UserLoginForm) => { }
+  const { mutate } = useMutation({
+    mutationFn: authenticateUser,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+    }
+  })
+
+  const handleLogin = (formData: UserLoginForm) => mutate(formData)
 
   return (
     <>
