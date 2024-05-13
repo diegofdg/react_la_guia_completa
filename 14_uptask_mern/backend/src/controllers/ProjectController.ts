c91fe73b285da70395666dce778dc42b914c5a74
diegofdg <diegofdg@gmail.com>
2024-05-12 21:16:04 -0300

    14_uptask_mern-agregando el usuario al modelo de proyectos

--- 14_uptask_mern/backend/src/controllers/ProjectController.ts
+++ 14_uptask_mern/backend/src/controllers/ProjectController.ts
@@ -6,6 +6,9 @@ export class ProjectController {
   static createProject = async (req: Request, res: Response) => {
     const project = new Project(req.body)
 
+    // Asigna un manager
+    project.manager = req.user.id
+
     try {
       await project.save()
       res.send("Proyecto Creando Correctamente")
