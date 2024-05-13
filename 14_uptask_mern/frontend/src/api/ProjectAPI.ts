4065ceca4f7fa2022f98dc5ed489d5364b70b6e0
diegofdg <diegofdg@gmail.com>
2024-05-13 07:19:32 -0300

    14_uptask_mern-obtener los proyectos desde react

--- 14_uptask_mern/frontend/src/api/ProjectAPI.ts
+++ 14_uptask_mern/frontend/src/api/ProjectAPI.ts
@@ -14,8 +14,14 @@ export async function createProject(formData: ProjectFormData) {
 }
 
 export async function getProjects() {
+  const token = localStorage.getItem("AUTH_TOKEN")
+
   try {
-    const { data } = await api("/projects")
+    const { data } = await api("/projects", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
     const response = dashboardProjectSchema.safeParse(data)
     if (response.success) {
       return response.data
