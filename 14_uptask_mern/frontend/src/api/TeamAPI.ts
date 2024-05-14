ccd9e94a9386d7bb23f4623226a6c0ccb7eaf2a3
diegofdg <diegofdg@gmail.com>
2024-05-14 15:58:14 -0300

    14_uptask_mern-agregando el usuario al proyecto

--- 14_uptask_mern/frontend/src/api/TeamAPI.ts
+++ 14_uptask_mern/frontend/src/api/TeamAPI.ts
@@ -1,11 +1,23 @@
 import { isAxiosError } from "axios"
 import api from "@/lib/axios"
-import { Project, TeamMemberForm } from "../types"
+import { Project, TeamMember, TeamMemberForm } from "../types"
 
 export async function findUserByEmail({ projectId, formData }: { projectId: Project["_id"], formData: TeamMemberForm }) {
   try {
     const url = `/projects/${projectId}/team/find`
     const { data } = await api.post(url, formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function addUserToProject({ projectId, id }: { projectId: Project["_id"], id: TeamMember["_id"] }) {
+  try {
+    const url = `/projects/${projectId}/team`
+    const { data } = await api.post<string>(url, { id })
     return data
   } catch (error) {
     if (isAxiosError(error) && error.response) {
--- 14_uptask_mern/frontend/src/components/team/SearchResult.tsx
+++ 14_uptask_mern/frontend/src/components/team/SearchResult.tsx
@@ -1,10 +1,35 @@
+import { useMutation } from "@tanstack/react-query"
 import { TeamMember } from "@/types/index"
+import { toast } from "react-toastify"
+import { addUserToProject } from "@/api/TeamAPI"
+import { useParams } from "react-router-dom"
 
 type SearchResultProps = {
   user: TeamMember
 }
 
 export default function SearchResult({ user }: SearchResultProps) {
+  const params = useParams()
+  const projectId = params.projectId!
+
+  const { mutate } = useMutation({
+    mutationFn: addUserToProject,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+    }
+  })
+
+  const handleAddUserToProject = () => {
+    const data = {
+      projectId,
+      id: user._id
+    }
+    mutate(data)
+  }
+
   return (
     <>
       <p className="mt-10 text-center font-bold">Resultado:</p>
@@ -12,6 +37,7 @@ export default function SearchResult({ user }: SearchResultProps) {
         <p>{user.name}</p>
         <button
           className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer"
+          onClick={handleAddUserToProject}
         >Agregar al Proyecto</button>
       </div>
     </>
