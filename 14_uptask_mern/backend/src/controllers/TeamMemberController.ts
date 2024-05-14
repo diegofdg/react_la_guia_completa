3f266641c65c24839a156f7818f0fe22a6fb058c
diegofdg <diegofdg@gmail.com>
2024-05-14 01:09:26 -0300

    14_uptask_mern-creando el endpoint para añadir miembros al equipo

--- 14_uptask_mern/backend/src/controllers/TeamMemberController.ts
+++ 14_uptask_mern/backend/src/controllers/TeamMemberController.ts
@@ -14,4 +14,25 @@ export class TeamMemberController {
     }
     res.json(user)
   }
+
+  static addMemberById = async (req: Request, res: Response) => {
+    const { id } = req.body
+
+    // Find user
+    const user = await User.findById(id).select("id")
+    if (!user) {
+      const error = new Error("Usuario No Encontrado")
+      return res.status(404).json({ error: error.message })
+    }
+
+    if (req.project.team.some(team => team.toString() === user.id.toString())) {
+      const error = new Error("El usuario ya existe en el proyecto")
+      return res.status(409).json({ error: error.message })
+    }
+
+    req.project.team.push(user.id)
+    await req.project.save()
+
+    res.send("Usuario agregado correctamente")
+  }
 }
--- 14_uptask_mern/backend/src/routes/projectRoutes.ts
+++ 14_uptask_mern/backend/src/routes/projectRoutes.ts
@@ -108,4 +108,11 @@ router.post("/:projectId/team/find",
   TeamMemberController.findMemberByEmail
 )
 
+router.post("/:projectId/team",
+  body("id")
+    .isMongoId().withMessage("ID No válido"),
+  handleInputErrors,
+  TeamMemberController.addMemberById
+)
+
 export default router
