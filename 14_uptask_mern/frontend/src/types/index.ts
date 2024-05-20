fc2abc2071b669ad12bb40c51ed32ce0d780f0de
diegofdg <diegofdg@gmail.com>
2024-05-20 20:19:47 -0300

    14_uptask_mern-creando el formulario de perfil

--- 14_uptask_mern/frontend/src/types/index.ts
+++ 14_uptask_mern/frontend/src/types/index.ts
@@ -25,6 +25,7 @@ export const userSchema = authSchema.pick({
   _id: z.string()
 })
 export type User = z.infer<typeof userSchema>
+export type UserProfileForm = Pick<User, "name" | "email">
 
 /** Notes */
 const noteSchema = z.object({
--- 14_uptask_mern/frontend/src/views/profile/ProfileView.tsx
+++ 14_uptask_mern/frontend/src/views/profile/ProfileView.tsx
@@ -1,5 +1,8 @@
+import ProfileForm from "@/components/profile/ProfileForm"
+import { useAuth } from "@/hooks/useAuth"
+
 export default function ProfileView() {
-  return (
-    <div>ProfileView</div>
-  )
+  const { data, isLoading } = useAuth()
+  if (isLoading) return "Cargando..."
+  if (data) return <ProfileForm data={data} />
 }
