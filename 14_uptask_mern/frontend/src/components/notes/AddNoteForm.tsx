03164b38a29248b2ea3042e355f7ec0f4b161853
diegofdg <diegofdg@gmail.com>
2024-05-20 18:56:55 -0300

    14_uptask_mern-creando el type en zod

--- 14_uptask_mern/frontend/src/components/notes/AddNoteForm.tsx
+++ 14_uptask_mern/frontend/src/components/notes/AddNoteForm.tsx
@@ -1,7 +1,21 @@
+import { NoteFormData } from "@/types/index"
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../ErrorMessage"
+
 export default function AddNoteForm() {
+  const initialValues: NoteFormData = {
+    content: ""
+  }
+
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+  const handleAddNote = (formData: NoteFormData) => {
+    console.log(formData)
+  }
+
   return (
     <form
-      onSubmit={() => { }}
+      onSubmit={handleSubmit(handleAddNote)}
       className="space-y-3"
       noValidate
     >
@@ -12,7 +26,13 @@ export default function AddNoteForm() {
           type="text"
           placeholder="Contenido de la nota"
           className="w-full p-3 border border-gray-300"
+          {...register("content", {
+            required: "El Contenido de la nota es obligatorio"
+          })}
         />
+        {errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        )}
       </div>
 
       <input
--- 14_uptask_mern/frontend/src/types/index.ts
+++ 14_uptask_mern/frontend/src/types/index.ts
@@ -26,6 +26,16 @@ export const userSchema = authSchema.pick({
 })
 export type User = z.infer<typeof userSchema>
 
+/** Notes */
+const noteSchema = z.object({
+  _id: z.string(),
+  content: z.string(),
+  createdBy: userSchema,
+  task: z.string()
+})
+export type Note = z.infer<typeof noteSchema>
+export type NoteFormData = Pick<Note, "content">
+
 /** Tasks */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
