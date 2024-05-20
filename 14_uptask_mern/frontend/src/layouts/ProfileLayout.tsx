b819e41b8f9e63670b37d2c201a65d798466e6a0
diegofdg <diegofdg@gmail.com>
2024-05-20 20:13:06 -0300

    14_uptask_mern-routing para las rutas del perfil

--- 14_uptask_mern/frontend/src/layouts/ProfileLayout.tsx
+++ 14_uptask_mern/frontend/src/layouts/ProfileLayout.tsx
@@ -1,9 +1,10 @@
+import Tabs from "@/components/profile/Tabs"
 import { Outlet } from "react-router-dom"
 
 export default function ProfileLayout() {
   return (
     <>
-      <div>ProfileLayout</div>
+      <Tabs />
       <Outlet />
     </>
   )
