bd60e53509d0f17b152b9a050afa55f22f63a3e1
diegofdg <diegofdg@gmail.com>
2024-05-20 19:12:23 -0300

    14_uptask_mern-creando la función y mutación para crear nota

---
+++ 14_uptask_mern/frontend/src/api/NoteAPI.ts
@@ -0,0 +1,22 @@
+import { isAxiosError } from "axios"
+import { Note, NoteFormData, Project, Task } from "../types"
+import api from "@/lib/axios"
+
+type NoteAPIType = {
+  formData: NoteFormData
+  projectId: Project["_id"]
+  taskId: Task["_id"]
+  noteId: Note["_id"]
+}
+
+export async function createNote({ projectId, taskId, formData }: Pick<NoteAPIType, "projectId" | "taskId" | "formData">) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes`
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
--- 14_uptask_mern/frontend/src/components/notes/AddNoteForm.tsx
+++ 14_uptask_mern/frontend/src/components/notes/AddNoteForm.tsx
@@ -1,16 +1,37 @@
 import { NoteFormData } from "@/types/index"
 import { useForm } from "react-hook-form"
 import ErrorMessage from "../ErrorMessage"
+import { createNote } from "@/api/NoteAPI"
+import { toast } from "react-toastify"
+import { useMutation } from "@tanstack/react-query"
+import { useLocation, useParams } from "react-router-dom"
 
 export default function AddNoteForm() {
+  const params = useParams()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const projectId = params.projectId!
+  const taskId = queryParams.get("viewTask")!
+
   const initialValues: NoteFormData = {
     content: ""
   }
 
   const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues })
 
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+    }
+  })
+
   const handleAddNote = (formData: NoteFormData) => {
-    console.log(formData)
+    mutate({ projectId, taskId, formData })
+    reset()
   }
 
   return (
