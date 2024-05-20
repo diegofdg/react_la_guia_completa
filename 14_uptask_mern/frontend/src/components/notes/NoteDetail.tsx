8bff3c3cfafd929d715d17b70370a2608082e54b
diegofdg <diegofdg@gmail.com>
2024-05-20 19:50:33 -0300

    14_uptask_mern-detectando si el usuario creó la nota para que pueda eliminar

--- 14_uptask_mern/frontend/src/components/notes/NoteDetail.tsx
+++ 14_uptask_mern/frontend/src/components/notes/NoteDetail.tsx
@@ -1,11 +1,18 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
 
 type NoteDetailProps = {
   note: Note
 }
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+  const { data, isLoading } = useAuth()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+  if (isLoading) return "Cargando..."
   return (
     <div className="p-3 flex justify-between items-center">
       <div>
@@ -17,13 +24,13 @@ export default function NoteDetail({ note }: NoteDetailProps) {
         </p>
       </div>
 
-
-      <button
-        type="button"
-        className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
-        onClick={() => { }}
-      >Eliminar</button>
-
+      {canDelete && (
+        <button
+          type="button"
+          className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors"
+          onClick={() => { }}
+        >Eliminar</button>
+      )}
     </div>
   )
 }
