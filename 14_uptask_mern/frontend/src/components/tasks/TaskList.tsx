b0d5a7d2c6dc60c5552eb28d15c0cbefb4300563
diegofdg <diegofdg@gmail.com>
2024-05-21 07:28:04 -0300

    14_uptask_mern-primeros pasos con dndkit

--- 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
@@ -1,6 +1,7 @@
 import { Task } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTranslations } from "@/locales/es"
+import DropTask from "./DropTask"
 
 type TaskListProps = {
   tasks: Task[]
@@ -44,6 +45,9 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
             <h3
               className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
             >{statusTranslations[status]}</h3>
+
+            <DropTask />
+            
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
