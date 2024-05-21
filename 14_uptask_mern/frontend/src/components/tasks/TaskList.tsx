3839f26e5ea5a4d8b5e958733b96628420a763e0
diegofdg <diegofdg@gmail.com>
2024-05-21 15:44:10 -0300

    14_uptask_mern-useDroppable para soltar elementos

--- 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
+++ 14_uptask_mern/frontend/src/components/tasks/TaskList.tsx
@@ -1,4 +1,4 @@
-import { DndContext } from "@dnd-kit/core"
+import { DndContext, DragEndEvent } from "@dnd-kit/core"
 import { Task } from "@/types/index"
 import TaskCard from "./TaskCard"
 import { statusTranslations } from "@/locales/es"
@@ -36,12 +36,21 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
     return { ...acc, [task.status]: currentGroup }
   }, initialStatusGroups)
 
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { over, active } = e
+
+    if (over && over.id) {
+      console.log("Válido")
+    }
+
+  }
+
   return (
     <>
       <h2 className="text-5xl font-black my-10">Tareas</h2>
 
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
-        <DndContext>
+        <DndContext onDragEnd={handleDragEnd}>
 
           {Object.entries(groupedTasks).map(([status, tasks]) => (
             <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
@@ -49,7 +58,7 @@ export default function TaskList({ tasks, canEdit }: TaskListProps) {
                 className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
               >{statusTranslations[status]}</h3>
 
-              <DropTask />
+              <DropTask status={status} />
 
               <ul className="mt-5 space-y-5">
                 {tasks.length === 0 ? (
