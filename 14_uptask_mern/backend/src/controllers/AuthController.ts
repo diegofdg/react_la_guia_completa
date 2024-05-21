76b9b194727c5ac12e7737b399d792331cf07b91
diegofdg <diegofdg@gmail.com>
2024-05-21 06:53:20 -0300

    14_uptask_mern-creando una función que revise el password del usuario autenticado

--- 14_uptask_mern/backend/src/controllers/AuthController.ts
+++ 14_uptask_mern/backend/src/controllers/AuthController.ts
@@ -252,4 +252,18 @@ export class AuthController {
       res.status(500).send("Hubo un error")
     }
   }
+
+  static checkPassword = async (req: Request, res: Response) => {
+    const { password } = req.body
+
+    const user = await User.findById(req.user.id)
+
+    const isPasswordCorrect = await checkPassword(password, user.password)
+    if (!isPasswordCorrect) {
+      const error = new Error("El Password es incorrecto")
+      return res.status(401).json({ error: error.message })
+    }
+
+    res.send("Password Correcto")
+  }
 }
--- 14_uptask_mern/backend/src/routes/authRoutes.ts
+++ 14_uptask_mern/backend/src/routes/authRoutes.ts
@@ -108,4 +108,12 @@ router.post("/update-password",
   AuthController.updateCurrentUserPassword
 )
 
+router.post("/check-password",
+  authenticate,
+  body("password")
+    .notEmpty().withMessage("El password no puede ir vacio"),
+  handleInputErrors,
+  AuthController.checkPassword
+)
+
 export default router
