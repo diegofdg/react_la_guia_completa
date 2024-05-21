b9d031d27875658c7052dcc98e13b165d06b275c
diegofdg <diegofdg@gmail.com>
2024-05-21 20:38:43 -0300

    14_uptask_mern-página 404 cuando algo no existe

--- 14_uptask_mern/frontend/src/router.tsx
+++ 14_uptask_mern/frontend/src/router.tsx
@@ -15,6 +15,7 @@ import ProjectTeamView from "@/views/projects/ProjectTeamView"
 import ProfileView from "./views/profile/ProfileView"
 import ChangePasswordView from "./views/profile/ChangePasswordView"
 import ProfileLayout from "./layouts/ProfileLayout"
+import NotFound from "./views/404/NotFound"
 
 export default function Router() {
   return (
@@ -39,6 +40,10 @@ export default function Router() {
           <Route path="/auth/request-code" element={<RequestNewCodeView />} />
           <Route path="/auth/forgot-password" element={<ForgotPasswordView />} />
           <Route path="/auth/new-password" element={<NewPasswordView />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
