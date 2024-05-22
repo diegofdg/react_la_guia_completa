011726d2339d1f5ceccc0958d9e163c174f2ec04
diegofdg <diegofdg@gmail.com>
2024-05-21 21:19:32 -0300

    14_uptask_mern-eliminar notas cuando se elimina una tarea

--- 14_uptask_mern/backend/src/models/Task.ts
+++ 14_uptask_mern/backend/src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose"
+import Note from "./Note"
 
 const taskStatus = {
   PENDING: "pending",
@@ -63,6 +64,13 @@ export const TaskSchema: Schema = new Schema({
     }
   ]
 }, { timestamps: true })
+
+// Middleware
+TaskSchema.pre("deleteOne", { document: true }, async function () {
+  const taskId = this._id
+  if (!taskId) return
+  await Note.deleteMany({ task: taskId })
+})
 
 const Task = mongoose.model<ITask>("Task", TaskSchema)
 export default Task
