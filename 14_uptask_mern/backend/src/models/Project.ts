2ecfafad2038a60ac236c9e519d7d91a33400f8b
diegofdg <diegofdg@gmail.com>
2024-05-21 21:26:21 -0300

    14_uptask_mern-eliminar tareas que pertenecen a un proyecto

--- 14_uptask_mern/backend/src/models/Project.ts
+++ 14_uptask_mern/backend/src/models/Project.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose"
-import { ITask } from "./Task"
+import Task, { ITask } from "./Task"
 import { IUser } from "./User"
+import Note from "./Note"
 
 export interface IProject extends Document {
   projectName: string
@@ -44,6 +45,19 @@ const ProjectSchema: Schema = new Schema({
     }
   ],
 }, { timestamps: true })
+
+// Middleware
+ProjectSchema.pre("deleteOne", { document: true }, async function () {
+  const projectId = this._id
+  if (!projectId) return
+
+  const tasks = await Task.find({ project: projectId })
+  for (const task of tasks) {
+    await Note.deleteMany({ task: task.id })
+  }
+
+  await Task.deleteMany({ project: projectId })
+})
 
 const Project = mongoose.model<IProject>("Project", ProjectSchema)
 export default Project
