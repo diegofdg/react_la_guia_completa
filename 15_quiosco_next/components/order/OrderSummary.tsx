a62b9779154a0d8d915841c57a3d5bc83be4625a
diegofdg <diegofdg@gmail.com>
2024-05-23 07:55:17 -0300

    15_quiosco_next-consumir el state de orden

--- 15_quiosco_next/components/order/OrderSummary.tsx
+++ 15_quiosco_next/components/order/OrderSummary.tsx
@@ -1,7 +1,17 @@
+"use client"
+import { useStore } from "@/src/store"
+
 export default function OrderSummary() {
+  const order = useStore((state) => state.order)
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+
+      {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
+        <div className="mt-5">
+          <p>Sí hay algo</p>
+        </div>
+      )}
     </aside>
   )
 }
