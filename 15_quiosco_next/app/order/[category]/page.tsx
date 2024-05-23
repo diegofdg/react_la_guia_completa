0ef9b9564a9d251b935288a68ef35cf35ebf0004
diegofdg <diegofdg@gmail.com>
2024-05-23 07:18:32 -0300

    15_quiosco_next-detectar la categoría actual y traer los productos

--- 15_quiosco_next/app/order/[category]/page.tsx
+++ 15_quiosco_next/app/order/[category]/page.tsx
@@ -1,5 +1,18 @@
-export default function OrderPage({ params }: { params: { category : string }}) {
-  console.log(params)
+import { prisma } from "@/src/lib/prisma"
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category
+      }
+    }
+  })
+  return products
+}
+export default async function OrderPage({ params }: { params: { category : string }}) {
+  const products = await getProducts(params.category)
+  console.log(products)
   return (
     <div>OrderPage</div>
   )
