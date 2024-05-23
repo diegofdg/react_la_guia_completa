580034a449b5d73166d6de47e940a999439f9a50
diegofdg <diegofdg@gmail.com>
2024-05-23 07:45:26 -0300

    15_quiosco_next-resaltando la categoría actual

--- 15_quiosco_next/components/ui/CategoryIcon.tsx
+++ 15_quiosco_next/components/ui/CategoryIcon.tsx
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 import { Category } from "@prisma/client"
 
 type CategoryIconProps = {
@@ -7,9 +9,10 @@ type CategoryIconProps = {
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+  const params = useParams<{category: string}>()
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+    className={`${category.slug === params.category ? "bg-amber-400" : ""} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       <div className="w-16 h-16 relative">
         <Image
