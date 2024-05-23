8f6492fe9c6cd02e0a76ca46acd378f08e2dbb10
diegofdg <diegofdg@gmail.com>
2024-05-23 06:54:00 -0300

    15_quiosco_next-mostrando categorías y los iconos

--- 15_quiosco_next/components/ui/CategoryIcon.tsx
+++ 15_quiosco_next/components/ui/CategoryIcon.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import { Category } from "@prisma/client"
 
 type CategoryIconProps = {
@@ -6,6 +7,20 @@ type CategoryIconProps = {
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
   return (
-    <div>{category.name}</div>
+    <div
+      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+    >
+      <div className="w-16 h-16 relative">
+        <Image
+          fill
+          src={`/icon_${category.slug}.svg`}
+          alt="Imagen Categoria"
+        />
+      </div>
+
+      <p
+        className="text-xl font-bold"
+      >{category.name}</p>
+    </div>
   )
 }
