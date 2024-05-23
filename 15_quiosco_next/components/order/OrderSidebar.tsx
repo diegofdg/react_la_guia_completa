70012c0c7cf54a4b90a9bc81f78409ebfec1b176
diegofdg <diegofdg@gmail.com>
2024-05-23 06:48:08 -0300

    15_quiosco_next-iterando sobre las categorías

--- 15_quiosco_next/components/order/OrderSidebar.tsx
+++ 15_quiosco_next/components/order/OrderSidebar.tsx
@@ -1,7 +1,25 @@
-export default function OrderSidebar() {
+import { prisma } from "@/src/lib/prisma"
+import CategoryIcon from "../ui/CategoryIcon"
+
+async function getCategories() {
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories()
+  console.log(categories)
+
   return (
+
     <aside className="md:w-72 md:h-screen bg-white">
-        OrderSidebar
+      <nav className="mt-10">
+        {categories.map(category => (
+          <CategoryIcon
+            key={category.id}
+            category={category}
+          />
+        ))}
+      </nav>
     </aside>
   )
 }
