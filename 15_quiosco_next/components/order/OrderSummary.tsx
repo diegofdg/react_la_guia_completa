b0a6fb4bfef5432311bf91cd1df5c8d76f251c5a
diegofdg <diegofdg@gmail.com>
2024-05-24 07:35:36 -0300

    15_quiosco_next-calculando el total a pagar

--- 15_quiosco_next/components/order/OrderSummary.tsx
+++ 15_quiosco_next/components/order/OrderSummary.tsx
@@ -1,9 +1,13 @@
 "use client"
 import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
+import { formatCurrency } from "@/src/utils"
+import { useMemo } from "react"
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) , [order])
+  
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -16,6 +20,10 @@ export default function OrderSummary() {
               item={item}
             />
           ))}
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar: {""}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
