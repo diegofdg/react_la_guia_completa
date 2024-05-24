504bf4c59621714a84326939420c70abe2ae444b
diegofdg <diegofdg@gmail.com>
2024-05-24 07:30:01 -0300

    15_quiosco_next-decrementar la cantidad de un artículo

--- 15_quiosco_next/components/order/ProductDetails.tsx
+++ 15_quiosco_next/components/order/ProductDetails.tsx
@@ -2,14 +2,21 @@ import { XCircleIcon, PlusIcon, MinusIcon } from "@heroicons/react/24/outline"
 import { OrderItem } from "@/types"
 import { formatCurrency } from "@/src/utils"
 import { useStore } from "@/src/store"
+import { useMemo } from "react"
 
 type ProductDetailsProps = {
   item: OrderItem
 }
 
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
 export default function ProductDetails({ item }: ProductDetailsProps) {
   const increaseQuantity = useStore((state) => state.increaseQuantity)
-  
+  const decreaseQuantity = useStore((state) => state.decreaseQuantity)
+  const disableDecreaseButton = useMemo(() => item.quantity === MIN_ITEMS, [item])
+  const disableIncreaseButton = useMemo(() => item.quantity === MAX_ITEMS, [item])
+
   return (
     <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
       <div className="space-y-4">
@@ -29,7 +36,9 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
         <div className="flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg">
           <button
             type="button"
-            onClick={() => { }}
+            onClick={() => decreaseQuantity(item.id)}
+            disabled={disableDecreaseButton}
+            className="disabled:opacity-20"
           >
             <MinusIcon className="h-6 w-6" />
           </button>
@@ -41,6 +50,8 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
           <button
             type="button"
             onClick={() => increaseQuantity(item.id)}
+            className="disabled:opacity-10"
+            disabled={disableIncreaseButton}
           >
             <PlusIcon className="h-6 w-6" />
           </button>
--- 15_quiosco_next/src/store.ts
+++ 15_quiosco_next/src/store.ts
@@ -6,6 +6,7 @@ interface Store {
   order: OrderItem[]
   addToOrder: (product: Product) => void
   increaseQuantity: (id: Product["id"]) => void
+  decreaseQuantity: (id: Product["id"]) => void
 }
 
 export const useStore = create<Store>((set, get) => ({
@@ -41,6 +42,17 @@ export const useStore = create<Store>((set, get) => ({
         quantity: item.quantity + 1,
         subtotal: item.price * (item.quantity + 1)
       } : item)
+    }))
+  },
+  decreaseQuantity: (id) => {
+    const order = get().order.map(item => item.id === id ? {
+      ...item,
+      quantity: item.quantity - 1,
+      subtotal: item.price * (item.quantity - 1)
+    } : item)
+
+    set(() => ({
+      order
     }))
   },
 }))
