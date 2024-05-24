566fef217791fc6b60c52ccd4e487c2ebe1be98b
diegofdg <diegofdg@gmail.com>
2024-05-24 07:32:02 -0300

    15_quiosco_next-eliminar artículos de la orden

--- 15_quiosco_next/components/order/ProductDetails.tsx
+++ 15_quiosco_next/components/order/ProductDetails.tsx
@@ -14,6 +14,7 @@ const MAX_ITEMS = 5
 export default function ProductDetails({ item }: ProductDetailsProps) {
   const increaseQuantity = useStore((state) => state.increaseQuantity)
   const decreaseQuantity = useStore((state) => state.decreaseQuantity)
+  const removeItem = useStore((state) => state.removeItem)
   const disableDecreaseButton = useMemo(() => item.quantity === MIN_ITEMS, [item])
   const disableIncreaseButton = useMemo(() => item.quantity === MAX_ITEMS, [item])
 
@@ -25,7 +26,7 @@ export default function ProductDetails({ item }: ProductDetailsProps) {
 
           <button
             type="button"
-            onClick={() => { }}
+            onClick={() => removeItem(item.id)}
           >
             <XCircleIcon className="text-red-600 h-8 w-8" />
           </button>
--- 15_quiosco_next/src/store.ts
+++ 15_quiosco_next/src/store.ts
@@ -7,6 +7,7 @@ interface Store {
   addToOrder: (product: Product) => void
   increaseQuantity: (id: Product["id"]) => void
   decreaseQuantity: (id: Product["id"]) => void
+  removeItem: (id: Product["id"]) => void
 }
 
 export const useStore = create<Store>((set, get) => ({
@@ -53,6 +54,11 @@ export const useStore = create<Store>((set, get) => ({
 
     set(() => ({
       order
+    }))
+  },
+  removeItem: (id) => {
+    set((state) => ({
+      order: state.order.filter(item => item.id !== id)
     }))
   },
 }))
