c2b7da145741eb69c22aa29aff0e6c5bce1bece8
diegofdg <diegofdg@gmail.com>
2024-05-24 07:20:06 -0300

    15_quiosco_next-evitar duplicados

--- 15_quiosco_next/src/store.ts
+++ 15_quiosco_next/src/store.ts
@@ -11,12 +11,26 @@ export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, image, ...data } = product
-    set((state) => ({
-      order: [...state.order, {
+    let order: OrderItem[] = []
+
+    if (get().order.find(item => item.id === product.id)) {
+      order = get().order.map(item => item.id === product.id ? {
+        ...item,
+        quantity: item.quantity + 1,
+        subtotal: item.price * (item.quantity + 1)
+      } : item)
+
+    } else {
+      order = [...get().order, {
         ...data,
         quantity: 1,
         subtotal: 1 * product.price
       }]
+
+    }
+    set(() => ({
+      order
     }))
+
   }
 }))
