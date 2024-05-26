7871edcb8d1ae5becec6121694a6cba2ab69db9e
diegofdg <diegofdg@gmail.com>
2024-05-26 19:15:41 -0300

    15_quiosco_next-mostrar las ordenes pendientes

--- 15_quiosco_next/app/admin/orders/page.tsx
+++ 15_quiosco_next/app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from "@/components/order/OrderCard"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
 
@@ -14,18 +15,28 @@ async function getPendingOrders() {
       }
     }
   })
-  return orders  
+  return orders
 }
 
 export default async function OrdersPage() {
   const orders = await getPendingOrders()
-  console.log(JSON.stringify(orders, null,2))
+  console.log(JSON.stringify(orders, null, 2))
 
   return (
     <>
       <Heading>
         Administrar Ordenes
       </Heading>
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          {orders.map(order => (
+            <OrderCard
+              key={order.id}
+              order={order}
+            />
+          ))}
+        </div>
+      ) : <p className="text-center">No hay ordenes Pendientes</p>}
     </>
   )
 }
---
+++ 15_quiosco_next/components/order/OrderCard.tsx
@@ -0,0 +1,33 @@
+import { OrderWithProducts } from "@/types"
+
+type OrderCardProps = {
+  order: OrderWithProducts
+}
+
+export default function OrderCard({ order }: OrderCardProps) {
+
+  return (
+    <section
+      aria-labelledby="summary-heading"
+      className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6  lg:mt-0 lg:p-8 space-y-4"
+    >
+      <p className="text-2xl font-medium text-gray-900">Cliente: {order.name}</p>
+      <p className="text-lg font-medium text-gray-900">Productos Ordenados:</p>
+      <dl className="mt-6 space-y-4">
+
+        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+          <dt className="text-base font-medium text-gray-900">Total a Pagar:</dt>
+          <dd className="text-base font-medium text-gray-900">{ }</dd>
+        </div>
+      </dl>
+
+      <form>
+        <input
+          type="submit"
+          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          value="Marcar Orden Completada"
+        />
+      </form>
+    </section>
+  )
+}
