44d70a4710b5b9bd774563a28a01c864ebf1857d
diegofdg <diegofdg@gmail.com>
2024-05-26 19:50:01 -0300

    15_quiosco_next-listando los productos

--- 15_quiosco_next/app/admin/products/page.tsx
+++ 15_quiosco_next/app/admin/products/page.tsx
@@ -1,11 +1,23 @@
+import ProductTable from "@/components/products/ProductsTable"
 import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
 
-export default function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany({})
+  return products
+
+}
+
+export default async function ProductsPage() {
+  const products = await getProducts()
   return (
     <>
       <Heading>
         Administrar Productos
       </Heading>
+      <ProductTable
+            products={products}
+          />  
     </>
   )
 }
