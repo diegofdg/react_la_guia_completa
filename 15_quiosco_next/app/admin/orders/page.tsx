31db4d77479bba473db9d1e37d2e843ce87c247f
diegofdg <diegofdg@gmail.com>
2024-05-26 19:10:15 -0300

    15_quiosco_next-obtener las ordenes pendientes

--- 15_quiosco_next/app/admin/orders/page.tsx
+++ 15_quiosco_next/app/admin/orders/page.tsx
@@ -1,6 +1,26 @@
-import Heading from "@/components/ui/Heading";
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+  return orders  
+}
+
+export default async function OrdersPage() {
+  const orders = await getPendingOrders()
+  console.log(JSON.stringify(orders, null,2))
+
   return (
     <>
       <Heading>
