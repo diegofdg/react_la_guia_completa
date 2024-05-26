57e103a25de227566b5afb41b669263f614d7f5e
diegofdg <diegofdg@gmail.com>
2024-05-26 07:34:18 -0300

    15_quiosco_next-introducción a server actions

--- 15_quiosco_next/components/order/OrderSummary.tsx
+++ 15_quiosco_next/components/order/OrderSummary.tsx
@@ -6,8 +6,10 @@ import { useMemo } from "react"
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
-  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) , [order])
-  
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+  const handleCreateOrder = async () => {}
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -24,6 +26,23 @@ export default function OrderSummary() {
             Total a pagar: {""}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+          <form
+            className="w-full mt-10 space-y-5"
+            action={handleCreateOrder}
+          >
+            <input
+              type="text"
+              placeholder="Tu Nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name"
+            />
+
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
