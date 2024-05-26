c46a46d24e5985abd6ccef87a09a7ae5f218d244
diegofdg <diegofdg@gmail.com>
2024-05-26 19:18:41 -0300

    15_quiosco_next-mostrar el contenido de la orden

--- 15_quiosco_next/components/order/OrderCard.tsx
+++ 15_quiosco_next/components/order/OrderCard.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "@/src/utils"
 import { OrderWithProducts } from "@/types"
 
 type OrderCardProps = {
@@ -14,10 +15,21 @@ export default function OrderCard({ order }: OrderCardProps) {
       <p className="text-2xl font-medium text-gray-900">Cliente: {order.name}</p>
       <p className="text-lg font-medium text-gray-900">Productos Ordenados:</p>
       <dl className="mt-6 space-y-4">
+        {order.orderProducts.map(product => (
+          <div
+            key={product.productId}
+            className="flex items-center gap-2 border-t border-gray-200 pt-4"
+          >
+            <dt className="flex items-center text-sm text-gray-600">
+              <span className="font-black">({product.quantity}) {""}</span>
+            </dt>
+            <dd className="text-sm font-medium text-gray-900">{product.product.name}</dd>
+          </div>
+        ))}
 
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <dt className="text-base font-medium text-gray-900">Total a Pagar:</dt>
-          <dd className="text-base font-medium text-gray-900">{ }</dd>
+          <dd className="text-base font-medium text-gray-900">{formatCurrency(order.total)}</dd>
         </div>
       </dl>
 
