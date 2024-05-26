598282b571813c8855a611268322821fbf3e2149
diegofdg <diegofdg@gmail.com>
2024-05-26 12:32:39 -0300

    15_quiosco_next-navegación en el panel de administración

--- 15_quiosco_next/app/products/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ProductsPage() {
-  return (
-    <div>ProductsPage</div>
-  )
-}
--- 15_quiosco_next/components/admin/AdminSidebar.tsx
+++ 15_quiosco_next/components/admin/AdminSidebar.tsx
@@ -1,4 +1,5 @@
 import Logo from "../ui/Logo"
+import AdminRoute from "./AdminRoute"
 
 const adminNavigation = [
   { url: "/admin/orders", text: "Ordenes", blank: false },
@@ -14,7 +15,12 @@ export default function AdminSidebar() {
       <div className="space-y-3 ">
         <p className="mt-10 uppercase font-bold text-sm text-gray-600 text-center">Navegación</p>
         <nav className="flex flex-col">
-
+          {adminNavigation.map(link => (
+            <AdminRoute
+              key={link.url}
+              link={link}
+            />
+          ))}
         </nav>
       </div>
     </>
