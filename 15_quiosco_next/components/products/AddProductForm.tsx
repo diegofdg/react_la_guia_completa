3b63daa7be9efb239fcced976f35835b934f16fe
diegofdg <diegofdg@gmail.com>
2024-05-27 18:40:55 -0300

    15_quiosco_next-validando la creación de productos

--- 15_quiosco_next/components/products/AddProductForm.tsx
+++ 15_quiosco_next/components/products/AddProductForm.tsx
@@ -1,8 +1,26 @@
 "use client"
 
-export default function AddProductForm({children}: {children : React.ReactNode}) {
+import { ProductSchema } from "@/src/schema"
+import { toast } from "react-toastify"
+
+export default function AddProductForm({ children }: { children: React.ReactNode }) {
   const handleSubmit = async (formData: FormData) => {
-    console.log("Desde handleSubmit")
+    const data = {
+      name: formData.get("name"),
+      price: formData.get("price"),
+      categoryId: formData.get("categoryId")
+    }
+    const result = ProductSchema.safeParse(data)
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      })
+      return
+    }
+
+    console.log(result.data)
+
+
   }
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
