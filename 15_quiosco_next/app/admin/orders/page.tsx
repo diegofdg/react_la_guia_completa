b8cedb408dd7b08eff0b70bc2746003239c29b5f
diegofdg <diegofdg@gmail.com>
2024-05-27 20:33:09 -0300

    15_quiosco_next-revalidación manual

--- 15_quiosco_next/app/admin/orders/page.tsx
+++ 15_quiosco_next/app/admin/orders/page.tsx
@@ -1,6 +1,7 @@
 import OrderCard from "@/components/order/OrderCard"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
+import { revalidatePath } from "next/cache"
 
 async function getPendingOrders() {
   const orders = await prisma.order.findMany({
@@ -20,12 +21,27 @@ async function getPendingOrders() {
 
 export default async function OrdersPage() {
   const orders = await getPendingOrders()
+  const refreshOrders = async () => {
+    "use server"
+    revalidatePath("/admin/orders")
+  }
 
   return (
     <>
       <Heading>
         Administrar Ordenes
       </Heading>
+      <form
+        action={refreshOrders}
+      >
+        <input
+          type="submit"
+          value="Actualizar ordenes"
+          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
+        />
+
+      </form>
+
       {orders.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
           {orders.map(order => (
