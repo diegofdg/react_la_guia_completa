25987ab5047f0e21d37738b7b77529895118939e
diegofdg <diegofdg@gmail.com>
2024-05-27 19:24:41 -0300

    15_quiosco_next-almacenando el producto en la base de datos

--- 15_quiosco_next/components/products/AddProductForm.tsx
+++ 15_quiosco_next/components/products/AddProductForm.tsx
@@ -1,14 +1,19 @@
 "use client"
 
+import { createProduct } from "@/actions/create-product-action"
 import { ProductSchema } from "@/src/schema"
+import { useRouter } from "next/navigation"
 import { toast } from "react-toastify"
 
 export default function AddProductForm({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
   const handleSubmit = async (formData: FormData) => {
     const data = {
       name: formData.get("name"),
       price: formData.get("price"),
-      categoryId: formData.get("categoryId")
+      categoryId: formData.get("categoryId"),
+      image: formData.get("image")
     }
     const result = ProductSchema.safeParse(data)
     if (!result.success) {
@@ -18,10 +23,18 @@ export default function AddProductForm({ children }: { children: React.ReactNode
       return
     }
 
-    console.log(result.data)
+    const response = await createProduct(result.data)
+    if (response?.errors) {
+      response.errors.forEach(issue => {
+        toast.error(issue.message)
+      })
+      return
+    }
 
-
+    toast.success("Producto Creado correctamente")
+    router.push("/admin/products")
   }
+
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form
