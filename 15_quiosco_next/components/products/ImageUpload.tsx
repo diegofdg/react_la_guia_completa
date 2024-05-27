a9074a8cde8d3ffc0203f54b07fd21147e599eaa
diegofdg <diegofdg@gmail.com>
2024-05-27 19:03:34 -0300

    15_quiosco_next-creando el componente para subir imagenes

--- 15_quiosco_next/components/products/ImageUpload.tsx
+++ 15_quiosco_next/components/products/ImageUpload.tsx
@@ -1,5 +1,34 @@
+"use client"
+import { CldUploadWidget } from "next-cloudinary"
+import { TbPhotoPlus } from "react-icons/tb"
+
 export default function ImageUpload() {
   return (
-    <div>ImageUpload</div>
+    <CldUploadWidget
+      onSuccess={(result, { widget }) => {
+        console.log(result)
+      }}
+      uploadPreset="o4mi4jct"
+      options={{
+        maxFiles: 1
+      }}
+    >
+      {({ open }) => (
+        <>
+          <div className="space-y-2">
+            <label className="text-slate-800">Imagen Producto</label>
+            <div
+              className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100 "
+              onClick={() => open()}
+            >
+              <TbPhotoPlus
+                size={50}
+              />
+              <p className="text-lg font-semibold">Agregar Imagen</p>
+            </div>
+          </div>
+        </>
+      )}
+    </CldUploadWidget>
   )
 }
