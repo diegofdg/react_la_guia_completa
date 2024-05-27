b5aadd92ee7f3ba808648fc2e68ce1ecc497be12
diegofdg <diegofdg@gmail.com>
2024-05-27 07:14:04 -0300

    15_quiosco_next-validando el buscador

--- 15_quiosco_next/components/products/ProductSearchForm.tsx
+++ 15_quiosco_next/components/products/ProductSearchForm.tsx
@@ -1,6 +1,28 @@
+"use client"
+import { SearchSchema } from "@/src/schema"
+import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
+
 export default function ProductSearchForm() {
+  const router = useRouter()
+  
+  const handleSearchForm = (formData: FormData) => {
+    const data = {
+      search: formData.get('search')
+    }
+    const result = SearchSchema.safeParse(data)
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      })
+      return
+    }
+    router.push(`/admin/products/search?search=${result.data.search}`)
+
+  }
   return (
     <form
+      action={handleSearchForm}
       className="flex items-center"
     >
       <input
--- 15_quiosco_next/src/schema/index.ts
+++ 15_quiosco_next/src/schema/index.ts
@@ -19,3 +19,9 @@ export const OrderIdSchema = z.object({
     .transform((value) => parseInt(value))
     .refine(value => value > 0, { message: "Hay errores" })
 })
+
+export const SearchSchema = z.object({
+  search: z.string()
+    .trim()
+    .min(1, { message: "La búsqueda no puede ir vacia" })
+})
