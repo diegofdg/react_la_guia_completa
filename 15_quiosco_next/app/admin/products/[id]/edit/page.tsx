e76fbb0d27050ca8ae9b721d371259c9a493f484
diegofdg <diegofdg@gmail.com>
2024-05-27 19:39:09 -0300

    15_quiosco_next-obtener el producto a editar

---
+++ 15_quiosco_next/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import ProductForm from "@/components/products/ProductForm"
+import Heading from "@/components/ui/Heading"
+import { prisma } from "@/src/lib/prisma"
+import { notFound, redirect } from "next/navigation"
+
+async function getProductById(id: number) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id
+    }
+  })
+  if (!product) {
+    notFound()
+  }
+
+  return product
+}
+
+export default async function EditProductsPage({ params }: { params: { id: string } }) {
+
+  const product = await getProductById(+params.id)
+  console.log(product)
+
+  return (
+    <>
+      <div>EditProductPage</div>
+    </>
+  )
+}
