890cfa0e7d56e91186c4d228263b9bea0c715cec
diegofdg <diegofdg@gmail.com>
2024-05-27 21:07:11 -0300

    15_quiosco_next-configurando swr

--- 15_quiosco_next/app/admin/orders/page.tsx
+++ 15_quiosco_next/app/admin/orders/page.tsx
@@ -1,50 +1,26 @@
+"use client"
+import useSWR from "swr"
 import OrderCard from "@/components/order/OrderCard"
 import Heading from "@/components/ui/Heading"
-import { prisma } from "@/src/lib/prisma"
-import { revalidatePath } from "next/cache"
+import { OrderWithProducts } from "@/types"
 
-async function getPendingOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true
-        }
-      }
-    }
+export default function OrdersPage() {
+  const url = "/admin/orders/api"
+  const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 1000,
+    revalidateOnFocus: false,
   })
-  return orders
-}
 
-export default async function OrdersPage() {
-  const orders = await getPendingOrders()
-  const refreshOrders = async () => {
-    "use server"
-    revalidatePath("/admin/orders")
-  }
+  if (isLoading) return <p>Cargando...</p>
 
-  return (
+  if (data) return (
     <>
-      <Heading>
-        Administrar Ordenes
-      </Heading>
-      <form
-        action={refreshOrders}
-      >
-        <input
-          type="submit"
-          value="Actualizar ordenes"
-          className="bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer"
-        />
+      <Heading>Administrar Ordenes</Heading>
 
-      </form>
-
-      {orders.length ? (
+      {data.length ? (
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => (
+          {data.map(order => (
             <OrderCard
               key={order.id}
               order={order}
